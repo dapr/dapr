65884ca5dfd6d8acdc88bda4fe7abbb44d546010
unknown <ryanvolum@gmail.com>
2019-06-27 22:56:00 -0700

    moved kubernetes resources to samples directory

--- samples/kubernetes_zero_to_hero/node.js/app.js
+++ samples/kubernetes_zero_to_hero/node.js/app.js
@@ -1,42 +1,39 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.json())
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
 
-const port = 3000
+const port = 3000;
 
 app.post('/state', (req, res) => {
-    e = req.body
+    e = req.body;
 
     if (e.length > 0) {
-        order = e[0].value
+        order = e[e.length - 1].value;
     }
 
-    res.status(200).send()
-})
+    res.status(200).send();
+});
 
-var order;
+let order;
 
-app.get('/order', (req, res) => {
-    res.json(order)
-})
+app.get('/order', (_req, res) => {
+    res.json(order);
+});
 
 app.post('/neworder', (req, res) => {
-    data = req.body.data
-    orderID = data.orderID
+    const data = req.body.data;
+    const orderId = data.orderId;
+    console.log("Got a new order! Order ID: " + orderId);
 
-    console.log("Got a new order! Order ID: " + orderID)
+    order = data;
 
-    order = data
-    
     res.json({
-        state: [
-            {
-                key: "order",
-                value: order
-            }
-        ]
-    })
-})
+        state: [{
+            key: "order",
+            value: order
+        }]
+    });
+});
 
-app.listen(port, () => console.log(`Node App listening on port ${port}!`))
+app.listen(port, () => console.log(`Node App listening on port ${port}!`));
