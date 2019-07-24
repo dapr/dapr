917869884c31b144b804d65823f395dc776cbe6d
unknown <ryanvolum@gmail.com>
2019-07-24 14:54:15 -0700

    reformat

--- samples/distributed-calculator/react-calculator/client/src/logic/operate.js
+++ samples/distributed-calculator/react-calculator/client/src/logic/operate.js
@@ -12,14 +12,17 @@ export default async function operate(operandOne, operandTwo, operationSymbol) {
 
   const operation = operationMap[operationSymbol];
   console.log(`Calling ${operation} service`);
-  
+
   const rawResponse = await fetch(`/calculate/${operation}`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({operandOne: operandOne, operandTwo: operandTwo}),
+    body: JSON.stringify({
+      operandOne,
+      operandTwo
+    }),
   });
   const response = await rawResponse.json();
 
