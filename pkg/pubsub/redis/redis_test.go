c61070180474aa96de28419e30c769b65cd034a8
Young Bu Park <youngp@microsoft.com>
2019-07-24 13:37:28 -0700

    intial commit of redis_test

---
+++ pkg/pubsub/redis/redis_test.go
@@ -0,0 +1,23 @@
+package redis
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseRedisMetadata(t *testing.T) {
+	assert.Equal(t, 123, 123)
+
+	t.Run("metadata is correct", func(t *testing.T) {
+
+	})
+
+	t.Run("host is not given", func(t *testing.T) {
+
+	})
+
+	t.Run("consumerID is not given", func(t *testing.T) {
+
+	})
+}
