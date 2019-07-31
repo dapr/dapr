bf6dececd022a4ddadf8cfe5ab3ef3752cc13064
yaron2 <yaronsc@microsoft.com>
2019-07-31 11:21:36 -0700

    linux http test fix

--- pkg/channel/http/http_channel_test.go
+++ pkg/channel/http/http_channel_test.go
@@ -14,21 +14,21 @@ import (
 )
 
 func TestInvokeMethod(t *testing.T) {
-	srv := &http.Server{Addr: ":9999"}
+	srv := &http.Server{Addr: ":5555"}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, r.URL.RawQuery)
 	})
 	go func() {
 		srv.ListenAndServe()
 	}()
-	c := Channel{baseAddress: "http://localhost:9999", client: &fasthttp.Client{}}
+	c := Channel{baseAddress: "http://localhost:5555", client: &fasthttp.Client{}}
 	request := &channel.InvokeRequest{
 		Metadata: map[string]string{QueryString: "param1=val1&param2=val2"},
 	}
 	response, err := c.InvokeMethod(request)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	srv.Shutdown(ctx)
 	assert.NoError(t, err)
 	assert.Equal(t, "param1=val1&param2=val2", string(response.Data))
+	srv.Shutdown(ctx)
 }
