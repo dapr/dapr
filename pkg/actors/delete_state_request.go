c0f624b8f040e5eaf0c8e67220f32f1de80eb3a6
Young Bu Park <youngp@microsoft.com>
2019-08-16 19:03:25 -0700

    fix comment

--- pkg/actors/delete_state_request.go
+++ pkg/actors/delete_state_request.go
@@ -1,6 +1,6 @@
 package actors
 
-// DeleteStateRequest is the request object for saving an actor state
+// DeleteStateRequest is the request object for deleting an actor state
 type DeleteStateRequest struct {
 	ActorID   string `json:"actorId"`
 	ActorType string `json:"actorType"`
