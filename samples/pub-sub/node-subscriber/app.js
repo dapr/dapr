dc9e57949ab6aef54a2e89d5eb4d5f6665602376
unknown <ryanvolum@gmail.com>
2019-08-22 13:08:45 -0700

    reordered routes

--- samples/pub-sub/node-subscriber/app.js
+++ samples/pub-sub/node-subscriber/app.js
@@ -6,6 +6,13 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
+app.get('/actions/subscribe', (_req, res) => {
+    res.json([
+        'A',
+        'B'
+    ]);
+});
+
 app.post('/A', (req, res) => {
     console.log("A: ", req.body);
     res.sendStatus(200);
@@ -15,12 +22,5 @@ app.post('/B', (req, res) => {
     console.log("B: ", req.body);
     res.sendStatus(200);
 });
-
-app.get('/actions/subscribe', (req, res) => {
-    res.json([
-        'A',
-        'B'
-    ]);
-})
 
 app.listen(port, () => console.log(`Node App listening on port ${port}!`));
