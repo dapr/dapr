3ab40ce8daf4e91c1dbe8e72dcc0faef2769a756
yaron2 <yaronsc@microsoft.com>
2019-09-17 23:39:35 -0700

    add cloudevents support

---
+++ pkg/components/pubsub/envelope_test.go
@@ -0,0 +1,34 @@
+package pubsub
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateCloudEventsEnvelope(t *testing.T) {
+	envelope := NewCloudEventsEnvelope("a", "source", "eventType", nil)
+	assert.NotNil(t, envelope)
+}
+
+func TestCreateCloudEventsEnvelopeDefaults(t *testing.T) {
+	t.Run("default event type", func(t *testing.T) {
+		envelope := NewCloudEventsEnvelope("a", "source", "", nil)
+		assert.Equal(t, DefaultCloudEventType, envelope.Type)
+	})
+
+	t.Run("non-default event type", func(t *testing.T) {
+		envelope := NewCloudEventsEnvelope("a", "source", "e1", nil)
+		assert.Equal(t, "e1", envelope.Type)
+	})
+
+	t.Run("spec version", func(t *testing.T) {
+		envelope := NewCloudEventsEnvelope("a", "source", "", nil)
+		assert.Equal(t, CloudEventsSpecVersion, envelope.SpecVersion)
+	})
+
+	t.Run("has data", func(t *testing.T) {
+		envelope := NewCloudEventsEnvelope("a", "source", "", []byte("data"))
+		assert.Equal(t, "data", envelope.Data.(string))
+	})
+}
