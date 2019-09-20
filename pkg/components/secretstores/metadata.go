e1b07692c48e83a23315dd7a919b088263ddb40c
Young Bu Park <youngp@microsoft.com>
2019-09-19 19:52:10 -0700

    add comment

--- pkg/components/secretstores/metadata.go
+++ pkg/components/secretstores/metadata.go
@@ -1,5 +1,6 @@
 package secretstores
 
+// DefaultSecretRefKeyName is the default key if secretKeyRef.key is not given
 const DefaultSecretRefKeyName = "_value"
 
 // Metadata contains a secretstore specific set of metadata properties
