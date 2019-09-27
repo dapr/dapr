fce82e42b0f044428b303db0f422ea200d51ae4f
unknown <ryanvolum@gmail.com>
2019-09-27 11:08:38 -0700

    deleted samples

--- samples/3.distributed-calculator/C#/Models/Operands.cs
+++
@@ -1,6 +0,0 @@
-namespace Subtract.Models {
-    public class Operands {
-        public string OperandOne { get; set;}
-        public string OperandTwo { get; set;}
-    }
-}
--- samples/4.pub-sub/node-subscriber/Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:8-alpine
-WORKDIR /usr/src/app
-COPY . .
-RUN npm install
-EXPOSE 3000
-CMD [ "node", "app.js" ]
