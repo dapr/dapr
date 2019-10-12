4283ddef45aafde5c2e4f007da5dcf751714be00
Brendan Burns <bburns@microsoft.com>
2019-10-12 09:31:17 -0700

    Update for dotnet generation. (#571)
    * Update for dotnet generation.
    
    * Updating  csharp_namespace tp DApr.Client.Grpc
    
    * Chaning path for dotnet generation to Dapr.Client.Grpc

--- tools/proto/generate.sh
+++ tools/proto/generate.sh
@@ -14,18 +14,21 @@ VERSION=3.10.0
 # Generate proto buffers
 # First arg is name of language (e.g. 'javascript')
 # Second arg is name of tool (e.g. 'js')
+# Third arg is path within the language directory to write to
+# Fourth arg is args for the generator
+# Remaining args are appended to the command.
 generate() {
     language=${1}
     tool=${2}
     path=${3}
     args=${4}
 
-    mkdir -p ${top_root}/../dapr-${language}/${path}
+    mkdir -p ${top_root}/../${language}-sdk/${path}
     
     for proto_file in "daprclient/daprclient.proto" "dapr/dapr.proto"; do
         echo "Generating ${language} for ${proto_file}"
         ${root}/bin/protoc --proto_path ${top_root}/pkg/proto/ \
-            --${2}_out=${args}:${top_root}/../dapr-${language}/${path} \
+            --${tool}_out=${args}:${top_root}/../${language}-sdk/${path} \
             ${top_root}/pkg/proto/${proto_file} ${@:5}
     done
 }
@@ -67,21 +70,20 @@ PROTOC_PLUGIN=$(which grpc_tools_node_protoc_plugin)
 dotnet_grpc_plugin_file="${HOME}/.nuget/packages/grpc.tools/2.24.0/tools/${full_os}_x64/grpc_csharp_plugin"
 
 # generate javascript
-generate javascript js src 'import_style=commonjs' \
+generate js js src 'import_style=commonjs' \
   --plugin=protoc-gen-grpc=${PROTOC_PLUGIN} \
-  --grpc_out=${top_root}/../dapr-javascript/src
+  --grpc_out=${top_root}/../js-sdk/src
 
 # generate java
-mkdir -p ${top_root}/../dapr-java/src/main/java
 generate java java src/main/java '' \
   --plugin=protoc-gen-grpc-java=${java_grpc_plugin_path} \
-  --grpc-java_out=${top_root}/../dapr-java/src/main/java
+  --grpc-java_out=${top_root}/../java-sdk/src/main/java
 
 # generate python
-mkdir -p ${top_root}/../dapr-python
+mkdir -p ${top_root}/../python-sdk
 python3 -m grpc.tools.protoc -I${top_root}/pkg/proto \
-   --python_out=${top_root}/../dapr-python \
-   --grpc_python_out=${top_root}/../dapr-python \
+   --python_out=${top_root}/../python-sdk \
+   --grpc_python_out=${top_root}/../python-sdk \
    dapr/dapr.proto \
    daprclient/daprclient.proto
 
@@ -90,9 +92,9 @@ generate go go . `` --plugin=grpc
 
 # generate dotnet
 # dotnet generates their own via dotnet build...
-generate dotnet csharp src '' \
+generate dotnet csharp src/Dapr.Client.Grpc '' \
   --plugin=protoc-gen-grpc=${dotnet_grpc_plugin_file} \
-  --grpc_out=${top_root}/../dapr-dotnet/src
+  --grpc_out=${top_root}/../dotnet-sdk/src/Dapr.Client.Grpc
 
 # cleanup
 rm -r ${root}/include ${root}/bin ${root}/${file} ${root}/readme.txt ${java_grpc_plugin_path}
