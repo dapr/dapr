0a822349580e7e257984021be8cbfab326e3414c
shalabhs <shalabhs@microsoft.com>
2019-10-23 14:33:40 -0700

    Adding Unit tests for DaprHandler

---
+++ pkg/operator/handlers/dapr_handler_test.go
@@ -0,0 +1,111 @@
+package handlers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	testclient "github.com/dapr/dapr/pkg/client/clientset/versioned"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestGetDaprID_WithValidId(t *testing.T) {
+
+	// Arrange
+	expected := "test_id"
+	deployment := getDeployment(expected, "true")
+	testDaprHandler := NewDaprHandler(testclient.New(nil))
+
+	// Act
+	got := testDaprHandler.getDaprID(deployment)
+
+	// Assert
+	assert.Equal(t, expected, got)
+}
+
+func TestGetDaprID_WithEmptyId(t *testing.T) {
+
+	// Arrange
+	expected := ""
+	deployment := getDeployment(expected, "true")
+	testDaprHandler := NewDaprHandler(testclient.New(nil))
+
+	// Act
+	got := testDaprHandler.getDaprID(deployment)
+
+	// Assert
+	assert.Equal(t, expected, got)
+}
+
+func TestIsAnnotatedForDapr_WhenEnabled(t *testing.T) {
+
+	// Arrange
+	expected := "true"
+	deployment := getDeployment("test_id", expected)
+	testDaprHandler := NewDaprHandler(testclient.New(nil))
+
+	// Act
+	got := testDaprHandler.isAnnotatedForDapr(deployment)
+
+	// Assert
+	assert.True(t, got)
+}
+
+func TestIsAnnotatedForDapr_WhenDisabled(t *testing.T) {
+
+	// Arrange
+	expected := "false"
+	deployment := getDeployment("test_id", expected)
+	testDaprHandler := NewDaprHandler(testclient.New(nil))
+
+	// Act
+	got := testDaprHandler.isAnnotatedForDapr(deployment)
+
+	// Assert
+	assert.False(t, got)
+}
+
+func TestIsAnnotatedForDapr_WhenRandomString(t *testing.T) {
+
+	// Arrange
+	expected := "0"
+	deployment := getDeployment("test_id", expected)
+	testDaprHandler := NewDaprHandler(testclient.New(nil))
+
+	// Act
+	got := testDaprHandler.isAnnotatedForDapr(deployment)
+
+	// Assert
+	assert.False(t, got)
+}
+
+func getDeployment(daprId string, daprEnabled string) *appsv1.Deployment {
+
+	// Arrange
+	metadata := meta_v1.ObjectMeta{
+		Name:   "app",
+		Labels: map[string]string{"app": "test_app"},
+		Annotations: map[string]string{
+			daprIDAnnotationKey:      daprId,
+			daprEnabledAnnotationKey: daprEnabled,
+		},
+	}
+
+	podTemplateSpec := corev1.PodTemplateSpec{
+		ObjectMeta: metadata,
+	}
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name: "app",
+		},
+
+		Spec: appsv1.DeploymentSpec{
+			Template: podTemplateSpec,
+		},
+	}
+
+	return deployment
+}
