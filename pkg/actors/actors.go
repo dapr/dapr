25339f0ef017fd555acc3f20cdc1f5e84842c7c2
yaron2 <yaronsc@microsoft.com>
2019-10-29 11:53:39 -0700

    remove unused function

--- pkg/actors/actors.go
+++ pkg/actors/actors.go
@@ -206,11 +206,6 @@ func (a *actorsRuntime) Call(req *CallRequest) (*CallResponse, error) {
 	return resp, nil
 }
 
-func (a *actorsRuntime) actorInstanceExists(key string) bool {
-	_, exists := a.actorsTable.Load(key)
-	return exists
-}
-
 func (a *actorsRuntime) callLocalActor(actorType, actorID, actorMethod string, data []byte, metadata map[string]string) (*CallResponse, error) {
 	key := a.constructCombinedActorKey(actorType, actorID)
 
