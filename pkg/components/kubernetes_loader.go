48dd9027f63b6d39b64333ee916c2be0ef04753b
Dave Bennett <david.a.bennett7@gmail.com>
2019-10-31 16:17:00 -0400

    Retry Mechanism for Kubernetes Loader (#690)
    * initial commit
    
    * unit test for components loader and constant
    
    * Chnaged print statement to log

--- pkg/components/kubernetes_loader.go
+++ pkg/components/kubernetes_loader.go
@@ -12,12 +12,16 @@ import (
 
 	components_v1alpha1 "github.com/dapr/dapr/pkg/apis/components/v1alpha1"
 	config "github.com/dapr/dapr/pkg/config/modes"
+	log "github.com/Sirupsen/logrus"
 	"github.com/valyala/fasthttp"
+	"github.com/cenkalti/backoff"
 )
+
+const maxRetryTime  = time.Second * 30
 
 // KubernetesComponents loads components in a kubernetes environment
 type KubernetesComponents struct {
-	config config.KubernetesConfig
+	config config.KubernetesConfig 
 }
 
 // NewKubernetesComponents returns a new kubernetes loader
@@ -29,7 +33,29 @@ func NewKubernetesComponents(configuration config.KubernetesConfig) *KubernetesC
 
 // LoadComponents returns components from a given control plane address
 func (k *KubernetesComponents) LoadComponents() ([]components_v1alpha1.Component, error) {
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = maxRetryTime
+	
 	url := fmt.Sprintf("%s/components", k.config.ControlPlaneAddress)
+	var components []components_v1alpha1.Component
+	
+	err := backoff.Retry(func() error {
+		body, err := requestControlPlane(url)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(body, &components)
+		if err != nil {
+			return nil
+		}
+		return nil;
+	}, b)
+
+	return components, err
+}
+
+// Retry mechanism when requesting the Operator API
+func requestControlPlane(url string) ([]byte, error) {
 	req := fasthttp.AcquireRequest()
 	req.SetRequestURI(url)
 	req.Header.SetContentType("application/json")
@@ -40,16 +66,11 @@ func (k *KubernetesComponents) LoadComponents() ([]components_v1alpha1.Component
 	}
 	err := client.Do(req, resp)
 	if err != nil {
+		// Request failed, try again
+		log.Info("Retrying getting components")
 		return nil, err
 	}
 
 	body := resp.Body()
-
-	var components []components_v1alpha1.Component
-	err = json.Unmarshal(body, &components)
-	if err != nil {
-		return nil, err
-	}
-
-	return components, nil
+	return body, nil
 }
