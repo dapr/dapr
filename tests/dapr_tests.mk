a33c0677566889c95d43c151b691b67bdd2eb173
Young Bu Park <youngp@microsoft.com>
2019-11-14 10:29:31 -0800

    fix build

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -10,24 +10,20 @@ E2E_TEST_APPS=hellodapr
 # E2E test app root directory
 E2E_TESTAPP_DIR=./tests/apps
 
-# default test environment
-ifeq ($(TEST_ENV),)
-TEST_ENV=minikube
+ifeq ($(TEST_ENV),minikube)
+MINIKUBE_NODE_IP=$(shell miniube ip)
+ifeq ($(MINIKUBE_NODE_IP),)
+$(error cannot find get minikube node ip address. ensure that you have minikube environment.)
+endif
 endif
 
 # check the required environment variables
 check-e2e-env:
 ifeq ($(DAPR_TEST_REGISTRY),)
-	$(error DAPR_TEST_REGISTRY environment variable must be set)
+  $(error DAPR_TEST_REGISTRY environment variable must be set)
 endif
 ifeq ($(DAPR_TEST_TAG),)
-	$(error DAPR_TEST_TAG environment variable must be set)
-endif
-ifeq ($(TEST_ENV),minikube)
-MINIKUBE_NODE_IP=$(shell minikube ip)
-ifeq ($(MINIKUBE_NODE_IP),)
-$(error cannot find get minikube node ip address. Ensure that you have minikube environment.)
-endif
+  $(error DAPR_TEST_TAG environment variable must be set)
 endif
 
 define genTestAppImageBuild
@@ -45,11 +41,12 @@ push-e2e-app-$(1): check-e2e-env
 	$(DOCKER) push $(DAPR_TEST_REGISTRY)/e2e-$(1):$(DAPR_TEST_TAG)
 endef
 
-
 # Generate test app image push targets
 $(foreach ITEM,$(E2E_TEST_APPS),$(eval $(call genTestAppImagePush,$(ITEM))))
 
+# Enumerate test app build targets
 BUILD_E2E_APPS_TARGETS:=$(foreach ITEM,$(E2E_TEST_APPS),build-e2e-app-$(ITEM))
+# Enumerate test app push targets
 PUSH_E2E_APPS_TARGETS:=$(foreach ITEM,$(E2E_TEST_APPS),push-e2e-app-$(ITEM))
 
 # build test app image
@@ -59,5 +56,5 @@ build-e2e-app-all: $(BUILD_E2E_APPS_TARGETS)
 push-e2e-app-all: $(PUSH_E2E_APPS_TARGETS)
 
 # start all e2e tests
-test-e2e-all:
+test-e2e-all: check-e2e-env
 	DAPR_TEST_MINIKUBE_IP=$(MINIKUBE_NODE_IP) go test -v -tags=e2e ./tests/e2e/...
