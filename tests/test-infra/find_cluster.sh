887fddd9e1c495c49e9624da917911503722c203
Young Bu Park <youngp@microsoft.com>
2019-11-22 17:58:51 -0800

    use creating namespace

--- tests/test-infra/find_cluster.sh
+++ tests/test-infra/find_cluster.sh
@@ -16,6 +16,9 @@ testclusterpool=(
     "dapr-aks-e2e-04"
 )
 
+# Define max e2e test timeout
+MAX_TEST_TIMEOUT=3600
+
 if [ -z "$DAPR_TEST_RESOURCE_GROUP" ]; then
     DAPR_TEST_RESOURCE_GROUP="dapre2e"
 fi
@@ -35,46 +38,37 @@ for clustername in ${testclusterpool[@]}; do
     # Switch to cluster context
     az aks get-credentials -n $clustername -g $DAPR_TEST_RESOURCE_GROUP
     if [ $? -ne 0 ]; then
-        echo "Failed to switch to $clustername context. Retry to get available test cluster after 10 seconds ..."
-        sleep 10
-        continue
-    fi
-
-    echo "Checking if $KUBE_TEST_NAMESPACE namespace has any running pod ..."
-
-    running_pods=$(kubectl get pods -o=jsonpath='{.items}' -n ${KUBE_TEST_NAMESPACE})
-    if [ $? -ne 0 ]; then
-        echo "Failed to run kubectl. Retry to get available test cluster after 10 seconds."
+        echo "Failed to switch to $clustername context. Retry to get available test cluster after 5 seconds ..."
         sleep 5
         continue
     fi
 
-    # If the namespace is empty, then items array is empty
-    if [ "$running_pods" == "[]" ]; then
-        echo "Found available test cluster: $clustername"
-        exit 0
-    fi
+    # To resolve the racing condition when multiple tests are running,
+    # this script tries to create the namespace. If it is failed, we can assume 
+    # that this cluster is being used by the other tests.
+    echo "Trying to create ${KUBE_TEST_NAMESPACE} namespace..."
+    kubectl create namespace ${KUBE_TEST_NAMESPACE}
 
-    # Get the running time for one of pods in dapr-tests namespace
-    start_datetime=$(kubectl get pods -n ${KUBE_TEST_NAMESPACE} -o=jsonpath='{.items[0].status.startTime}')
+    # Get the running time of dapr-tests namespace
+    start_datetime=$(kubectl get namespace ${KUBE_TEST_NAMESPACE} -o=jsonpath='{.metadata.creationTimestamp}')
     if [ $? -ne 0 ]; then
-        echo "Cannot get startTime of the first pod with kubectl (Test cluster is being cleaned up)."
-        echo "Retry to get available test cluster after 10 seconds."
+        echo "Cannot get creation time of namespace with kubectl (Test cluster is being cleaned up)."
+        echo "Retry to get available test cluster after 5 seconds."
         sleep 5
         continue
     fi
 
     # NOTE: 'date' is GNU version date. Please use gdate on mac os
     # after installing coreutils (brew install coreutils)
-    start_sec=$(date -d "$start_datetime" +%s)
-    now_sec=$(date +%s)
+    start_sec=$(gdate -d "$start_datetime" +%s)
+    now_sec=$(gdate +%s)
     running_time=$((now_sec-start_sec))
 
-    echo "Found that the POD is running for $running_time seconds"
+    echo "Namespace is being used for $running_time seconds"
 
-    # If running time is greater than 7200 seconds (2 hours), it might be because of test failure.
-    # In this case, we can use this cluster after cleaning up the old pods
-    if [ $running_time -gt 100 ]; then
+    # If running time is greater than $MAX_TEST_TIMEOUT, it might be because of test failure.
+    # In this case, we can use this cluster.
+    if [ $running_time -gt $MAX_TEST_TIMEOUT ]; then
         echo "The previous test running in this cluster might be cancelled or failed accidently so use $clustername cluster for e2e test."
         exit 0
     fi
