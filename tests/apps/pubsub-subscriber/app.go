275cedb275b64af9137c4ecf18060e40e67ccfa6
LMWF <lemai@microsoft.com>
2019-12-09 12:18:12 -0800

    pubsub e2e test

---
+++ tests/apps/pubsub-subscriber/app.go
@@ -0,0 +1,178 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	appPort = 3000
+	pubsubA = "pubsub-a-topic"
+	pubsubB = "pubsub-b-topic"
+)
+
+type appResponse struct {
+	Message   string `json:"message,omitempty"`
+	StartTime int    `json:"start_time,omitempty"`
+	EndTime   int    `json:"end_time,omitempty"`
+}
+
+type topicsList struct {
+	Topic []string
+}
+
+type receivedMessagesResponse struct {
+	ReceivedByTopicA []string `json:"pubsub-a-topic"`
+	ReceivedByTopicB []string `json:"pubsub-b-topic"`
+}
+
+var receivedMessagesA []string
+var receivedMessagesB []string
+
+var lock sync.Mutex
+
+// indexHandler is the handler for root path
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("indexHandler is called\n")
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(appResponse{Message: "OK"})
+}
+
+// this handles /dapr/subscribe, which is called from dapr into this app.
+// this returns the list of topics the app is subscribed to.
+func configureSubscribeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("configureSubscribeHandler called\n")
+
+	var t topicsList
+	t.Topic = append(t.Topic, pubsubA, pubsubB)
+	log.Printf("configureSubscribeHandler subscribing to:%v\n", t.Topic)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(t.Topic)
+}
+
+// this handles messages published to "pubsub-a-topic"
+func subscribeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("aHandler is called %s\n", r.URL)
+
+	w.WriteHeader(http.StatusOK)
+	defer r.Body.Close()
+
+	var err error
+	var data []byte
+	var body []byte
+	if r.Body != nil {
+		if data, err = ioutil.ReadAll(r.Body); err == nil {
+			body = data
+			log.Printf("assigned\n")
+		}
+	} else {
+		// error
+		err = errors.New("r.Body is nil")
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(appResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	msg, err := extractMessage(body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(appResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	lock.Lock()
+	defer lock.Unlock()
+	if strings.HasSuffix(r.URL.String(), pubsubA) {
+		receivedMessagesA = append(receivedMessagesA, msg)
+	} else if strings.HasSuffix(r.URL.String(), pubsubB) {
+		receivedMessagesB = append(receivedMessagesB, msg)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(appResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	json.NewEncoder(w).Encode(appResponse{
+		Message: "success",
+	})
+}
+
+func extractMessage(body []byte) (string, error) {
+	log.Printf("extractMessage() called")
+
+	log.Printf("body=%s", string(body))
+
+	m := make(map[string]interface{})
+	err := json.Unmarshal(body, &m)
+	if err != nil {
+		log.Printf("Could not unmarshal, %s", err.Error())
+		return "", err
+	}
+
+	msg := m["data"].(string)
+	log.Printf("output='%s'\n", msg)
+
+	return msg, nil
+}
+
+// the test calls this to get the messages received
+func getReceivedMessages(w http.ResponseWriter, r *http.Request) {
+	log.Println("Enter getReceivedMessages")
+
+	response := receivedMessagesResponse{
+		ReceivedByTopicA: receivedMessagesA,
+		ReceivedByTopicB: receivedMessagesB}
+
+	log.Printf("receivedMessagesResponse=%s", response)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+// appRouter initializes restful api router
+func appRouter() *mux.Router {
+	log.Printf("Enter appRouter()")
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", indexHandler).Methods("GET")
+
+	router.HandleFunc("/tests/get", getReceivedMessages).Methods("POST")
+
+	router.HandleFunc("/dapr/subscribe", configureSubscribeHandler).Methods("GET")
+
+	router.HandleFunc("/"+pubsubA, subscribeHandler).Methods("POST")
+	router.HandleFunc("/"+pubsubB, subscribeHandler).Methods("POST")
+	router.Use(mux.CORSMethodMiddleware(router))
+
+	return router
+}
+
+func main() {
+	log.Printf("Hello Dapr v2 - listening on http://localhost:%d", appPort)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", appPort), appRouter()))
+}
---
+++ tests/e2e/pubsub/pubsub_test.go
@@ -0,0 +1,189 @@
+// +build e2e
+
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package pubsubapp_e2e
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"reflect"
+	"testing"
+
+	
+	"github.com/dapr/dapr/tests/e2e/utils"
+	kube "github.com/dapr/dapr/tests/platforms/kubernetes"
+	"github.com/dapr/dapr/tests/runner"
+	"github.com/stretchr/testify/require"
+)
+
+const numHealthChecks = 3      // Number of get calls before starting tests.
+const testManyEntriesCount = 5 // Anything between 1 and the number above (inclusive).
+
+var tr *runner.TestRunner
+
+const (
+	publisherAppName  = "pubsub-publisher"
+	subscriberAppName = "pubsub-subscriber"
+)
+
+type testCommandRequest struct {
+	Message string `json:"message,omitempty"`
+}
+
+// sent to the publisher app, which will publish data to dapr
+type publishCommand struct {
+	Topic string `json:"topic"`
+	Data  string `json:"data"`
+}
+
+type appResponse struct {
+	Message   string `json:"message,omitempty"`
+	StartTime int    `json:"start_time,omitempty"`
+	EndTime   int    `json:"end_time,omitempty"`
+}
+
+// data returned from the subscriber app
+type receivedMessagesResponse struct {
+	ReceivedByTopicA []string `json:"pubsub-a-topic"`
+	ReceivedByTopicB []string `json:"pubsub-b-topic"`
+}
+
+var receivedMessages []string
+
+// indexHandler is the handler for root path
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("indexHandler is called\n")
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(appResponse{Message: "OK"})
+}
+
+// sends messages to the publisher app.  The publisher app does the actual publish
+func sendToPublisher(t *testing.T, publisherExternalURL string, topic string) ([]string, error) {
+	var sentMessages []string
+	commandBody := publishCommand{Topic: topic}
+	offset := rand.Intn(100)
+	for i := offset; i < offset+100; i++ {
+		commandBody.Data = fmt.Sprintf("message-%d", i)
+
+		sentMessages = append(sentMessages, commandBody.Data)
+		jsonValue, err := json.Marshal(commandBody)
+		require.NoError(t, err)
+	
+		// this is the publish app's endpoint, not a dapr endpoint
+		url := fmt.Sprintf("http://%s/tests/publish", publisherExternalURL)
+
+		// debuggability - trace info about the first message.  don't trace others so it doesn't flood log.
+		if i == offset {
+			log.Printf("Sending first publish app at url %s and body '%s', this log will not print for subsequent messages for same topic", url, jsonValue)
+		}
+
+		postResp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+		require.NoError(t, err)
+		if err != nil {
+			if postResp != nil {
+				log.Printf("Publish failed with error=%s, StatusCode=%d", err.Error(), postResp.StatusCode)
+			} else {
+				log.Printf("Publish failed with error=%s, response is nil", err.Error())
+			}
+
+			return nil, err
+		}
+	}
+
+	return sentMessages, nil
+}
+
+func doIt(t *testing.T, publisherExternalURL string) receivedMessagesResponse {
+	var err error
+	sentTopicAMessages, err := sendToPublisher(t, publisherExternalURL, "pubsub-a-topic")
+	require.NoError(t, err)
+
+	sentTopicBMessages, err := sendToPublisher(t, publisherExternalURL, "pubsub-b-topic")
+	require.NoError(t, err)
+
+	return receivedMessagesResponse{
+		ReceivedByTopicA: sentTopicAMessages,
+		ReceivedByTopicB: sentTopicBMessages}
+}
+
+func getReceivedMessages(t *testing.T, subscriberExternalURL string, sentMessages receivedMessagesResponse) {	
+	// this is the publish app's endpoint, not a dapr endpoint
+	url := fmt.Sprintf("http://%s/tests/get", subscriberExternalURL)
+	log.Printf("Publishing using url %s", url)
+
+	resp, err := utils.HTTPPost(url, nil)
+	require.NoError(t, err)
+
+	var appResp receivedMessagesResponse
+	err = json.Unmarshal(resp, &appResp)
+	require.NoError(t, err)
+
+	log.Printf("subscriber receieved %d messages on pubsub-a-topic and %d on pubsub-b-topic", len(appResp.ReceivedByTopicA), len(appResp.ReceivedByTopicB))
+	require.True(t, reflect.DeepEqual(sentMessages.ReceivedByTopicA, appResp.ReceivedByTopicA))
+	if !reflect.DeepEqual(sentMessages.ReceivedByTopicA, appResp.ReceivedByTopicA) {
+		for i := 0; i < len(sentMessages.ReceivedByTopicA); i++ {
+			log.Printf("%s, %s", sentMessages.ReceivedByTopicA[i], appResp.ReceivedByTopicA[i])
+		}
+	}
+
+	require.True(t, reflect.DeepEqual(sentMessages.ReceivedByTopicB, appResp.ReceivedByTopicB))
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("Enter TestMain")
+	// These apps will be deployed before starting actual test
+	// and will be cleaned up after all tests are finished automatically
+	testApps := []kube.AppDescription{
+		{
+			AppName:        publisherAppName,
+			DaprEnabled:    true,
+			ImageName:      "e2e-pubsub-publisher",
+			Replicas:       1,
+			IngressEnabled: true,
+		},
+		{
+			AppName:        subscriberAppName,
+			DaprEnabled:    true,
+			ImageName:      "e2e-pubsub-subscriber",
+			Replicas:       1,
+			IngressEnabled: true,
+		},
+	}
+
+	log.Printf("Creating TestRunner\n")
+	tr = runner.NewTestRunner("pubsubtest", testApps, nil)
+	log.Printf("Starting TestRunner\n")
+	os.Exit(tr.Start(m))
+}
+
+func TestPubSub(t *testing.T) {	
+	t.Log("Enter TestPubSub")
+	publisherExternalURL := tr.Platform.AcquireAppExternalURL(publisherAppName)
+	require.NotEmpty(t, publisherExternalURL, "publisherExternalURL must not be empty!")
+
+	subscriberExternalURL := tr.Platform.AcquireAppExternalURL(subscriberAppName)
+	require.NotEmpty(t, subscriberExternalURL, "subscriberExternalURL must not be empty!")
+
+	// This initial probe makes the test wait a little bit longer when needed,
+	// making this test less flaky due to delays in the deployment.
+	_, err := utils.HTTPGetNTimes(publisherExternalURL, numHealthChecks)
+	require.NoError(t, err)
+
+	_, err = utils.HTTPGetNTimes(subscriberExternalURL, numHealthChecks)
+	require.NoError(t, err)
+	
+	t.Run("pubsubtest1", func(t *testing.T) {
+		sentMessages := doIt(t, publisherExternalURL)
+		getReceivedMessages(t, subscriberExternalURL, sentMessages)
+	})
+}
