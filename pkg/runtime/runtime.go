56b3e3efd1c9b063615e3dc8af9d105d37be7fc7
Haishi Bai <hbai@microsoft.com>
2019-12-18 16:40:05 -0800

    remove trace statement

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -1060,7 +1060,6 @@ func (a *DaprRuntime) convertMetadataItemsToProperties(items []components_v1alph
 
 func (a *DaprRuntime) getComponent(componentType string, name string) *components_v1alpha1.Component {
 	for _, c := range a.components {
-		fmt.Printf("COMPONENT TYPE: %s, NAME: %s\n", c.Spec.Type, c.ObjectMeta.Name)
 		if c.Spec.Type == componentType && c.ObjectMeta.Name == name {
 			return &c
 		}
