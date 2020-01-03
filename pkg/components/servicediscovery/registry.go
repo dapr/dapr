8a1e5dd3a4b8153bb8a7dc01d6e6ce2325b86138
Phil Kedy <phil.kedy@gmail.com>
2020-01-03 11:45:44 -0500

    Refactoring how components are registered to allow for customized daprd builds with specific contrib components as well as custom components.

--- pkg/components/servicediscovery/registry.go
+++ pkg/components/servicediscovery/registry.go
@@ -7,42 +7,58 @@ package servicediscovery
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/dapr/components-contrib/servicediscovery"
 )
 
-type Registry interface {
-	CreateResolver(name string) (servicediscovery.Resolver, error)
+type (
+	// ServiceDiscovery is a service discovery component definition.
+	ServiceDiscovery struct {
+		Name          string
+		FactoryMethod func() servicediscovery.Resolver
+	}
+
+	// Registry handles registering and creating service discovery components.
+	Registry interface {
+		Register(components ...ServiceDiscovery)
+		Create(name string) (servicediscovery.Resolver, error)
+	}
+
+	serviceDiscoveryRegistry struct {
+		resolvers map[string]func() servicediscovery.Resolver
+	}
+)
+
+// New creates a ServiceDiscovery.
+func New(name string, factoryMethod func() servicediscovery.Resolver) ServiceDiscovery {
+	return ServiceDiscovery{
+		Name:          name,
+		FactoryMethod: factoryMethod,
+	}
 }
 
-type serviceDiscoveryRegistry struct {
-	resolvers map[string]func() servicediscovery.Resolver
-}
-
-var instance *serviceDiscoveryRegistry
-var once sync.Once
-
+// NewRegistry creates a servce discovery registry.
 func NewRegistry() Registry {
-	once.Do(func() {
-		instance = &serviceDiscoveryRegistry{
-			resolvers: map[string]func() servicediscovery.Resolver{},
-		}
-	})
-	return instance
+	return &serviceDiscoveryRegistry{
+		resolvers: map[string]func() servicediscovery.Resolver{},
+	}
 }
 
-func RegisterServiceDiscovery(name string, factoryMethod func() servicediscovery.Resolver) {
-	instance.resolvers[formatName(name)] = factoryMethod
+// Register adds one or many service discovery components to the registry.
+func (s *serviceDiscoveryRegistry) Register(components ...ServiceDiscovery) {
+	for _, component := range components {
+		s.resolvers[createFullName(component.Name)] = component.FactoryMethod
+	}
 }
 
-func (s *serviceDiscoveryRegistry) CreateResolver(name string) (servicediscovery.Resolver, error) {
-	if method, ok := s.resolvers[formatName(name)]; ok {
+// Create instantiates a service discovery resolver based on `name`.
+func (s *serviceDiscoveryRegistry) Create(name string) (servicediscovery.Resolver, error) {
+	if method, ok := s.resolvers[createFullName(name)]; ok {
 		return method(), nil
 	}
 	return nil, fmt.Errorf("couldn't find service resolver %s", name)
 }
 
-func formatName(s string) string {
-	return fmt.Sprintf("servicediscovery.%s", s)
+func createFullName(name string) string {
+	return fmt.Sprintf("servicediscovery.%s", name)
 }
