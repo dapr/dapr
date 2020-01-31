c5ac112253968bb37bb89e2156e27487c98b73b7
Joni Collinge <jonathancollinge@live.com>
2020-01-31 05:59:53 +0000

    Added a Middleware to add a Forwarded header (#951)
    * Added handle to add forwarded header
    
    * fixed import order
    
    * refactored middleware construction
    
    * Added X-Forwarded-[For|Host|Proto] headers
    
    * fixed linting issues
    
    Co-authored-by: Yaron Schneider <yaronsc@microsoft.com>
    Co-authored-by: Young Bu Park <youngp@microsoft.com>

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -10,11 +10,11 @@ import (
 	"strings"
 
 	cors "github.com/AdhityaRamadhanus/fasthttpcors"
-	log "github.com/sirupsen/logrus"
 	"github.com/dapr/dapr/pkg/config"
 	diag "github.com/dapr/dapr/pkg/diagnostics"
 	http_middleware "github.com/dapr/dapr/pkg/middleware/http"
 	routing "github.com/qiangxue/fasthttp-routing"
+	log "github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
 	"github.com/valyala/fasthttp/pprofhandler"
 )
@@ -43,19 +43,18 @@ func NewServer(api API, config ServerConfig, tracingSpec config.TracingSpec, pip
 
 // StartNonBlocking starts a new server in a goroutine
 func (s *server) StartNonBlocking() {
-	endpoints := s.api.APIEndpoints()
-	router := s.getRouter(endpoints)
-	origins := strings.Split(s.config.AllowedOrigins, ",")
-	corsHandler := s.getCorsHandler(origins)
-	handler := s.pipeline.Apply(router.HandleRequest)
+	handler :=
+		s.useProxy(
+			s.useCors(
+				s.useComponents(
+					s.useRouter())))
+
+	if s.tracingSpec.Enabled {
+		handler = s.useTracing(handler)
+	}
+
 	go func() {
-		if s.tracingSpec.Enabled {
-			log.Fatal(fasthttp.ListenAndServe(fmt.Sprintf(":%v", s.config.Port),
-				s.pipeline.Apply(diag.TracingHTTPMiddleware(s.tracingSpec, corsHandler.CorsMiddleware(handler)))))
-		} else {
-			log.Fatal(fasthttp.ListenAndServe(fmt.Sprintf(":%v", s.config.Port),
-				s.pipeline.Apply(corsHandler.CorsMiddleware(handler))))
-		}
+		log.Fatal(fasthttp.ListenAndServe(fmt.Sprintf(":%v", s.config.Port), handler))
 	}()
 
 	if s.config.EnableProfiling {
@@ -63,6 +62,51 @@ func (s *server) StartNonBlocking() {
 			log.Infof("starting profiling server on port %v", s.config.ProfilePort)
 			log.Fatal(fasthttp.ListenAndServe(fmt.Sprintf(":%v", s.config.ProfilePort), pprofhandler.PprofHandler))
 		}()
+	}
+}
+
+func (s *server) useTracing(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return diag.TracingHTTPMiddleware(s.tracingSpec, next)
+}
+
+func (s *server) useRouter() fasthttp.RequestHandler {
+	endpoints := s.api.APIEndpoints()
+	router := s.getRouter(endpoints)
+	return router.HandleRequest
+}
+
+func (s *server) useComponents(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return s.pipeline.Apply(next)
+}
+
+func (s *server) useCors(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	origins := strings.Split(s.config.AllowedOrigins, ",")
+	corsHandler := s.getCorsHandler(origins)
+	return corsHandler.CorsMiddleware(next)
+}
+
+func (s *server) useProxy(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		var proto string
+		if ctx.IsTLS() {
+			proto = "https"
+		} else {
+			proto = "http"
+		}
+		// Add Forwarded header: https://tools.ietf.org/html/rfc7239
+		ctx.Request.Header.Add("Forwarded",
+			fmt.Sprintf("by=%s;for=%s;host=%s;proto=%s",
+				ctx.LocalAddr(),
+				ctx.RemoteAddr(),
+				ctx.Host(),
+				proto))
+		// Add X-Forwarded-For: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
+		ctx.Request.Header.Add("X-Forwarded-For", ctx.RemoteAddr().String())
+		// Add X-Forwarded-Host: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Host
+		ctx.Request.Header.Add("X-Forwarded-Host", fmt.Sprintf("%s", ctx.Host()))
+		// Add X-Forwarded-Proto: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Proto
+		ctx.Request.Header.Add("X-Forwarded-", proto)
+		next(ctx)
 	}
 }
 
