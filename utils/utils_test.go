30cba6f42eb3f4b1244e5b8309892954e5cd9c07
Young Bu Park <youngp@microsoft.com>
2020-02-22 18:03:24 -0800

    fix test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -21,18 +21,18 @@ func TestToISO8601DateTimeString(t *testing.T) {
 	})
 
 	t.Run("succeed to parse generated iso8601 string to time.Time using RFC3339 Parser", func(t *testing.T) {
-		currentTime := time.Now().UTC()
+		currentTime := time.Now()
 		isoString := ToISO8601DateTimeString(currentTime)
 		parsed, err := time.Parse(time.RFC3339, isoString)
 
 		// assert
 		assert.NoError(t, err)
-		assert.Equal(t, currentTime.Year(), parsed.Year())
-		assert.Equal(t, currentTime.Month(), parsed.Month())
-		assert.Equal(t, currentTime.Day(), parsed.Day())
-		assert.Equal(t, currentTime.Hour(), parsed.Hour())
-		assert.Equal(t, currentTime.Minute(), parsed.Minute())
-		assert.Equal(t, currentTime.Second(), parsed.Second())
-		assert.Equal(t, currentTime.Nanosecond()/1000, parsed.Nanosecond()/1000)
+		assert.Equal(t, currentTime.UTC().Year(), parsed.Year())
+		assert.Equal(t, currentTime.UTC().Month(), parsed.Month())
+		assert.Equal(t, currentTime.UTC().Day(), parsed.Day())
+		assert.Equal(t, currentTime.UTC().Hour(), parsed.Hour())
+		assert.Equal(t, currentTime.UTC().Minute(), parsed.Minute())
+		assert.Equal(t, currentTime.UTC().Second(), parsed.Second())
+		assert.Equal(t, currentTime.UTC().Nanosecond()/1000, parsed.Nanosecond()/1000)
 	})
 }
