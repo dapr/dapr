46ba8b814fb4992b72e753623dc96d90ba2e67ed
Young Bu Park <youngp@microsoft.com>
2020-02-25 12:48:42 -0800

    make log same

--- pkg/logger/dapr_logger.go
+++ pkg/logger/dapr_logger.go
@@ -13,8 +13,6 @@ import (
 type daprLogger struct {
 	// name is the name of logger that is published to log as a scope
 	name string
-	// jsonFormatEnabled is a flag to turn on JSON formatted log
-	jsonFormatEnabled bool
 	// loger is the instance of logrus logger
 	logger *logrus.Entry
 }
@@ -23,8 +21,7 @@ func newDaprLogger(name string) *daprLogger {
 	newLogger := logrus.New()
 
 	dl := &daprLogger{
-		name:              name,
-		jsonFormatEnabled: defaultJSONOutput,
+		name: name,
 		logger: newLogger.WithFields(logrus.Fields{
 			logFieldScope: name,
 			logFieldType:  LogTypeLog,
@@ -40,7 +37,6 @@ func newDaprLogger(name string) *daprLogger {
 func (l *daprLogger) EnableJSONOutput(enabled bool) {
 	var formatter logrus.Formatter
 
-	l.jsonFormatEnabled = enabled
 	fieldMap := logrus.FieldMap{
 		// If time field name is conflicted, logrus adds "fields." prefix.
 		// So rename to unused field @time to avoid the confliction.
@@ -49,16 +45,15 @@ func (l *daprLogger) EnableJSONOutput(enabled bool) {
 		logrus.FieldKeyMsg:   logFieldMessage,
 	}
 
+	hostname, _ := os.Hostname()
 	l.logger.Data = logrus.Fields{
-		logFieldScope: l.logger.Data[logFieldScope],
-		logFieldType:  LogTypeLog,
+		logFieldScope:    l.logger.Data[logFieldScope],
+		logFieldType:     LogTypeLog,
+		logFieldInstance: hostname,
+		logFieldDaprVer:  version.Version(),
 	}
 
 	if enabled {
-		hostname, _ := os.Hostname()
-		l.logger.Data[logFieldInstance] = hostname
-		l.logger.Data[logFieldDaprVer] = version.Version()
-
 		formatter = &logrus.JSONFormatter{
 			DisableTimestamp: true,
 			FieldMap:         fieldMap,
@@ -75,9 +70,7 @@ func (l *daprLogger) EnableJSONOutput(enabled bool) {
 
 // SetAppID sets app_id field in log. Default value is empty string
 func (l *daprLogger) SetAppID(id string) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldAppID] = id
-	}
+	l.logger.Data[logFieldAppID] = id
 }
 
 func toLogrusLevel(lvl LogLevel) logrus.Level {
@@ -94,88 +87,67 @@ func (l *daprLogger) SetOutputLevel(outputLevel LogLevel) {
 // WithLogType specify the log_type field in log. Default value is LogTypeLog
 func (l *daprLogger) WithLogType(logType string) Logger {
 	return &daprLogger{
-		name:              l.name,
-		jsonFormatEnabled: l.jsonFormatEnabled,
-		logger:            l.logger.WithField(logFieldType, logType),
+		name:   l.name,
+		logger: l.logger.WithField(logFieldType, logType),
 	}
 }
 
 // Info logs a message at level Info.
 func (l *daprLogger) Info(args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Log(logrus.InfoLevel, args...)
 }
 
 // Infof logs a message at level Info.
 func (l *daprLogger) Infof(format string, args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Logf(logrus.InfoLevel, format, args...)
 }
 
 // Debug logs a message at level Debug.
 func (l *daprLogger) Debug(args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Log(logrus.DebugLevel, args...)
 }
 
 // Debugf logs a message at level Debug.
 func (l *daprLogger) Debugf(format string, args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Logf(logrus.DebugLevel, format, args...)
 }
 
 // Warn logs a message at level Warn.
 func (l *daprLogger) Warn(args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Log(logrus.WarnLevel, args...)
 }
 
 // Warnf logs a message at level Warn.
 func (l *daprLogger) Warnf(format string, args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Logf(logrus.WarnLevel, format, args...)
 }
 
 // Error logs a message at level Error.
 func (l *daprLogger) Error(args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Log(logrus.ErrorLevel, args...)
 }
 
 // Errorf logs a message at level Error.
 func (l *daprLogger) Errorf(format string, args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Logf(logrus.ErrorLevel, format, args...)
 }
 
 // Fatal logs a message at level Fatal then the process will exit with status set to 1.
 func (l *daprLogger) Fatal(args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Fatal(args...)
 }
 
 // Fatalf logs a message at level Fatal then the process will exit with status set to 1.
 func (l *daprLogger) Fatalf(format string, args ...interface{}) {
-	if l.jsonFormatEnabled {
-		l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
-	}
+	l.logger.Data[logFieldTimeStamp] = utils.ToISO8601DateTimeString(time.Now())
 	l.logger.Fatalf(format, args...)
 }
