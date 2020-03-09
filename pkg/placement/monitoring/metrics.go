aff444a226f5c0c15dcc2d6e242a60430573f118
Shalabh Mohan Shrivastava <shalabhs@microsoft.com>
2020-03-09 12:54:16 -0700

    Adding additional metrics in placement service (#1211)
    * Adding additional metrics in placement service
    
    * Changing metrics names to lowercase
    
    * -resolving code review comments
    
    * Resolving code review comments - on metrics naming
    
    * fixing method name

--- pkg/placement/monitoring/metrics.go
+++ pkg/placement/monitoring/metrics.go
@@ -18,11 +18,31 @@ var (
 		"placement/hosts_total",
 		"The total number of active hosts reported to placement service.",
 		stats.UnitDimensionless)
+	actorTypesTotal = stats.Int64(
+		"placement/actortypes_total",
+		"The total number of actor types reported to placement service.",
+		stats.UnitDimensionless)
+	nonActorHostsTotal = stats.Int64(
+		"placement/nonactorhosts_total",
+		"The total number of non actor hosts reported to placement service.",
+		stats.UnitDimensionless)
+
+	nilKey = []tag.Key{}
 )
 
 // RecordActiveHostsCount records the number of active hosts
 func RecordActiveHostsCount(count int) {
 	stats.Record(context.Background(), activeHostsTotal.M(int64(count)))
+}
+
+// RecordActorTypesCount records the number of active actor types
+func RecordActorTypesCount(count int) {
+	stats.Record(context.Background(), actorTypesTotal.M(int64(count)))
+}
+
+// RecordNonActorHostsCount records the number of active non actor hosts
+func RecordNonActorHostsCount(count int) {
+	stats.Record(context.Background(), nonActorHostsTotal.M(int64(count)))
 }
 
 func newView(measure stats.Measure, keys []tag.Key, aggregation *view.Aggregation) *view.View {
@@ -38,7 +58,9 @@ func newView(measure stats.Measure, keys []tag.Key, aggregation *view.Aggregatio
 // InitMetrics initialize the placement service metrics
 func InitMetrics() error {
 	err := view.Register(
-		newView(activeHostsTotal, []tag.Key{}, view.LastValue()),
+		newView(activeHostsTotal, nilKey, view.LastValue()),
+		newView(actorTypesTotal, nilKey, view.LastValue()),
+		newView(nonActorHostsTotal, nilKey, view.LastValue()),
 	)
 
 	return err
