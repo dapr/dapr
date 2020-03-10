0d1068795b1c5a57b5f1ff97ec05a6a6c4248553
Young Bu Park <youngp@microsoft.com>
2020-03-09 17:40:15 -0700

    Add metrics to http client of appchannel (#1217)
    * Add metrics to http client of appchannel
    
    * fix build
    
    * update readme

--- pkg/channel/http/http_channel.go
+++ pkg/channel/http/http_channel.go
@@ -6,8 +6,10 @@
 package http
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -37,6 +39,8 @@ const (
 	// ContentType is the header for Content-Type
 	ContentType        = "Content-Type"
 	defaultContentType = "application/json"
+
+	httpInternalErrorCode = "500"
 )
 
 // Channel is an HTTP implementation of an AppChannel
@@ -103,12 +107,25 @@ func (h *Channel) InvokeMethod(invokeRequest *channel.InvokeRequest) (*channel.I
 		h.ch <- 1
 	}
 
+	// TODO: Use propagated context
+	ctx := context.Background()
+	// Emit metric when request is sent
+	diag.DefaultHTTPMonitoring.ClientRequestStarted(
+		ctx, method, invokeRequest.Method,
+		int64(len(invokeRequest.Payload)))
+
+	startRequest := time.Now()
 	err := h.client.Do(req, resp)
+	elapsedMs := float64(time.Since(startRequest) / time.Millisecond)
 
 	if h.ch != nil {
 		<-h.ch
 	}
 	if err != nil {
+		// Track failure request
+		diag.DefaultHTTPMonitoring.ClientRequestCompleted(
+			ctx, method, invokeRequest.Method,
+			httpInternalErrorCode, 0, elapsedMs)
 		return nil, err
 	}
 
@@ -117,6 +134,12 @@ func (h *Channel) InvokeMethod(invokeRequest *channel.InvokeRequest) (*channel.I
 	copy(arr, body)
 
 	statusCode := resp.StatusCode()
+
+	// Emit metric when requst is completed
+	diag.DefaultHTTPMonitoring.ClientRequestCompleted(
+		ctx, method, invokeRequest.Method,
+		strconv.Itoa(statusCode), int64(len(body)), elapsedMs)
+
 	headers := []string{}
 	resp.Header.VisitAll(func(key []byte, value []byte) {
 		headers = append(headers, fmt.Sprintf("%s&__header_equals__&%s", string(key), string(value)))
