040196a10ab55c157fa514a0b971b2e533965438
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2020-04-13 13:47:57 -0700

    changing the default sampling rate to 1e-4

--- pkg/diagnostics/utils/trace_utils.go
+++ pkg/diagnostics/utils/trace_utils.go
@@ -10,14 +10,14 @@ import (
 )
 
 const (
-	defaultTraceSamplingRate = 0.001
+	defaultSamplingRate = 1e-4
 )
 
 // GetTraceSamplingRate parses the given rate and returns the parsed rate
 func GetTraceSamplingRate(rate string) float64 {
 	f, err := strconv.ParseFloat(rate, 64)
 	if err != nil {
-		return defaultTraceSamplingRate
+		return defaultSamplingRate
 	}
 	return f
 }
