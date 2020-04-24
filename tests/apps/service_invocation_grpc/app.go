3eea8624e417c0c6e22fe8f8bccca778b4a15d01
Young Bu Park <youngp@microsoft.com>
2020-04-24 14:09:26 -0700

    clean up e2e tests

--- tests/apps/service_invocation_grpc/app.go
+++ tests/apps/service_invocation_grpc/app.go
@@ -17,8 +17,8 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/empty"
 
-	commonv1pb "app/proto/common/v1"
-	pb "app/proto/daprclient/v1"
+	commonv1pb "github.com/dapr/dapr/pkg/proto/common/v1"
+	pb "github.com/dapr/dapr/pkg/proto/daprclient/v1"
 
 	"google.golang.org/grpc"
 )
