6683bafe92c1655eee6a5967003c0fe110aebbaf
Sashi Kumar <ksashikumark93@gmail.com>
2020-05-11 23:57:15 +0200

    Set Metadata option to get state store API

--- pkg/http/api.go
+++ pkg/http/api.go
@@ -324,6 +324,8 @@ func (a *api) onGetState(reqCtx *fasthttp.RequestCtx) {
 		return
 	}
 
+	metadata := getMetadataFromRequest(reqCtx)
+
 	key := reqCtx.UserValue(stateKeyParam).(string)
 	consistency := string(reqCtx.QueryArgs().Peek(consistencyParam))
 	req := state.GetRequest{
@@ -331,6 +333,7 @@ func (a *api) onGetState(reqCtx *fasthttp.RequestCtx) {
 		Options: state.GetStateOption{
 			Consistency: consistency,
 		},
+		Metadata: metadata,
 	}
 
 	resp, err := a.stateStores[storeName].Get(&req)
@@ -417,15 +420,7 @@ func (a *api) onGetSecret(reqCtx *fasthttp.RequestCtx) {
 		return
 	}
 
-	metadata := map[string]string{}
-	const metadataPrefix string = "metadata."
-	reqCtx.QueryArgs().VisitAll(func(key []byte, value []byte) {
-		queryKey := string(key)
-		if strings.HasPrefix(queryKey, metadataPrefix) {
-			k := strings.TrimPrefix(queryKey, metadataPrefix)
-			metadata[k] = string(value)
-		}
-	})
+	metadata := getMetadataFromRequest(reqCtx)
 
 	key := reqCtx.UserValue(secretNameParam).(string)
 	req := secretstores.GetSecretRequest{
@@ -1004,4 +999,18 @@ func (a *api) onGetHealthz(reqCtx *fasthttp.RequestCtx) {
 	} else {
 		respondEmpty(reqCtx, 200)
 	}
+}
+
+func getMetadataFromRequest(reqCtx *fasthttp.RequestCtx) map[string]string {
+	metadata := map[string]string{}
+	const metadataPrefix string = "metadata."
+	reqCtx.QueryArgs().VisitAll(func(key []byte, value []byte) {
+		queryKey := string(key)
+		if strings.HasPrefix(queryKey, metadataPrefix) {
+			k := strings.TrimPrefix(queryKey, metadataPrefix)
+			metadata[k] = string(value)
+		}
+	})
+
+	return metadata
 }
