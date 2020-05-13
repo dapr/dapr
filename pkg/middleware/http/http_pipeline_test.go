40e9401ded76446226dd51a0fbff36115ee3c02f
jjcollinge <jonathancollinge@live.com>
2020-05-13 15:34:37 +0100

    added selector parsing test

---
+++ pkg/middleware/http/http_pipeline_test.go
@@ -0,0 +1,122 @@
+package http
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSelectorRule(t *testing.T) {
+	testCases := []struct {
+		expression string
+		rule selectorRule
+	}{
+		{
+			expression: "path=test,test1,test2;method=get,post,delete;version=v1.0",
+		  rule: selectorRule{
+				Paths: []string{
+					"test",
+					"test1",
+					"test2",
+				},
+				Methods: []string{
+					"get",
+					"post",
+					"delete",
+				},
+				Versions: []string{
+					"v1.0",
+				},
+			},
+		},
+		{
+			expression: "method=get;path=test,test1;",
+		  rule: selectorRule{
+				Paths: []string{
+					"test",
+					"test1",
+				},
+				Methods: []string{
+					"get",
+				},
+				Versions: nil,
+			},
+		},
+		{
+			expression: "version=v1.0,v2.0;method=get,post,options;",
+		  rule: selectorRule{
+				Paths: nil,
+				Methods: []string{
+					"get",
+					"post",
+					"options",
+				},
+				Versions: []string{
+					"v1.0",
+					"v2.0",
+				},
+			},
+		},
+		{
+			expression: "method=get;path=test,test1;",
+		  rule: selectorRule{
+				Paths: []string{
+					"test",
+					"test1",
+				},
+				Methods: []string{
+					"get",
+				},
+				Versions: nil,
+			},
+		},
+		{
+			expression: "path=test1",
+		  rule: selectorRule{
+				Paths: []string{
+					"test1",
+				},
+				Methods: nil,
+				Versions: nil,
+			},
+		},
+		{
+			expression: "method=get,post,path=badexp",
+		  rule: selectorRule{
+				Paths: nil,
+				Methods: nil,
+				Versions: nil,
+			},
+		},
+		{
+			expression: "path;=method=version;=get",
+		  rule: selectorRule{
+				Paths: nil,
+				Methods: nil,
+				Versions: nil,
+			},
+		},
+		{
+			expression: "",
+		  rule: selectorRule{
+				Paths: nil,
+				Methods: nil,
+				Versions: nil,
+			},
+		},
+		{
+			expression: "12345",
+		  rule: selectorRule{
+				Paths: nil,
+				Methods: nil,
+				Versions: nil,
+			},
+		},
+	}
+
+	for _, tt := range testCases {
+		newRule := newSelectorRule(tt.expression)
+		assert.True(t, reflect.DeepEqual(tt.rule, *newRule))
+	}
+}
