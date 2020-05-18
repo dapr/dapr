8b1668ff6fe8cc225c4d4f4b597dadbe308e05d7
jjcollinge <jonathancollinge@live.com>
2020-05-18 12:11:07 +0100

    updated mdns registration

--- pkg/discovery/mdns.go
+++ pkg/discovery/mdns.go
@@ -6,12 +6,9 @@
 package discovery
 
 import (
-	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/dapr/dapr/pkg/logger"
 	"github.com/grandcat/zeroconf"
@@ -20,13 +17,20 @@ import (
 var log = logger.NewLogger("dapr.runtime.discovery")
 
 // RegisterMDNS uses mdns to publish an entry of the service to a local network
-func RegisterMDNS(id string, host string, port int) error {
+func RegisterMDNS(id string, host string, ips []string, port int) error {
 	go func() {
 		if host == "" {
 			host, _ = os.Hostname()
 		}
 		info := []string{id}
-		server, err := zeroconf.Register(host, id, "local.", port, info, nil)
+		var server *zeroconf.Server
+		var err error
+		domain := "local."
+		if len(ips) == 0 {
+			server, err = zeroconf.Register(host, id, domain, port, info, nil)
+		} else {
+			server, err = zeroconf.RegisterProxy(host, id, domain, port, host, ips, info, nil)
+		}
 		if err != nil {
 			log.Errorf("error from zeroconf register: %s", err)
 			return
@@ -38,40 +42,4 @@ func RegisterMDNS(id string, host string, port int) error {
 		<-sig
 	}()
 	return nil
-}
-
-// LookupPortMDNS uses mdns to find the port of a given service entry on a local network
-func LookupPortMDNS(id string) (int, error) {
-	resolver, err := zeroconf.NewResolver(nil)
-	if err != nil {
-		log.Fatalf("Failed to initialize resolver: %v", err.Error())
-	}
-
-	port := -1
-	entries := make(chan *zeroconf.ServiceEntry)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
-	defer cancel()
-
-	go func(results <-chan *zeroconf.ServiceEntry) {
-		for entry := range results {
-			for _, text := range entry.Text {
-				if text == id {
-					port = entry.Port
-					cancel()
-					return
-				}
-			}
-		}
-	}(entries)
-
-	err = resolver.Browse(ctx, id, "local.", entries)
-	if err != nil {
-		return -1, fmt.Errorf("failed to browse: %s", err.Error())
-	}
-
-	<-ctx.Done()
-	if port == -1 {
-		return port, fmt.Errorf("couldn't find service: %s", id)
-	}
-	return port, nil
 }
