fa8b7c88c7f8f78c765176d36ea104c38e100678
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2020-06-16 14:55:30 -0700

    Update dapr dependencies in e2e test apps whenever proto has breaking changes

--- dapr/README.md
+++ dapr/README.md
@@ -31,3 +31,12 @@ protoc -I . ./dapr/proto/common/v1/*.proto --go_out=plugins=grpc:../../../
 protoc -I . ./dapr/proto/runtime/v1/*.proto --go_out=plugins=grpc:../../../
 protoc -I . ./dapr/proto/internals/v1/*.proto --go_out=plugins=grpc:../../../
 ```
+
+## Update e2e test apps
+Whenever there are breaking changes in the proto files, we need to update the e2e test apps to use the correct version of dapr dependencies. This can be done by navigating to the tests folder and running the commands:-
+```
+go build update_testapps_dependencies.go
+./update_testapps_dependencies.go
+```
+
+Check in all the go.mod files for the test apps that have now been modified to point to the latest dapr version.
---
+++ tests/update_testapps_dependencies.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	var files []string
+
+	root := "./apps"
+	_ = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			panic(err)
+		}
+		if info.IsDir() {
+
+			_, err := os.Stat(path + "/go.mod")
+			if !os.IsNotExist(err) {
+				files = append(files, path)
+			}
+		}
+		return nil
+	})
+
+	testsPath, _ := os.Getwd()
+	goExecutable, _ := exec.LookPath("go")
+	for _, file := range files {
+		os.Chdir(file)
+		_, err := RunCmdAndWait(goExecutable, "get", "-u", "github.com/dapr/dapr@master")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Successfully modified dapr dependencies for: ", file)
+		// Go back to the apps dir
+		os.Chdir(testsPath)
+	}
+}
+
+// RunCmdAndWait runs and waits for a shell command to complete
+func RunCmdAndWait(name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		return "", err
+	}
+	errB, err := ioutil.ReadAll(stderr)
+	if err != nil {
+		return "", nil
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		// in case of error, capture the exact message
+		if len(errB) > 0 {
+			return "", errors.New(string(errB))
+		}
+		return "", err
+	}
+
+	return string(resp), nil
+}
