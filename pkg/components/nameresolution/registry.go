381471aabbe989ed854ecd2011fbadccf5c60a0d
Young Bu Park <youngp@microsoft.com>
2020-06-24 14:22:42 -0700

    change servicediscovery pkg to nameresolution pkg and remove mdns code (#1713)
    * change to nameresolution pkg
    
    * add comment
    
    * remove duplicated import

---
+++ pkg/components/nameresolution/registry.go
@@ -0,0 +1,64 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package nameresolution
+
+import (
+	"fmt"
+
+	nr "github.com/dapr/components-contrib/nameresolution"
+)
+
+type (
+	// NameResolution is a name resolution component definition.
+	NameResolution struct {
+		Name          string
+		FactoryMethod func() nr.Resolver
+	}
+
+	// Registry handles registering and creating name resolution components.
+	Registry interface {
+		Register(components ...NameResolution)
+		Create(name string) (nr.Resolver, error)
+	}
+
+	nameResolutionRegistry struct {
+		resolvers map[string]func() nr.Resolver
+	}
+)
+
+// New creates a NameResolution.
+func New(name string, factoryMethod func() nr.Resolver) NameResolution {
+	return NameResolution{
+		Name:          name,
+		FactoryMethod: factoryMethod,
+	}
+}
+
+// NewRegistry creates a name resolution registry.
+func NewRegistry() Registry {
+	return &nameResolutionRegistry{
+		resolvers: map[string]func() nr.Resolver{},
+	}
+}
+
+// Register adds one or many name resolution components to the registry.
+func (s *nameResolutionRegistry) Register(components ...NameResolution) {
+	for _, component := range components {
+		s.resolvers[createFullName(component.Name)] = component.FactoryMethod
+	}
+}
+
+// Create instantiates a name resolution resolver based on `name`.
+func (s *nameResolutionRegistry) Create(name string) (nr.Resolver, error) {
+	if method, ok := s.resolvers[createFullName(name)]; ok {
+		return method(), nil
+	}
+	return nil, fmt.Errorf("couldn't find name resolver %s", name)
+}
+
+func createFullName(name string) string {
+	return fmt.Sprintf("nameresolution.%s", name)
+}
