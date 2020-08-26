a6a52ddc42449da8b3091f1570aa7f1621df2f4d
Shailesh Suryawanshi <ss.shailesh28@gmail.com>
2020-08-26 01:56:42 -0400

    pkg/operator:removed errors for github/pkg/errors
    removed erros.New() method in favour of github.com/pkg/errors

--- pkg/operator/client/client.go
+++ pkg/operator/client/client.go
@@ -2,14 +2,13 @@ package client
 
 import (
 	"crypto/x509"
-	"errors"
 
 	dapr_credentials "github.com/dapr/dapr/pkg/credentials"
 	diag "github.com/dapr/dapr/pkg/diagnostics"
 	operatorv1pb "github.com/dapr/dapr/pkg/proto/operator/v1"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
-	pkgerror "github.com/pkg/errors"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
@@ -32,12 +31,12 @@ func GetOperatorClient(address, serverName string, certChain *dapr_credentials.C
 		cp := x509.NewCertPool()
 		ok := cp.AppendCertsFromPEM(certChain.RootCA)
 		if !ok {
-			return nil, nil, pkgerror.Wrap(errors.New("failed to append PEM root cert to x509 CertPool"), "err")
+			return nil, nil, errors.New("failed to append PEM root cert to x509 CertPool")
 		}
 
 		config, err := dapr_credentials.TLSConfigFromCertAndKey(certChain.Cert, certChain.Key, serverName, cp)
 		if err != nil {
-			return nil, nil, pkgerror.Wrap(err, "failed to create tls config from cert and key")
+			return nil, nil, errors.Wrap(err, "failed to create tls config from cert and key")
 		}
 		opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(config)))
 	} else {
