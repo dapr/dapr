1d07f158701242e56a45c9c1664f93c034d96753
Artur Souza <artur.barbalho@outlook.com>
2020-09-11 17:04:53 -0700

    Fixes JSON for bulk get response.

--- pkg/http/responses.go
+++ pkg/http/responses.go
@@ -20,9 +20,9 @@ const (
 // BulkGetResponse is the response object for a state bulk get operation
 type BulkGetResponse struct {
 	Key   string              `json:"key"`
-	Data  jsoniter.RawMessage `json:"data"`
-	ETag  string              `json:"etag"`
-	Error string              `json:"error"`
+	Data  jsoniter.RawMessage `json:"data,omitempty"`
+	ETag  string              `json:"etag,omitempty"`
+	Error string              `json:"error,omitempty"`
 }
 
 // respondWithJSON overrides the content-type with application/json
