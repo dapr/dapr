42a70722691e2e8705d7296e1c724579910dc612
Young Bu Park <youngp@microsoft.com>
2020-09-15 17:23:30 -0700

    Initial commit of actor load test app (#2029)
    * initial actor load test
    
    * refactoring
    
    * wip
    
    * add makefile
    
    * change log format
    
    * add k8s deployment yamls
    
    * add metrics for load request
    
    * Revert "enable placement lock when updating tables (#1851)"
    
    This reverts commit 6986943d6ecd1697b14e1f25c517dc1e00d2cf9c.
    
    * add gitignore.
    
    * Revert "Revert "enable placement lock when updating tables (#1851)""
    
    This reverts commit b7c5c0df5fbd327fe0b5df4658e6fcc3cc674986.
    
    * add license.
    
    * ren
    
    Co-authored-by: Aman Bhardwaj <amanbha@users.noreply.github.com>

---
+++ tests/apps/actorload/pkg/actor/client/http/client.go
@@ -0,0 +1,153 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package http
+
+import (
+	cl "actorload/pkg/actor/client"
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	apiVersion      = "v1.0"
+	defaultHTTPPort = "3500"
+	defaultHost     = "127.0.0.1"
+)
+
+type DaprActorClientError struct {
+	Code    int
+	Message string
+}
+
+func (e *DaprActorClientError) Error() string {
+	return fmt.Sprintf("%d %s", e.Code, e.Message)
+}
+
+type httpClient struct {
+	address string
+	client  http.Client
+}
+
+type TransactionalRequest struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type TransactionalStateOperation struct {
+	Operation string               `json:"operation"`
+	Request   TransactionalRequest `json:"request"`
+}
+
+func NewClient() cl.ActorClient {
+	port := os.Getenv("DAPR_HTTP_PORT")
+	if port == "" {
+		port = defaultHTTPPort
+	}
+	return &httpClient{
+		address: fmt.Sprintf("http://%s:%s", defaultHost, port),
+		client:  http.Client{},
+	}
+}
+
+func (c *httpClient) defaultURL(actorType, actorID string) string {
+	return fmt.Sprintf("%s/%s/actors/%s/%s", c.address, apiVersion, actorType, actorID)
+}
+
+func (c *httpClient) InvokeMethod(actorType, actorID, method string, contentType string, data []byte) ([]byte, error) {
+	url := fmt.Sprintf("%s/method/%s", c.defaultURL(actorType, actorID), method)
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", contentType)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode >= 300 {
+		return nil, &DaprActorClientError{Code: resp.StatusCode, Message: string(body)}
+	}
+
+	return body, nil
+}
+
+func (c *httpClient) SaveStateTransactionally(actorType, actorID string, data []byte) error {
+	url := fmt.Sprintf("%s/state", c.defaultURL(actorType, actorID))
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json; utf-8")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode >= 300 {
+		return &DaprActorClientError{Code: resp.StatusCode, Message: string(body)}
+	}
+
+	return nil
+}
+
+func (c *httpClient) GetState(actorType, actorID, name string) ([]byte, error) {
+	url := fmt.Sprintf("%s/state/%s", c.defaultURL(actorType, actorID), name)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode >= 300 {
+		return nil, &DaprActorClientError{Code: resp.StatusCode, Message: string(body)}
+	}
+
+	return body, nil
+}
+
+func (c *httpClient) WaitUntilDaprIsReady() error {
+	for i := 0; i < 10; i++ {
+		resp, err := c.client.Get(fmt.Sprintf("%s/%s", c.address, "v1.0/healthz"))
+		if err == nil && resp.StatusCode == 200 {
+			return nil
+		}
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	return errors.New("dapr is unavailable")
+}
+
+func (c *httpClient) Close() {
+	c.client.CloseIdleConnections()
+}
