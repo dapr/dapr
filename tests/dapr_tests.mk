4641ec729a54773bd39e4ede72723e81de90eb03
Mukundan Sundararajan <musundar@microsoft.com>
2020-09-18 16:16:10 -0700

    Refactor code.

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -155,7 +155,6 @@ setup-app-configurations:
 # Apply component yaml for state, secrets, pubsub, and bindings
 setup-test-components:
 	$(KUBECTL) apply -f ./tests/config/kubernetes_secret.yaml --namespace $(DAPR_TEST_NAMESPACE)
-	$(KUBECTL) apply -f ./tests/config/kubernetes_secret_2.yaml --namespace $(DAPR_TEST_NAMESPACE)
 	$(KUBECTL) apply -f ./tests/config/kubernetes_secret_config.yaml --namespace $(DAPR_TEST_NAMESPACE)
 	$(KUBECTL) apply -f ./tests/config/kubernetes_redis_secret.yaml --namespace $(DAPR_TEST_NAMESPACE)
 	$(KUBECTL) apply -f ./tests/config/dapr_redis_state.yaml --namespace $(DAPR_TEST_NAMESPACE)
