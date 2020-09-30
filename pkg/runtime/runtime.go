bc1410cdd5e2bbcf5fc87e95e61d651bbe7a7423
Young Bu Park <youngp@microsoft.com>
2020-09-29 17:36:56 -0700

    Fix exporter component typo (#2154)

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -77,7 +77,7 @@ type ComponentCategory string
 
 const (
 	bindingsComponent    ComponentCategory = "bindings"
-	exporterComponent    ComponentCategory = "exporter"
+	exporterComponent    ComponentCategory = "exporters"
 	pubsubComponent      ComponentCategory = "pubsub"
 	secretStoreComponent ComponentCategory = "secretstores"
 	stateComponent       ComponentCategory = "state"
@@ -1261,7 +1261,7 @@ func (a *DaprRuntime) appendOrReplaceComponents(component components_v1alpha1.Co
 	}
 }
 
-func (a *DaprRuntime) figureOutComponentCategory(component components_v1alpha1.Component) ComponentCategory {
+func (a *DaprRuntime) extractComponentCategory(component components_v1alpha1.Component) ComponentCategory {
 	for _, category := range componentCategoriesNeedProcess {
 		if strings.HasPrefix(component.Spec.Type, fmt.Sprintf("%s.", category)) {
 			return category
@@ -1294,7 +1294,7 @@ func (a *DaprRuntime) processComponentAndDependents(comp components_v1alpha1.Com
 		return nil
 	}
 
-	compCategory := a.figureOutComponentCategory(comp)
+	compCategory := a.extractComponentCategory(comp)
 	if err := a.doProcessOneComponent(compCategory, comp); err != nil {
 		log.Errorf("process component %s error, %s", comp.Name, err)
 		return err
