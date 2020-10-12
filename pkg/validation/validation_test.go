13198381de0dbbb3f36865237d76e0b3408c697e
yaron2 <yaronsc@microsoft.com>
2020-10-12 11:42:25 -0700

    insert RFC1123 validation for app id on k8s

---
+++ pkg/validation/validation_test.go
@@ -0,0 +1,42 @@
+package validation
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidationForKubernetes(t *testing.T) {
+	t.Run("invalid length", func(t *testing.T) {
+		id := ""
+		for i := 0; i < 64; i++ {
+			id = id + "a"
+		}
+		err := ValidateKubernetesAppID(id)
+		assert.Error(t, err)
+	})
+
+	t.Run("valid id", func(t *testing.T) {
+		id := "my-app-id"
+		err := ValidateKubernetesAppID(id)
+		assert.NoError(t, err)
+	})
+
+	t.Run("invalid char: .", func(t *testing.T) {
+		id := "my-app-id.app"
+		err := ValidateKubernetesAppID(id)
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid chars space", func(t *testing.T) {
+		id := "my-app-id.app"
+		err := ValidateKubernetesAppID(id)
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid empty", func(t *testing.T) {
+		id := ""
+		err := ValidateKubernetesAppID(id)
+		assert.Error(t, err)
+	})
+}
