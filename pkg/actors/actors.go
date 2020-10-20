622334153a3e43ad5b28e6d8b32bd8036fec9c11
Young Bu Park <youngp@microsoft.com>
2020-10-20 10:43:41 -0700

    wip

--- pkg/actors/actors.go
+++ pkg/actors/actors.go
@@ -153,7 +153,7 @@ func (a *actorsRuntime) Init() error {
 
 	// Be careful to configure healthz endpoint option. If app healthz returns unhealthy status, Dapr will
 	// disconnect from placement to remove the node from consistent hashing ring.
-	// i.e if app is really busy state, the heathz status would be flaky, which leads to frequent
+	// i.e if app is busy state, the heathz status would be flaky, which leads to frequent
 	// actor reblanacing. It will impact the entire service.
 	go a.startAppHealthCheck(
 		health.WithFailureThreshold(4),
