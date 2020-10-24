131b2c02396d4eeb9dc4b45f746724c03561b67b
Ubuntu <wmeints@vm-dapr-dev.1xxvpgubenqepa4er5o4vy0s1c.ax.internal.cloudapp.net>
2020-10-24 09:05:45 +0000

    Add performance test app for grpc

---
+++ tests/apps/perf/service_invocation_grpc/Dockerfile
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------------------------------
+
+FROM golang:1.15 as build_env
+
+WORKDIR /app
+COPY app.go .
+RUN go get -d -v
+RUN go build -o app .
+
+FROM debian:buster-slim
+WORKDIR /
+COPY --from=build_env /app/app /
+CMD ["/app"]
---
+++ tests/apps/perf/service_invocation_grpc/app.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/dapr/go-sdk/service/common"
+	daprd "github.com/dapr/go-sdk/service/grpc"
+)
+
+func main() {
+	s, err := daprd.NewService(":50001")
+
+	if err != nil {
+		log.Fatalf("failed to start server on port 50001: %v", err)
+	}
+
+	if err := s.AddServiceInvocationHandler("echo", runHandler); err != nil {
+		log.Fatalf("failed to add service invocation handler: %v", err)
+	}
+
+	if err := s.Start(); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
+}
+
+func runHandler(ctx context.Context, e * common.InvocationEvent) (out *common.Content, err error) {
+	out = &common.Content{
+		Data: []byte("ok"),
+		ContentType: "text/plain",
+	}
+	return
+}
