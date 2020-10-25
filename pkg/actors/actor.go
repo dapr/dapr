a79c7934da3e232ead5e7080fbdcd2aba1a1de1e
Young Bu Park <youngp@microsoft.com>
2020-10-25 16:41:15 -0700

    fix race condition and unnecessary mem alloc

--- pkg/actors/actor.go
+++ pkg/actors/actor.go
@@ -13,54 +13,68 @@ import (
 	diag "github.com/dapr/dapr/pkg/diagnostics"
 )
 
+// actor represents single actor object and maintains its turn-based concurrency.
 type actor struct {
+	// actorType is the type of actor.
 	actorType string
-	actorID   string
+	// actorID is the ID of actorType.
+	actorID string
 
-	concurrencyLock *sync.RWMutex
-	lastUsedTime    time.Time
-	busy            bool
-	busyCh          chan (bool)
+	// concurrencyLock is the lock to maintain actor's turn-based concurrency.
+	concurrencyLock *sync.Mutex
+	// pendingActorCalls is the number of the current pending actor calls by turn-based concurrency.
+	pendingActorCalls int32
 
-	pendingLockCount int32
+	// When consistent hashing tables are updated, actor runtime drains actor to rebalance actors
+	// across actor hosts after drainOngoingCallTimeout or until all pending actor calls are completed.
+	// lastUsedTime is the time when the last actor call holds lock. This is used to calculate
+	// the duration of ongoing calls to time out.
+	lastUsedTime time.Time
+	// releaseCh is the channel to signal when all pending actor calls are completed. This channel
+	// is used when runtime drains actor.
+	releaseCh chan struct{}
+
+	once sync.Once
 }
 
 func newActor(actorType, actorID string) *actor {
 	return &actor{
-		actorType:       actorType,
-		actorID:         actorID,
-		concurrencyLock: &sync.RWMutex{},
-		busy:            false,
-		busyCh:          make(chan bool, 1),
-		lastUsedTime:    time.Now().UTC(),
+		actorType:         actorType,
+		actorID:           actorID,
+		concurrencyLock:   &sync.Mutex{},
+		releaseCh:         nil,
+		lastUsedTime:      time.Now().UTC(),
+		pendingActorCalls: 0,
 	}
 }
 
+// isBusy returns true when pending actor calls are ongoing.
 func (a *actor) isBusy() bool {
-	return a.busy
+	return a.pendingActorCalls > 0
 }
 
-func (a *actor) channel() chan (bool) {
-	return a.busyCh
+// channel creates or get new release channel. this channel is used for draining the actor.
+func (a *actor) channel() chan struct{} {
+	a.once.Do(func() {
+		a.releaseCh = make(chan struct{})
+	})
+	return a.releaseCh
 }
 
+// lock holds the lock for turn-based concurrency.
 func (a *actor) lock() {
-	atomic.AddInt32(&a.pendingLockCount, 1)
-	diag.DefaultMonitoring.ReportCurrentPendingLocks(a.actorType, a.pendingLockCount)
+	atomic.AddInt32(&a.pendingActorCalls, int32(1))
+	diag.DefaultMonitoring.ReportActorPendingCalls(a.actorType, a.pendingActorCalls)
 	a.concurrencyLock.Lock()
-
-	a.busy = true
-	a.busyCh = make(chan bool, 1)
 	a.lastUsedTime = time.Now().UTC()
 }
 
-func (a *actor) unLock() {
-	if a.busy {
-		a.busy = false
-		close(a.busyCh)
-	}
-
+// unlock release the lock for turn-based concurrency. If releaseChannel is avaiable,
+// it will close the channel to notify runtime to delete actor.
+func (a *actor) unlock() {
 	a.concurrencyLock.Unlock()
-	atomic.AddInt32(&a.pendingLockCount, -1)
-	diag.DefaultMonitoring.ReportCurrentPendingLocks(a.actorType, a.pendingLockCount)
+	if atomic.AddInt32(&a.pendingActorCalls, int32(-1)) == 0 && a.releaseCh != nil {
+		close(a.releaseCh)
+	}
+	diag.DefaultMonitoring.ReportActorPendingCalls(a.actorType, a.pendingActorCalls)
 }
--- pkg/actors/actor_test.go
+++ pkg/actors/actor_test.go
@@ -16,10 +16,10 @@ func TestIsBusy(t *testing.T) {
 	testActor := newActor("testType", "testID")
 	testActor.lock()
 	assert.Equal(t, true, testActor.isBusy())
-	testActor.unLock()
+	testActor.unlock()
 }
 
-func TestConcurrencyLocks(t *testing.T) {
+func TestTurnBasedConcurrencyLocks(t *testing.T) {
 	testActor := newActor("testType", "testID")
 
 	// first lock
@@ -33,39 +33,86 @@ func TestConcurrencyLocks(t *testing.T) {
 	}()
 
 	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, int32(2), testActor.pendingActorCalls)
 	assert.True(t, testActor.isBusy())
 	assert.Equal(t, firstLockTime, testActor.lastUsedTime)
 
 	// unlock the first lock
-	testActor.unLock()
+	testActor.unlock()
 
 	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, int32(1), testActor.pendingActorCalls)
 	assert.True(t, testActor.isBusy())
 	assert.NotEqual(t, firstLockTime, testActor.lastUsedTime)
 
 	// unlock the second lock
-	testActor.unLock()
+	testActor.unlock()
+	assert.Equal(t, int32(0), testActor.pendingActorCalls)
 	assert.False(t, testActor.isBusy())
 	assert.NotEqual(t, firstLockTime, testActor.lastUsedTime)
 }
 
 func TestBusyChannel(t *testing.T) {
-	testActor := newActor("testType", "testID")
-	testActor.lock()
+	t.Run("no pending actor call with new actor object", func(t *testing.T) {
+		testActor := newActor("testType", "testID")
+		channelClosed := false
 
-	channelClosed := false
-	go func() {
 		select {
-		case <-time.After(10 * time.Second):
+		case <-time.After(10 * time.Millisecond):
 			break
 		case <-testActor.channel():
 			channelClosed = true
 			break
 		}
-	}()
 
-	time.Sleep(10 * time.Millisecond)
-	testActor.unLock()
-	time.Sleep(100 * time.Millisecond)
-	assert.True(t, channelClosed)
+		assert.False(t, channelClosed)
+	})
+
+	t.Run("close channel before timeout", func(t *testing.T) {
+		testActor := newActor("testType", "testID")
+		testActor.lock()
+
+		channelClosed := false
+		go func() {
+			select {
+			case <-time.After(200 * time.Millisecond):
+				break
+			case <-testActor.channel():
+				channelClosed = true
+				break
+			}
+		}()
+
+		time.Sleep(10 * time.Millisecond)
+		testActor.unlock()
+		time.Sleep(100 * time.Millisecond)
+		assert.True(t, channelClosed)
+	})
+
+	t.Run("multiple listeners", func(t *testing.T) {
+		testActor := newActor("testType", "testID")
+		testActor.lock()
+
+		nListeners := 10
+		releaseSignaled := make([]bool, nListeners)
+
+		for i := 0; i < nListeners; i++ {
+			releaseCh := testActor.channel()
+			go func(listenerIndex int) {
+				select {
+				case <-time.After(200 * time.Millisecond):
+					break
+				case <-releaseCh:
+					releaseSignaled[listenerIndex] = true
+					break
+				}
+			}(i)
+		}
+		testActor.unlock()
+		time.Sleep(100 * time.Millisecond)
+
+		for i := 0; i < nListeners; i++ {
+			assert.True(t, releaseSignaled[i])
+		}
+	})
 }
