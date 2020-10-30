7b3b50123353d1ecd2ed1d15510efa79ab3a4b48
Young Bu Park <youngp@microsoft.com>
2020-10-30 15:51:07 -0700

    Initial commit of Raft implementation in Placement service (#2277)
    This PR is quite huge because this is the initial commit of raft implementation in placement service. The implementation uses hashicorp/raft to implement leader election and get dapr runtime member state consensus among multiple replicas.
    
    In this PR, Placement uses the old way to maintain member list and consistent hashing table. Next PRs will switch this logic to use the state data maintained by Raft protocol.
    
    Change summary:
    
    1. Add initial Raft Server implementation.
    2. Add DaprHostMemberState State for Raft Finite State Machine.
    3. cmd log will construct the list of Dapr runtime member hosts and treat this list as a state. FSM will snapshot this list
    4. Consistent hashing table map will be built based on the list of Dapr runtime member hosts; this will not be saved as state.
    5. Use MessagePack serialization for state and snapshot.
    6. Add three command flags for Raft
    id: raft node id
    initial-cluster: raft peer node list - we will not support join option to join new placement node to raft cluster for now.
    inmem-store-enabled: use in-memory storage for raft log and snapshot (for dev or non-HA mode)
    7. Refactor main.go to clean up the code.
    8. Add unit-tests

---
+++ pkg/placement/raft/state_test.go
@@ -0,0 +1,227 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package raft
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDaprHostMemberState(t *testing.T) {
+	// act
+	s := newDaprHostMemberState()
+
+	// assert
+	assert.Equal(t, uint64(0), s.Index)
+	assert.Equal(t, 0, len(s.Members))
+	assert.Equal(t, 0, len(s.hashingTableMap))
+}
+
+func TestClone(t *testing.T) {
+	// arrange
+	s := newDaprHostMemberState()
+	s.upsertMember(&DaprHostMember{
+		Name:     "127.0.0.1:8080",
+		AppID:    "FakeID",
+		Entities: []string{"actorTypeOne", "actorTypeTwo"},
+	})
+
+	// act
+	newState := s.clone()
+
+	// assert
+	assert.NotSame(t, s, newState)
+	assert.Nil(t, newState.hashingTableMap)
+	assert.Equal(t, s.Index, newState.Index)
+	assert.EqualValues(t, s.Members, newState.Members)
+}
+
+func TestUpsertMember(t *testing.T) {
+	// arrange
+	s := newDaprHostMemberState()
+
+	t.Run("add new actor member", func(t *testing.T) {
+		// act
+		s.upsertMember(&DaprHostMember{
+			Name:     "127.0.0.1:8080",
+			AppID:    "FakeID",
+			Entities: []string{"actorTypeOne", "actorTypeTwo"},
+		})
+
+		// assert
+		assert.Equal(t, 1, len(s.Members))
+		assert.Equal(t, 2, len(s.hashingTableMap))
+	})
+
+	t.Run("add non actor member", func(t *testing.T) {
+		// act
+		s.upsertMember(&DaprHostMember{
+			Name:     "127.0.0.1:8081",
+			AppID:    "FakeID_2",
+			Entities: []string{},
+		})
+
+		// assert
+		assert.Equal(t, 2, len(s.Members))
+		assert.Equal(t, 2, len(s.hashingTableMap))
+	})
+
+	t.Run("update existing actor member", func(t *testing.T) {
+		testMember := &DaprHostMember{
+			Name:     "127.0.0.1:8080",
+			AppID:    "FakeID",
+			Entities: []string{"actorTypeOneA"},
+		}
+
+		oldUpdatedAt := s.Members[testMember.Name].UpdatedAt
+
+		time.Sleep(100 * time.Millisecond)
+
+		// act
+		//
+		// this tries to update the existing actor members.
+		// it will delete empty consistent hashing table.
+		s.upsertMember(testMember)
+
+		// assert
+		assert.Equal(t, 2, len(s.Members))
+		assert.Equal(t, 1, len(s.Members[testMember.Name].Entities))
+		assert.True(t, s.Members[testMember.Name].UpdatedAt.Sub(oldUpdatedAt) > 0,
+			"UpdatedAt must be updated old: %v, new: %v", oldUpdatedAt, s.Members[testMember.Name].UpdatedAt)
+		assert.Equal(t, 1, len(s.hashingTableMap), "this doesn't delete empty consistent hashing table")
+	})
+}
+
+func TestRemoveMember(t *testing.T) {
+	// arrange
+	s := newDaprHostMemberState()
+
+	t.Run("remove member and clean up consistent hashing table", func(t *testing.T) {
+		// act
+		s.upsertMember(&DaprHostMember{
+			Name:     "127.0.0.1:8080",
+			AppID:    "FakeID",
+			Entities: []string{"actorTypeOne", "actorTypeTwo"},
+		})
+
+		// assert
+		assert.Equal(t, 1, len(s.Members))
+		assert.Equal(t, 2, len(s.hashingTableMap))
+
+		// act
+		s.removeMember(&DaprHostMember{
+			Name: "127.0.0.1:8080",
+		})
+
+		// assert
+		assert.Equal(t, 0, len(s.Members))
+		assert.Equal(t, 0, len(s.hashingTableMap))
+	})
+}
+
+func TestUpdateHashingTable(t *testing.T) {
+	// each subtest has dependency on the state
+
+	// arrange
+	s := newDaprHostMemberState()
+
+	t.Run("add new hashing table per actor types", func(t *testing.T) {
+		testMember := &DaprHostMember{
+			Name:     "127.0.0.1:8080",
+			AppID:    "FakeID",
+			Entities: []string{"actorTypeOne", "actorTypeTwo"},
+		}
+
+		// act
+		s.updateHashingTables(testMember)
+
+		assert.Equal(t, 2, len(s.hashingTableMap))
+		for _, ent := range testMember.Entities {
+			assert.NotNil(t, s.hashingTableMap[ent])
+		}
+	})
+
+	t.Run("update new hashing table per actor types", func(t *testing.T) {
+		testMember := &DaprHostMember{
+			Name:     "127.0.0.1:8080",
+			AppID:    "FakeID",
+			Entities: []string{"actorTypeOne", "actorTypeTwo", "actorTypeThree"},
+		}
+
+		// act
+		s.updateHashingTables(testMember)
+
+		assert.Equal(t, 3, len(s.hashingTableMap))
+		for _, ent := range testMember.Entities {
+			assert.NotNil(t, s.hashingTableMap[ent])
+		}
+	})
+}
+
+func TestRemoveHashingTable(t *testing.T) {
+	// each subtest has dependency on the state
+
+	// arrange
+	testMember := &DaprHostMember{
+		Name:     "fakeName",
+		AppID:    "fakeID",
+		Entities: []string{"actorTypeOne", "actorTypeTwo"},
+	}
+
+	var testcases = []struct {
+		name       string
+		totalTable int
+	}{
+		{"127.0.0.1:8080", 2},
+		{"127.0.0.1:8081", 0},
+	}
+
+	s := newDaprHostMemberState()
+	for _, tc := range testcases {
+		testMember.Name = tc.name
+		s.updateHashingTables(testMember)
+	}
+
+	// act
+	for _, tc := range testcases {
+		t.Run("remove host "+tc.name, func(t *testing.T) {
+			testMember.Name = tc.name
+			s.removeHashingTables(testMember)
+
+			assert.Equal(t, tc.totalTable, len(s.hashingTableMap))
+		})
+	}
+}
+
+func TestRestoreHashingTables(t *testing.T) {
+	// arrange
+	var testnames = []string{
+		"127.0.0.1:8080",
+		"127.0.0.1:8081",
+	}
+
+	s := &DaprHostMemberState{
+		Index:           0,
+		Members:         map[string]*DaprHostMember{},
+		hashingTableMap: nil,
+	}
+	for _, tn := range testnames {
+		s.Members[tn] = &DaprHostMember{
+			Name:     tn,
+			AppID:    "fakeID",
+			Entities: []string{"actorTypeOne", "actorTypeTwo"},
+		}
+	}
+	assert.Equal(t, 0, len(s.hashingTableMap))
+
+	// act
+	s.restoreHashingTables()
+
+	// assert
+	assert.Equal(t, 2, len(s.hashingTableMap))
+}
