206592a78a5093715ccab8944f3ea5fe7b5995e0
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2020-11-02 13:40:06 -0800

    error

--- tests/e2e/allowlists/allowlists_service_invocation_test.go
+++ tests/e2e/allowlists/allowlists_service_invocation_test.go
@@ -99,7 +99,7 @@ var allowListsForServiceInvocationTests = []struct {
 		"Test deny with callee side grpc",
 		"allowlists-callee-grpc",
 		"httptogrpctest",
-		"fail to invoke, id: allowlists-callee-grpc, err: rpc error: code = PermissionDenied desc = access control policy has denied access to appid: allowlists-caller operation: httpToGrpcTest verb: NONE",
+		"HTTP call failed with fail to invoke, id: allowlists-callee-grpc, err: rpc error: code = PermissionDenied desc = access control policy has denied access to appid: allowlists-caller operation: httpToGrpcTest verb: NONE",
 		"grpc",
 	},
 }
