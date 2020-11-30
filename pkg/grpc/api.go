728d474fa5771819dd9bf912a7257932b096a5e6
cvictory <shenglicao2@gmail.com>
2020-11-30 17:29:36 +0800

    #2498 Add metadata for stateStore

--- pkg/grpc/api.go
+++ pkg/grpc/api.go
@@ -333,6 +333,7 @@ func (a *api) GetBulkState(ctx context.Context, in *runtimev1pb.GetBulkStateRequ
 			} else if r != nil {
 				item.Data = r.Data
 				item.Etag = r.ETag
+				item.Metadata = r.Metadata
 			}
 			resp.Items = append(resp.Items, item)
 		}
@@ -381,6 +382,7 @@ func (a *api) GetState(ctx context.Context, in *runtimev1pb.GetStateRequest) (*r
 	if getResponse != nil {
 		response.Etag = getResponse.ETag
 		response.Data = getResponse.Data
+		response.Metadata = getResponse.Metadata
 	}
 	return response, nil
 }
