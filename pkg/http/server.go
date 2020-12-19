463b08fb05db9fe13dd68865178560bc51f54c90
Juan Carlos Zamora <juazasan@msn.com>
2020-12-19 14:38:41 +0100

    implemented new middleware to escape HTML characters in the request URI

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -15,6 +15,8 @@ import (
 	cors_dapr "github.com/dapr/dapr/pkg/cors"
 	"github.com/dapr/dapr/pkg/logger"
 
+	"net/url"
+
 	diag "github.com/dapr/dapr/pkg/diagnostics"
 	diag_utils "github.com/dapr/dapr/pkg/diagnostics/utils"
 	http_middleware "github.com/dapr/dapr/pkg/middleware/http"
@@ -56,7 +58,8 @@ func (s *server) StartNonBlocking() {
 		useAPIAuthentication(
 			s.useCors(
 				s.useComponents(
-					s.useRouter())))
+					unscapeURIQuery(
+						s.useRouter()))))
 
 	handler = s.useMetrics(handler)
 	handler = s.useTracing(handler)
@@ -123,6 +126,20 @@ func useAPIAuthentication(next fasthttp.RequestHandler) fasthttp.RequestHandler 
 			next(ctx)
 		} else {
 			ctx.Error("invalid api token", http.StatusUnauthorized)
+		}
+	}
+}
+
+func unscapeURIQuery(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		unescapedRequestURI, err := url.QueryUnescape(string(ctx.RequestURI()))
+		if err == nil {
+			log.Info("RequestURI unescaped successfully")
+			ctx.Request.SetRequestURI(unescapedRequestURI)
+			next(ctx)
+		} else {
+			errorMessage := fmt.Sprintf("Failed to unescape request %s with error %s", ctx.RequestURI(), err.Error())
+			ctx.Error(errorMessage, fasthttp.StatusBadRequest)
 		}
 	}
 }
