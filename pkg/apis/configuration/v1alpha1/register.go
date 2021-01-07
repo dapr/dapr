4f850e2ec2a9ff6f9bff3f693bd43c44a5814cd8
AlphaBaby <fujianhao1997@qq.com>
2021-01-07 11:27:21 +0800

    delete redundant code for issue #2627

--- pkg/apis/configuration/v1alpha1/register.go
+++ pkg/apis/configuration/v1alpha1/register.go
@@ -37,7 +37,6 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&Configuration{},
 		&ConfigurationList{},
 	)
-	scheme.AddKnownTypes(SchemeGroupVersion)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
