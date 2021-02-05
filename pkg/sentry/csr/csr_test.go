14dac30c217ab824ff568242049291a79592cd0a
seachen <seachen@tencent.com>
2021-02-05 12:32:52 +0800

    feat(sentry): add parse PKCS#8 certificate request UT

--- pkg/sentry/csr/csr_test.go
+++ pkg/sentry/csr/csr_test.go
@@ -1,12 +1,15 @@
 package csr
 
 import (
+	"crypto"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/x509"
 	"testing"
 	"time"
 
+	"github.com/dapr/dapr/pkg/sentry/certs"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -55,4 +58,39 @@ func TestGenerateCSR(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, len(b) > 0)
 	assert.True(t, len(c) > 0)
+}
+
+func TestEncode(t *testing.T) {
+	var CertTests = []struct {
+		org     string `json:"org"`
+		isPkcs8 bool   `json:"is_pkcs8"`
+	}{
+		{
+			org:     "dapr.io",
+			isPkcs8: false,
+		},
+		{
+			org:     "dapr.io",
+			isPkcs8: true,
+		},
+	}
+	var errs = make([]error, len(CertTests))
+	for index, test := range CertTests {
+		key, _ := certs.GenerateECPrivateKey()
+		templ, _ := genCSRTemplate(test.org)
+		csrBytes, err := x509.CreateCertificateRequest(rand.Reader, templ, crypto.PrivateKey(key))
+		if err != nil {
+			errs[index] = err
+			return
+		}
+		crtPem, keyPem, err := encode(true, csrBytes, key, test.isPkcs8)
+		if err != nil {
+			errs[index] = err
+			return
+		}
+		_, err = PEMCredentialRequestsFromFiles(crtPem, keyPem)
+		errs[index] = err
+	}
+	assert.Nil(t, errs[0])
+	assert.Nil(t, errs[1])
 }
