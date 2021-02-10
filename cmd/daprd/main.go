f4d50b1369e416a8f7b93e3e226c4360307d1313
Yaron Schneider <yaronsc@microsoft.com>
2021-02-10 11:25:53 -0800

    Add missing Alibaba Cloud OSS binding (#2792)
    * update helm chart
    
    * Fix dashboard helm chart to point to 0.6 instead of edge. (#2741)
    
    * fix sidecar injector secret (#2742)
    
    * reuse webhook cert bundle (#2745)
    
    * fix grpc pub/sub serialization to app (#2751)
    
    * fix grpc pub/sub serialization to app
    
    * updated content type check
    
    * remove redundant package
    
    * Fix helm chart non-root issue (#2759)
    
    * fix helm chart non-root issue
    
    * update README
    
    * Release notes for RC3 (#2753)
    
    * Auto-generated release notes for rc3.
    
    * remove tests from release notes
    
    * Update based on rc2.
    
    * Updated upgrade instructions + more
    
    * Saving
    
    * Sync
    
    * Sync
    
    * sync
    
    * sync
    
    * sync
    
    * sync
    
    * Update v1.0.0-rc.3.md
    
    * Update docs/release_notes/v1.0.0-rc.3.md
    
    * Update docs/release_notes/v1.0.0-rc.3.md
    
    Co-authored-by: Mukundan Sundararajan <musundar@microsoft.com>
    Co-authored-by: HackMD <no-reply@hackmd.io>
    Co-authored-by: Yaron Schneider <yaronsc@microsoft.com>
    Co-authored-by: Charlie Stanley <charlie.stanley@microsoft.com>
    
    * add missing alibaba cloud oss binding
    
    * go mod tidy
    
    Co-authored-by: Artur Souza <artursouza.ms@outlook.com>
    Co-authored-by: Trond Hindenes <trond@hindenes.com>
    Co-authored-by: Mukundan Sundararajan <musundar@microsoft.com>
    Co-authored-by: HackMD <no-reply@hackmd.io>
    Co-authored-by: Charlie Stanley <charlie.stanley@microsoft.com>

--- cmd/daprd/main.go
+++ cmd/daprd/main.go
@@ -74,6 +74,7 @@ import (
 
 	// Bindings
 	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/components-contrib/bindings/alicloud/oss"
 	"github.com/dapr/components-contrib/bindings/apns"
 	"github.com/dapr/components-contrib/bindings/aws/dynamodb"
 	"github.com/dapr/components-contrib/bindings/aws/kinesis"
@@ -299,6 +300,9 @@ func main() {
 			}),
 		),
 		runtime.WithOutputBindings(
+			bindings_loader.NewOutput("alicloud.oss", func() bindings.OutputBinding {
+				return oss.NewAliCloudOSS(logContrib)
+			}),
 			bindings_loader.NewOutput("apns", func() bindings.OutputBinding {
 				return apns.NewAPNS(logContrib)
 			}),
