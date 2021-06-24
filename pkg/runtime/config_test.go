8cac0e661e642671fdf825c21894a74a24157a77
apuzyrevsky <anatoliypuzyrevsky@gmail.com>
2021-06-25 00:26:03 +0300

    fixing one more test

--- pkg/runtime/config_test.go
+++ pkg/runtime/config_test.go
@@ -13,7 +13,7 @@ import (
 
 func TestNewConfig(t *testing.T) {
 	c := NewRuntimeConfig("app1", []string{"localhost:5050"}, "localhost:5051", "*", "config", "components", "http", "kubernetes",
-		3500, 50002, 50001, 8080, 7070, true, 1, true, "localhost:5052", true, 4, 4, false)
+		3500, 50002, 50001, 8080, 7070, true, 1, true, "localhost:5052", true, 4, 4, true)
 
 	assert.Equal(t, "app1", c.ID)
 	assert.Equal(t, "localhost:5050", c.PlacementAddresses[0])
