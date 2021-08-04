758321a23bc60cb492615420d4c0f091dc7eb218
Long <long0dai@foxmail.com>
2021-08-04 10:35:38 +0000

    runtime: support unix domain socket
    Signed-off-by: Long <long0dai@foxmail.com>

--- pkg/runtime/runtime_test.go
+++ pkg/runtime/runtime_test.go
@@ -2576,7 +2576,7 @@ func NewTestDaprRuntimeWithProtocol(mode modes.DaprMode, protocol string, appPor
 		-1,
 		false,
 		"",
-		false, 4)
+		false, 4, false)
 
 	return NewDaprRuntime(testRuntimeConfig, &config.Configuration{}, &config.AccessControlList{})
 }
