dd6ac4077ed9427b68f0e20919dc739a3d464ae9
Ian Luo <ian.luo@gmail.com>
2021-08-30 21:38:50 +0800

    enhance doc

--- pkg/http/endpoint.go
+++ pkg/http/endpoint.go
@@ -7,9 +7,12 @@ package http
 
 import "github.com/valyala/fasthttp"
 
-// Endpoint is a collection of route information for an Dapr API. Either a combination of Route and Version or an Alias
-// is required, and when Alias is present, it indicates extra necessary infos are provided via HTTP headers instead of
-// via URL path.
+// Endpoint is a collection of route information for an Dapr API.
+//
+// If an Alias, e.g. "hello", is provided along with the Route, e.g. "invoke/app-id/method/hello" and the Version,
+// "v1.0", then two endpoints will be installed instead of one. Besiding the canonical Dapr API URL
+// "/v1.0/invoke/app-id/method/hello", one another URL "/hello" is provided for the Alias. When Alias URL is used,
+// extra infos are required to pass through HTTP headers, for example, application's ID.
 type Endpoint struct {
 	Methods []string
 	Route   string
