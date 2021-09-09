ee0e40c5da0996a921973b7d7f3cad0e848f835a
theonefx <chenxilzx1@gmail.com>
2021-09-09 19:39:30 +0800

    test build done

--- pkg/runtime/config_test.go
+++ pkg/runtime/config_test.go
@@ -14,7 +14,7 @@ import (
 func TestNewConfig(t *testing.T) {
 	publicPort := DefaultDaprPublicPort
 	c := NewRuntimeConfig("app1", []string{"localhost:5050"}, "localhost:5051", "*", "config", "components", "http", "kubernetes",
-		3500, 50002, 50001, []string{"1.2.3.4"}, &publicPort, 8080, 7070, true, 1, true, "localhost:5052", true, 4, "")
+		3500, 50002, 50001, []string{"1.2.3.4"}, &publicPort, 8080, 7070, true, 1, true, "localhost:5052", true, 4, "", "", false)
 
 	assert.Equal(t, "app1", c.ID)
 	assert.Equal(t, "localhost:5050", c.PlacementAddresses[0])
