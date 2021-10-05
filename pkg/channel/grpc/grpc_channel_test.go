8c4bca07e0d1bf0f89642910ebb32a135fb64481
apuzyrevsky <anatoliypuzyrevsky@gmail.com>
2021-10-05 14:08:56 +0300

    removing unecessary checks

--- pkg/channel/grpc/grpc_channel_test.go
+++ pkg/channel/grpc/grpc_channel_test.go
@@ -58,8 +58,6 @@ func TestInvokeMethod(t *testing.T) {
 	assert.Equal(t, "method", actual["method"])
 	assert.Equal(t, "token1", actual[auth.APITokenHeader])
 	assert.Equal(t, "param1=val1&param2=val2", actual["querystring"])
-	assert.Equal(t, 4, c.maxRequestBodySize)
-	assert.Equal(t, 4, c.readBufferSize)
 }
 
 func close(t *testing.T, c io.Closer) {
