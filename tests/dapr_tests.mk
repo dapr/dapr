e79e1562790ddddf9be00b8b5345e7d843d9f67f
crazyhzm <crazyhzm@gmail.com>
2022-01-07 11:03:28 +0800

    Correct use of go get and go install in Makefile
    Signed-off-by: crazyhzm <crazyhzm@gmail.com>
    Signed-off-by: HueiFeng <695979933@qq.com>

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -208,9 +208,9 @@ push-kind-perf-app-all: $(PUSH_KIND_PERF_APPS_TARGETS)
 test-deps:
 	# The desire here is to download this test dependency without polluting go.mod
 	# In golang >=1.16 there is a new way to do this with `go install gotest.tools/gotestsum@latest`
-	# But this doesn't work with <=1.15, so we do it the old way for now 
+	# But this doesn't work with <=1.15.
 	# (see: https://golang.org/ref/mod#go-install)
-	command -v gotestsum || GO111MODULE=off go get gotest.tools/gotestsum
+	command -v gotestsum || go install gotest.tools/gotestsum@latest
 
 # start all e2e tests
 test-e2e-all: check-e2e-env test-deps
