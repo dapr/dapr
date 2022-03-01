1776557226735fa3ca4981cdf53d740227110295
Phil Kedy <phil.kedy@gmail.com>
2022-02-28 20:21:00 -0500

    NoOp implementation of Resiliency (#4308)
    * Adding NoOp implementation of Resiliency for true bypass behavior
    
    Signed-off-by: Phil Kedy <phil.kedy@gmail.com>
    
    * Fix comment
    
    Signed-off-by: Phil Kedy <phil.kedy@gmail.com>
    
    * Add license headers
    
    Signed-off-by: Phil Kedy <phil.kedy@gmail.com>
    
    * Check errors in unit test
    
    Signed-off-by: Phil Kedy <phil.kedy@gmail.com>

---
+++ pkg/resiliency/noop.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resiliency
+
+import (
+	"context"
+)
+
+// NoOp is a true bypass implementation of `Provider`.
+type NoOp struct{}
+
+// Ensure `*NoOp` satisfies the `Provider` interface.
+var _ = (Provider)((*NoOp)(nil))
+
+// RoutePolicy returns a NoOp policy runner for a route.
+func (*NoOp) RoutePolicy(ctx context.Context, name string) Runner {
+	return func(oper Operation) error {
+		return oper(ctx)
+	}
+}
+
+// EndpointPolicy returns a NoOp policy runner for a service endpoint.
+func (*NoOp) EndpointPolicy(ctx context.Context, service string, endpoint string) Runner {
+	return func(oper Operation) error {
+		return oper(ctx)
+	}
+}
+
+// ActorPolicy returns a NoOp policy runner for an actor instance.
+func (*NoOp) ActorPolicy(ctx context.Context, actorType string, id string) Runner {
+	return func(oper Operation) error {
+		return oper(ctx)
+	}
+}
+
+// ComponentPolicy returns a NoOp policy runner for a component.
+func (*NoOp) ComponentPolicy(ctx context.Context, name string) Runner {
+	return func(oper Operation) error {
+		return oper(ctx)
+	}
+}
