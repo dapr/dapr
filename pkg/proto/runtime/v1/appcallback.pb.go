10203df0b618c7a4b3572e09dd7ca867a618251c
Shubham Sharma <shubhash@microsoft.com>
2022-03-07 16:00:55 +0530

    Generate new proto
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- pkg/proto/runtime/v1/appcallback.pb.go
+++ pkg/proto/runtime/v1/appcallback.pb.go
@@ -19,14 +19,12 @@
 package runtime
 
 import (
-	reflect "reflect"
-	sync "sync"
-
+	v1 "github.com/dapr/dapr/pkg/proto/common/v1"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
-
-	v1 "github.com/dapr/dapr/pkg/proto/common/v1"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
