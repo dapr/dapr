1897f2f3ce132c750e651bf62443e309fdada142
Shubham Sharma <shubhash@microsoft.com>
2022-03-14 19:26:26 +0530

    Stop using jsoniter in test
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- tests/apps/stateapp/app.go
+++ tests/apps/stateapp/app.go
@@ -29,7 +29,6 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
-	jsoniter "github.com/json-iterator/go"
 	"google.golang.org/grpc"
 
 	daprhttp "github.com/dapr/dapr/pkg/http"
@@ -364,7 +363,7 @@ func executeQuery(query []byte, statestore string, meta map[string]string) ([]da
 	}
 
 	var qres daprhttp.QueryResponse
-	err = jsoniter.Unmarshal(body, &qres)
+	err = json.Unmarshal(body, &qres)
 	if err != nil {
 		return nil, fmt.Errorf("could not unmarshal query response from Dapr: %s", err.Error())
 	}
