0ac20c1ff3a476475b2689d6865c38e2164fd1cc
halspang <70976921+halspang@users.noreply.github.com>
2022-04-14 22:30:13 +0000

    Remove error for Actor Timer DueTime in the past (#4518)
    The DueTime of an Actor Timer signifies the first time it should
    fire. An omitted or 0 DueTime means fire immediately. A check was
    introduced that forced a failure if the DueTime was in the past.
    However, when the time was 0, it was seen as in the past as the
    evaluation logic used multiple time.Now instances.
    
    Regardless, the semantic behavior of this did not make sense as
    a DueTime in the past should still just mean fire immediately. This
    commit removes that check allowing for this behavior. This is also
    how Actor Reminders work.
    
    https://github.com/dapr/dapr/issues/4514
    
    Signed-off-by: Hal Spang <halspang@microsoft.com>

--- pkg/actors/actors.go
+++ pkg/actors/actors.go
@@ -1125,9 +1125,6 @@ func (a *actorsRuntime) CreateTimer(ctx context.Context, req *CreateTimerRequest
 		if dueTime, err = parseTime(req.DueTime, nil); err != nil {
 			return errors.Wrap(err, "error parsing timer due time")
 		}
-		if dueTime.Before(time.Now()) {
-			return errors.Errorf("timer %s has already expired: dueTime: %s TTL: %s", timerKey, req.DueTime, req.TTL)
-		}
 	} else {
 		dueTime = time.Now()
 	}
