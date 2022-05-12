98ee42b7f4bea671b3c652c70214dac30470d85f
seeflood <349895584@qq.com>
2022-05-12 14:32:17 +0800

    add ut
    Signed-off-by: seeflood <349895584@qq.com>

--- pkg/components/lock/registry_test.go
+++ pkg/components/lock/registry_test.go
@@ -10,17 +10,25 @@ import (
 )
 
 const (
-	compName = "mock"
-	fullName = "lock." + compName
+	compName   = "mock"
+	compNameV2 = "mock/v2"
+	fullName   = "lock." + compName
 )
 
 func TestNewRegistry(t *testing.T) {
 	r := NewRegistry()
-	r.Register(New(compName, func() lock.Store {
-		return nil
-	}),
+	r.Register(
+		New(compName, func() lock.Store {
+			return nil
+		}),
+		New(compNameV2, func() lock.Store {
+			return nil
+		}),
 	)
 	if _, err := r.Create(fullName, "v1"); err != nil {
+		t.Fatalf("create mock store failed: %v", err)
+	}
+	if _, err := r.Create(fullName, "v2"); err != nil {
 		t.Fatalf("create mock store failed: %v", err)
 	}
 	if _, err := r.Create("not exists", "v1"); !strings.Contains(err.Error(), "couldn't find lock store") {
