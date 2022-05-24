a01591e3438024f74b7cf8bdb57aa4cb611ad267
syuparn <s.hello.spagetti@gmail.com>
2022-05-24 23:16:34 +0900

    fix concurrent gRPC proxy requests
    Signed-off-by: syuparn <s.hello.spagetti@gmail.com>

--- pkg/grpc/proxy/director.go
+++ pkg/grpc/proxy/director.go
@@ -21,4 +21,4 @@ import (
 // are invoked. So decisions around authorization, monitoring etc. are better to be handled there.
 //
 // See the rather rich example.
-type StreamDirector func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error)
+type StreamDirector func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, func(), error)
