5525744aa2de16c19ca3003343e854d80942af06
Artur Souza <artursouza.ms@outlook.com>
2022-06-10 14:02:28 -0700

    Enabling Actor.TypeMetadata as a stable feature. (#4737)
    Signed-off-by: Artur Souza <artursouza.ms@outlook.com>
    
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>

--- pkg/actors/actors_test.go
+++ pkg/actors/actors_test.go
@@ -344,12 +344,7 @@ func newTestActorsRuntimeWithMockAndActorMetadataPartition(appChannel channel.Ap
 		},
 	}
 	c := NewConfig("", TestAppID, []string{""}, 0, "", appConfig)
-	a := NewActors(store, appChannel, nil, c, nil, spec, []config.FeatureSpec{
-		{
-			Name:    config.ActorTypeMetadata,
-			Enabled: true,
-		},
-	}, resiliency.New(log), "actorStore")
+	a := NewActors(store, appChannel, nil, c, nil, spec, []config.FeatureSpec{}, resiliency.New(log), "actorStore")
 
 	return a.(*actorsRuntime)
 }
