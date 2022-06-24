47f2f0cb28cdd3991501ac31a51c0a4e5f94da40
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2022-06-24 13:44:53 -0700

    Build tools CLI (plus speed up E2E tests by copying test apps without pulling them) (#4729)
    * Copy e2e test app images without pulling them
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Copy test app images without pulling them first
    
    This is possible thanks to the new build-tools command
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Fixes for tester app not building
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Using `make build-push-e2e-app-all` in KIND too
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Changed as requested
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * In Makefile, run build-tools without compiling
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Renamed folder to .build-tools
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    Co-authored-by: halspang <70976921+halspang@users.noreply.github.com>
    Co-authored-by: Artur Souza <artursouza.ms@outlook.com>
    Signed-off-by: Addison Juarez <adjuarez@microsoft.com>

---
+++ .build-tools/cmd/root.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "dapr-build-tools",
+	Short: "Build tools for Dapr",
+	Long:  `A collection of commands and tools used to build and package Dapr`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
