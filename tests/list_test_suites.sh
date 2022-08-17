dc7c0be08f6cc8d6ef671d1d9f234d5f080b3ad9
Marcos Candeia <marrcooos@gmail.com>
2022-08-17 14:34:46 -0300

    Create environment variable to identify test suite
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- tests/list_test_suites.sh
+++ tests/list_test_suites.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+# List of all tests that should be executed
 tests=($1)
-tests_n=$(expr ${#tests[@]} / $2)
 
+# Count of tests
+total_tests=${#tests[@]}
+
+# Number of tests by suite
+tests_n=$(expr $total_tests / $2)
+
+# Double number (in case of remaining tests and division is not exactly)
+double=$(expr $tests_n \* 2)
+
+# divided suites
 suites=
-for((i=0; i < ${#tests[@]}; i+=tests_n))
+
+for((i=0; i < $total_tests; i+=tests_n))
 do
-  suites+="\"${tests[@]:i:tests_n}\","
+  remaining=$(expr $i + $double)
+
+  # if it can't be divided exactly so the last test will include the remaining tests.
+  if [[ $remaining -lt $total_tests ]]; then
+    suites+="\"${tests[@]:i:tests_n}\","
+  else
+    suites+="\"${tests[@]:i:remaining}\","
+    break
+  fi
 done
 
 suites="[${suites%?}]"
