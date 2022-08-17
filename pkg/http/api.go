b6585cf455aae2e6c25492b37981c58fec3c126b
Ryan Lettieri <ryanLettieri@microsoft.com>
2022-08-17 13:14:42 -0600

    Updates to workflows api to align with design document

--- pkg/http/api.go
+++ pkg/http/api.go
@@ -130,9 +130,9 @@ const (
 	secretStoreNameParam     = "secretStoreName"
 	secretNameParam          = "key"
 	nameParam                = "name"
-	workflowId               = "workflowId"
+	workflowComponent        = "workflowComponent"
+	workflowType             = "workflowType"
 	instanceId               = "instanceId"
-	workflowName             = "workflowName"
 	consistencyParam         = "consistency"
 	concurrencyParam         = "concurrency"
 	pubsubnameparam          = "pubsubname"
@@ -233,23 +233,26 @@ func (a *api) MarkStatusAsOutboundReady() {
 	a.outboundReadyStatus = true
 }
 
+// Workflow Component: Component specified in yaml (temporal, etc..)
+// Workflow Type: Name of the workflow to run (function name)
+// Instance ID: Identifier of the specific run
 func (a *api) constructWorkflowEndpoints() []Endpoint {
 	return []Endpoint{
 		{
 			Methods: []string{fasthttp.MethodGet},
-			Route:   "workflows/{workflowId}/{instanceId}",
+			Route:   "workflows/{workflowComponent}{workflowType}/{instanceId}",
 			Version: apiVersionV1alpha1,
 			Handler: a.onGetWorkflow,
 		},
 		{
 			Methods: []string{fasthttp.MethodPost},
-			Route:   "workflows/{workflowName}",
+			Route:   "workflows/{workflowComponent}{workflowType}/{instanceId}",
 			Version: apiVersionV1alpha1,
 			Handler: a.onStartWorkflow,
 		},
 		{
 			Methods: []string{fasthttp.MethodDelete},
-			Route:   "workflows/{workflowId}/{instanceId}",
+			Route:   "workflows/{workflowComponent}{workflowType}/{instanceId}",
 			Version: apiVersionV1alpha1,
 			Handler: a.onTerminateWorkflow,
 		},
@@ -733,7 +736,7 @@ func (a *api) getLockStoreWithRequestValidation(reqCtx *fasthttp.RequestCtx) (lo
 func (a *api) onStartWorkflow(reqCtx *fasthttp.RequestCtx) {
 	startReq := wfs.StartRequest{}
 
-	wfName := reqCtx.UserValue(workflowName).(string)
+	wfName := reqCtx.UserValue(workflowType).(string)
 	if wfName == "" {
 		log.Debug("No workflow, or empty workflow was provided.")
 		return
@@ -743,11 +746,12 @@ func (a *api) onStartWorkflow(reqCtx *fasthttp.RequestCtx) {
 	if err != nil {
 		log.Debug(err)
 	}
+
 	req := workflows.StartRequest{
-		WorkflowName: wfName,
-		Options:      startReq.Options,
-		Parameters:   startReq.Parameters,
+		Options:    startReq.Options,
+		Parameters: startReq.Parameters,
 	}
+	req.WorkflowInfo.InstanceId = reqCtx.UserValue(instanceId).(string)
 
 	resp, err := a.workFlows["workflows"].Start(reqCtx, &req)
 	if err != nil {
@@ -765,8 +769,8 @@ func (a *api) onStartWorkflow(reqCtx *fasthttp.RequestCtx) {
 func (a *api) onGetWorkflow(reqCtx *fasthttp.RequestCtx) {
 
 	req := workflows.WorkflowStruct{
-		WorkflowId:    reqCtx.UserValue(workflowId).(string),
-		WorkflowRunId: reqCtx.UserValue(instanceId).(string),
+		WorkflowId: reqCtx.UserValue(workflowType).(string),
+		InstanceId: reqCtx.UserValue(instanceId).(string),
 	}
 
 	resp, err := a.workFlows["workflows"].Get(reqCtx, &req)
@@ -784,8 +788,8 @@ func (a *api) onGetWorkflow(reqCtx *fasthttp.RequestCtx) {
 func (a *api) onTerminateWorkflow(reqCtx *fasthttp.RequestCtx) {
 
 	req := workflows.WorkflowStruct{
-		WorkflowId:    reqCtx.UserValue(workflowId).(string),
-		WorkflowRunId: reqCtx.UserValue(instanceId).(string),
+		WorkflowId: reqCtx.UserValue(workflowType).(string),
+		InstanceId: reqCtx.UserValue(instanceId).(string),
 	}
 	err := a.workFlows["workflows"].Terminate(reqCtx, &req)
 	if err != nil {
