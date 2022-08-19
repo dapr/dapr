96e4f0166b8ff058a1c7ee59e4051f44d2c52dac
Addison Juarez <adjuarez@microsoft.com>
2022-08-19 16:13:29 -0500

    brackets
    Signed-off-by: Addison Juarez <adjuarez@microsoft.com>

--- pkg/runtime/initError.go
+++ pkg/runtime/initError.go
@@ -33,10 +33,10 @@ type InitError struct {
 
 func (e *InitError) Error() string {
 	if e.entity == "" {
-		return fmt.Sprintf("(%s): %s", e.kind, e.err)
+		return fmt.Sprintf("[%s]: %s", e.kind, e.err)
 	}
 
-	return fmt.Sprintf("(%s): initialization error occurred for %s: %s", e.kind, e.entity, e.err)
+	return fmt.Sprintf("[%s]: initialization error occurred for %s: %s", e.kind, e.entity, e.err)
 }
 
 // NewInitError returns an InitError wrapping an existing context error.
