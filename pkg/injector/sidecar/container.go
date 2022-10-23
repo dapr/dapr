bc4a4b68b21cbd1aab75b6b2f15a4e2f3618d96d
Marcos Candeia <marrcooos@gmail.com>
2022-10-22 22:18:45 -0300

    Add pluggable component k8s annotation
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- pkg/injector/sidecar/container.go
+++ pkg/injector/sidecar/container.go
@@ -24,6 +24,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
+	"github.com/dapr/dapr/pkg/components/pluggable"
 	"github.com/dapr/dapr/pkg/injector/annotations"
 	authConsts "github.com/dapr/dapr/pkg/runtime/security/consts"
 	sentryConsts "github.com/dapr/dapr/pkg/sentry/consts"
@@ -34,26 +35,27 @@ import (
 
 // ContainerConfig contains the configuration for the sidecar container.
 type ContainerConfig struct {
-	AppID                       string
-	Annotations                 Annotations
-	CertChain                   string
-	CertKey                     string
-	ControlPlaneAddress         string
-	DaprSidecarImage            string
-	Identity                    string
-	IgnoreEntrypointTolerations []corev1.Toleration
-	ImagePullPolicy             corev1.PullPolicy
-	MTLSEnabled                 bool
-	Namespace                   string
-	PlacementServiceAddress     string
-	SentryAddress               string
-	SocketVolumeMount           *corev1.VolumeMount
-	TokenVolumeMount            *corev1.VolumeMount
-	Tolerations                 []corev1.Toleration
-	TrustAnchors                string
-	VolumeMounts                []corev1.VolumeMount
-	RunAsNonRoot                bool
-	ReadOnlyRootFilesystem      bool
+	AppID                        string
+	Annotations                  Annotations
+	CertChain                    string
+	CertKey                      string
+	ControlPlaneAddress          string
+	DaprSidecarImage             string
+	Identity                     string
+	IgnoreEntrypointTolerations  []corev1.Toleration
+	ImagePullPolicy              corev1.PullPolicy
+	MTLSEnabled                  bool
+	Namespace                    string
+	PlacementServiceAddress      string
+	SentryAddress                string
+	SocketVolumeMount            *corev1.VolumeMount
+	TokenVolumeMount             *corev1.VolumeMount
+	ComponentsSocketsVolumeMount *corev1.VolumeMount
+	Tolerations                  []corev1.Toleration
+	TrustAnchors                 string
+	VolumeMounts                 []corev1.VolumeMount
+	RunAsNonRoot                 bool
+	ReadOnlyRootFilesystem       bool
 }
 
 var (
@@ -266,6 +268,14 @@ func GetSidecarContainer(cfg ContainerConfig) (*corev1.Container, error) {
 
 	if cfg.SocketVolumeMount != nil {
 		container.VolumeMounts = []corev1.VolumeMount{*cfg.SocketVolumeMount}
+	}
+
+	if cfg.ComponentsSocketsVolumeMount != nil {
+		container.VolumeMounts = append(container.VolumeMounts, *cfg.ComponentsSocketsVolumeMount)
+		container.Env = append(container.Env, corev1.EnvVar{
+			Name:  pluggable.SocketFolderEnvVar,
+			Value: cfg.ComponentsSocketsVolumeMount.MountPath,
+		})
 	}
 
 	if cfg.TokenVolumeMount != nil {
