dea81fc658f96867cbb8e03bfd80e86884ad7efb
Bernd Verst <4535280+berndverst@users.noreply.github.com>
2022-10-25 14:55:28 -0700

    Update service bus topics import
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>

--- cmd/daprd/components/pubsub_azure_servicebus_topics.go
+++ cmd/daprd/components/pubsub_azure_servicebus_topics.go
@@ -14,10 +14,10 @@ limitations under the License.
 package components
 
 import (
-	"github.com/dapr/components-contrib/pubsub/azure/servicebus"
+	"github.com/dapr/components-contrib/pubsub/azure/servicebus/topics"
 	pubsubLoader "github.com/dapr/dapr/pkg/components/pubsub"
 )
 
 func init() {
-	pubsubLoader.DefaultRegistry.RegisterComponent(servicebus.NewAzureServiceBus, "azure.servicebus")
+	pubsubLoader.DefaultRegistry.RegisterComponent(topics.NewAzureServiceBusTopics, "azure.servicebus.topics", "azure.servicebus")
 }
