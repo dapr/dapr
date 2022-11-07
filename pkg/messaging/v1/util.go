ee4a9194e00d95e82eee722b22595173a06af9cb
sunzhaochang <zhchsun1992@gmail.com>
2022-11-07 16:10:55 +0800

    Add caller's and callee's app-ids in headers for service to service invocation
    Signed-off-by: sunzhaochang <zhchsun1992@gmail.com>

--- pkg/messaging/v1/util.go
+++ pkg/messaging/v1/util.go
@@ -65,6 +65,9 @@ const (
 	errorInfoDomain            = "dapr.io"
 	errorInfoHTTPCodeMetadata  = "http.code"
 	errorInfoHTTPErrorMetadata = "http.error_message"
+
+	CallerIDHeader = "caller-app-id"
+	CalleeIDHeader = "callee-app-id"
 )
 
 // DaprInternalMetadata is the metadata type to transfer HTTP header and gRPC metadata
