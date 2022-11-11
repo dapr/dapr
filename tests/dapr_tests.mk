25b12f38d06a28aaed8fe4ed83d88d900901bfc0
Shubham Sharma <shubhash@microsoft.com>
2022-11-11 10:13:25 +0530

    Add perf test
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -184,6 +184,8 @@ build-perf-app-$(1): check-e2e-env
 		--appdir "../$(E2E_TESTAPP_DIR)" \
 		--dest-registry "$(DAPR_TEST_REGISTRY)" \
 		--dest-tag "$(DAPR_TEST_TAG)" \
+		--target-os "$(TARGET_OS)" \
+		--target-arch "$(TARGET_ARCH)" \
 		--cache-registry "$(DAPR_CACHE_REGISTRY)"
 endef
 
