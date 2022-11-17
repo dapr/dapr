f39c94512a8eee8f30fbde5ba0f76468b6db0d3d
Joni Collinge <jonathancollinge@live.com>
2022-11-17 08:31:01 +0000

    add tests for tags
    Signed-off-by: Joni Collinge <jonathancollinge@live.com>
    Signed-off-by: TKTheTechie <thomas.kunnumpurath@solace.com>

--- pkg/diagnostics/service_monitoring.go
+++ pkg/diagnostics/service_monitoring.go
@@ -465,7 +465,6 @@ func (s *serviceMetrics) ServiceInvocationResponseReceived(sourceAppID, method s
 				methodKey, method,
 				statusKey, statusCode),
 			s.serviceInvocationResponseReceivedTotal.M(1))
-		elapsed := float64(time.Since(start) / time.Millisecond)
 		stats.RecordWithTags(
 			s.ctx,
 			diagUtils.WithTags(
