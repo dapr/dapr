9590274fdd876896d2203dd73e2562d3aa384d46
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-01-04 17:32:31 +0000

    Features in unit tests
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pkg/buildinfo/buildinfo.go
+++ pkg/buildinfo/buildinfo.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Dapr Authors
+Copyright 2023 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -23,10 +23,17 @@ var (
 
 	gitcommit, gitversion string
 	features              string
-	featuresSlice         []string
 )
 
+// Set by the init function
+var featuresSlice []string
+
 func init() {
+	if featuresSlice != nil {
+		// Return if another init method (e.g. for a build tag) already initialized
+		return
+	}
+
 	// At initialization, parse the value of "features" into a slice
 	if features == "" {
 		featuresSlice = []string{}
