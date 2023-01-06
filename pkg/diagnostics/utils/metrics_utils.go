adf3ab5dab64bc997f346c78afcdd2bac73bd5bc
Yaron Schneider <schneider.yaron@live.com>
2023-01-05 23:30:18 -0800

    Add metrics labels regex rules (#5716)
    * add metrics labels regex rules
    
    Signed-off-by: yaron2 <schneider.yaron@live.com>
    
    * linter
    
    Signed-off-by: yaron2 <schneider.yaron@live.com>
    
    * update header to correct year
    
    Signed-off-by: yaron2 <schneider.yaron@live.com>
    
    * linter
    
    Signed-off-by: yaron2 <schneider.yaron@live.com>
    
    Signed-off-by: yaron2 <schneider.yaron@live.com>

--- pkg/diagnostics/utils/metrics_utils.go
+++ pkg/diagnostics/utils/metrics_utils.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Dapr Authors
+Copyright 2023 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,12 +14,22 @@ limitations under the License.
 package utils
 
 import (
-	"context"
+	"regexp"
+	"strings"
 
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/tag"
+
+	"github.com/dapr/dapr/pkg/config"
 )
+
+var metricsRules map[string][]regexPair
+
+type regexPair struct {
+	regex   *regexp.Regexp
+	replace string
+}
 
 // NewMeasureView creates opencensus View instance using stats.Measure.
 func NewMeasureView(measure stats.Measure, keys []tag.Key, aggregation *view.Aggregation) *view.View {
@@ -35,7 +45,7 @@ func NewMeasureView(measure stats.Measure, keys []tag.Key, aggregation *view.Agg
 // WithTags converts tag key and value pairs to tag.Mutator array.
 // WithTags(key1, value1, key2, value2) returns
 // []tag.Mutator{tag.Upsert(key1, value1), tag.Upsert(key2, value2)}.
-func WithTags(opts ...interface{}) []tag.Mutator {
+func WithTags(name string, opts ...interface{}) []tag.Mutator {
 	tagMutators := []tag.Mutator{}
 	for i := 0; i < len(opts)-1; i += 2 {
 		key, ok := opts[i].(tag.Key)
@@ -48,26 +58,18 @@ func WithTags(opts ...interface{}) []tag.Mutator {
 		}
 		// skip if value is empty
 		if value != "" {
+			if len(metricsRules) > 0 {
+				pairs := metricsRules[strings.ReplaceAll(name, "_", "/")+key.Name()]
+
+				for _, p := range pairs {
+					value = p.regex.ReplaceAllString(value, p.replace)
+				}
+			}
+
 			tagMutators = append(tagMutators, tag.Upsert(key, value))
 		}
 	}
 	return tagMutators
-}
-
-// AddTagKeyToCtx assigns opencensus tag key value to context.
-func AddTagKeyToCtx(ctx context.Context, key tag.Key, value string) context.Context {
-	// return if value is not given
-	if value == "" {
-		return ctx
-	}
-
-	newCtx, err := tag.New(ctx, tag.Upsert(key, value))
-	if err != nil {
-		// return original if adding tagkey is failed.
-		return ctx
-	}
-
-	return newCtx
 }
 
 // AddNewTagKey adds new tag keys to existing view.
@@ -77,4 +79,33 @@ func AddNewTagKey(views []*view.View, key *tag.Key) []*view.View {
 	}
 
 	return views
+}
+
+// CreateRulesMap generates a fast lookup map for metrics regex.
+func CreateRulesMap(rules []config.MetricsRule) error {
+	metricsRules = map[string][]regexPair{}
+
+	for _, r := range rules {
+		// strip the metric name of known runtime prefixes and mutate them to fit stat names
+		r.Name = strings.Replace(r.Name, "dapr_", "", 1)
+		r.Name = strings.ReplaceAll(r.Name, "_", "/")
+
+		for _, l := range r.Labels {
+			metricsRules[r.Name+l.Name] = make([]regexPair, 0, len(l.Regex))
+
+			for k, v := range l.Regex {
+				regex, err := regexp.Compile(v)
+				if err != nil {
+					return err
+				}
+
+				metricsRules[r.Name+l.Name] = append(metricsRules[r.Name+l.Name], regexPair{
+					regex:   regex,
+					replace: k,
+				})
+			}
+		}
+	}
+
+	return nil
 }
