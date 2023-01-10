28b66172c5fe5ad3daae85db9fbfbe0bb2696b52
Ryan Lettieri <ryanLettieri@microsoft.com>
2023-01-10 15:17:14 -0700

    Updating temporal images to use github container registry
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -418,6 +418,10 @@ setup-test-env-temporal:
 					--set prometheus.enabled=false \
 					--set grafana.enabled=false \
 					--set elasticsearch.enabled=false \
+					--set temporalio/server=1.15.1 \
+					--set temporalio/web=1.14.0 \
+					--set cassandra=3.11.3 \
+					--set temporalio/admin-tools=1.15.1 \
 					dapr-temporal wener/temporal -f ./tests/config/temporal_override.yaml --namespace $(DAPR_TEST_NAMESPACE) --timeout 15m0s
 
 # delete temporal from cluster
