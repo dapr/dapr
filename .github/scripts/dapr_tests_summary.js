506dd7cffe8f35a3b5df255d738102e0811d79ff
Marcos Candeia <marrcooos@gmail.com>
2023-01-30 13:25:21 -0300

    Add some logs
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- .github/scripts/dapr_tests_summary.js
+++ .github/scripts/dapr_tests_summary.js
@@ -1,6 +1,8 @@
 module.exports = async ({ core, glob }) => {
     const globber = await glob.create(process.env["TEST_OUTPUT_FILE_PREFIX"] + "_summary_table_**.json")
+    console.log(process.env["TEST_OUTPUT_FILE_PREFIX"] + "_summary_table_**.json")
     for await (const file of globber.globGenerator()) {
+        console.log(file)
         const testSummary = require(file) // the file is a json file so we can import
         await core.summary
             .addHeading(testSummary.heading)
