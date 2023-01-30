5b137f506d06083747d248a8b0195f845c217d50
Marcos Candeia <marrcooos@gmail.com>
2023-01-30 18:19:38 -0300

    Use fs.readFile instead of require
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- .github/scripts/dapr_tests_summary.js
+++ .github/scripts/dapr_tests_summary.js
@@ -1,8 +1,9 @@
+const fs = require('fs')
 
 module.exports = async ({ glob, core }) => {
     const globber = await glob.create(process.env["TEST_OUTPUT_FILE_PREFIX"] + "_summary_table_*.json")
     for await (const file of globber.globGenerator()) {
-        const testSummary = require(file) // the file is a json file so we can import
+        const testSummary = JSON.parse(await fs.readFile(file, 'utf8'))
         await core.summary
             .addHeading(testSummary.test)
             .addTable(testSummary.data)
