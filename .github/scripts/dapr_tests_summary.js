5840c0245a9c994b9acdee854c764846d6a0342b
Marcos Candeia <marrcooos@gmail.com>
2023-01-30 18:32:10 -0300

    Use readfile sync
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- .github/scripts/dapr_tests_summary.js
+++ .github/scripts/dapr_tests_summary.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 module.exports = async ({ glob, core }) => {
     const globber = await glob.create(process.env["TEST_OUTPUT_FILE_PREFIX"] + "_summary_table_*.json")
     for await (const file of globber.globGenerator()) {
-        const testSummary = JSON.parse(await fs.readFile(file, 'utf8'))
+        const testSummary = JSON.parse(fs.readFileSync(file, 'utf8'))
         await core.summary
             .addHeading(testSummary.test)
             .addTable(testSummary.data)
