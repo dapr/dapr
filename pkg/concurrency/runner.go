bf0de5daadd178f8623fa86498e18cfcb155bff7
joshvanl <me@joshvanl.dev>
2023-01-31 17:10:06 +0000

    Adds review comments for concurrency
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/concurrency/runner.go
+++ pkg/concurrency/runner.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package concurrency
 
 import (
@@ -6,7 +19,10 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"sync/atomic"
 )
+
+var ErrManagerAlreadyStarted = errors.New("manager already started")
 
 // Runner is a function that runs a task.
 type Runner func(context.Context) error
@@ -19,25 +35,21 @@ type RunnerManager struct {
 	lock    sync.Mutex
 	wg      sync.WaitGroup
 	runners []Runner
-	running chan struct{}
+	running atomic.Bool
 }
 
 // NewRunnerManager creates a new RunnerManager.
 func NewRunnerManager(runners ...Runner) *RunnerManager {
 	return &RunnerManager{
 		runners: runners,
-		running: make(chan struct{}),
 	}
 }
 
 // Add adds a new runner to the RunnerManager.
 func (r *RunnerManager) Add(runner ...Runner) error {
-	select {
-	case <-r.running:
-		return errors.New("manager already started")
-	default:
+	if r.running.Load() {
+		return ErrManagerAlreadyStarted
 	}
-
 	r.lock.Lock()
 	defer r.lock.Unlock()
 	r.runners = append(r.runners, runner...)
@@ -48,12 +60,10 @@ func (r *RunnerManager) Add(runner ...Runner) error {
 // runner returns, the RunnerManager will stop all other runners and return any
 // error.
 func (r *RunnerManager) Run(ctx context.Context) error {
-	select {
-	case <-r.running:
-		return errors.New("manager already started")
-	default:
+	if r.running.Load() {
+		return ErrManagerAlreadyStarted
 	}
-	close(r.running)
+	r.running.Store(true)
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
