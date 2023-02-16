7bc7ca21e4e45295ea6b63b4770366834a9c3eb7
Filinto Duran <filinto@diagrid.io>
2023-02-16 17:44:50 -0600

    refactor imports
    Signed-off-by: Filinto Duran <filinto@diagrid.io>

--- pkg/operator/watchdog.go
+++ pkg/operator/watchdog.go
@@ -4,16 +4,13 @@ import (
 	"context"
 	"time"
 
-	"k8s.io/apimachinery/pkg/selection"
-
-	"k8s.io/apimachinery/pkg/types"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"go.uber.org/ratelimit"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/dapr/dapr/utils"
