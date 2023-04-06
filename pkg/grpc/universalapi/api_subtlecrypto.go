2ac82eead7a2688d47f830201f95d2598f523476
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-04-06 03:23:47 +0000

    Changed per review feedback
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pkg/grpc/universalapi/api_subtlecrypto.go
+++ pkg/grpc/universalapi/api_subtlecrypto.go
@@ -25,35 +25,35 @@ import (
 
 // SubtleGetKeyAlpha1 returns the public part of an asymmetric key stored in the vault.
 func (a *UniversalAPI) SubtleGetKeyAlpha1(ctx context.Context, in *runtimev1pb.SubtleGetKeyAlpha1Request) (*runtimev1pb.SubtleGetKeyAlpha1Response, error) {
-	return nil, messages.ErrAPIUnavailable
+	return nil, messages.ErrAPIUnimplemented
 }
 
 // SubtleEncryptAlpha1 encrypts a small message using a key stored in the vault.
 func (a *UniversalAPI) SubtleEncryptAlpha1(ctx context.Context, in *runtimev1pb.SubtleEncryptAlpha1Request) (*runtimev1pb.SubtleEncryptAlpha1Response, error) {
-	return nil, messages.ErrAPIUnavailable
+	return nil, messages.ErrAPIUnimplemented
 }
 
 // SubtleDecryptAlpha1 decrypts a small message using a key stored in the vault.
 func (a *UniversalAPI) SubtleDecryptAlpha1(ctx context.Context, in *runtimev1pb.SubtleDecryptAlpha1Request) (*runtimev1pb.SubtleDecryptAlpha1Response, error) {
-	return nil, messages.ErrAPIUnavailable
+	return nil, messages.ErrAPIUnimplemented
 }
 
 // SubtleWrapKeyAlpha1 wraps a key using a key stored in the vault.
 func (a *UniversalAPI) SubtleWrapKeyAlpha1(ctx context.Context, in *runtimev1pb.SubtleWrapKeyAlpha1Request) (*runtimev1pb.SubtleWrapKeyAlpha1Response, error) {
-	return nil, messages.ErrAPIUnavailable
+	return nil, messages.ErrAPIUnimplemented
 }
 
 // SubtleUnwrapKeyAlpha1 unwraps a key using a key stored in the vault.
 func (a *UniversalAPI) SubtleUnwrapKeyAlpha1(ctx context.Context, in *runtimev1pb.SubtleUnwrapKeyAlpha1Request) (*runtimev1pb.SubtleUnwrapKeyAlpha1Response, error) {
-	return nil, messages.ErrAPIUnavailable
+	return nil, messages.ErrAPIUnimplemented
 }
 
 // SubtleSignAlpha1 signs a message using a key stored in the vault.
 func (a *UniversalAPI) SubtleSignAlpha1(ctx context.Context, in *runtimev1pb.SubtleSignAlpha1Request) (*runtimev1pb.SubtleSignAlpha1Response, error) {
-	return nil, messages.ErrAPIUnavailable
+	return nil, messages.ErrAPIUnimplemented
 }
 
 // SubtleVerifyAlpha1 verifies the signature of a message using a key stored in the vault.
 func (a *UniversalAPI) SubtleVerifyAlpha1(ctx context.Context, in *runtimev1pb.SubtleVerifyAlpha1Request) (*runtimev1pb.SubtleVerifyAlpha1Response, error) {
-	return nil, messages.ErrAPIUnavailable
+	return nil, messages.ErrAPIUnimplemented
 }
--- pkg/messages/predefined.go
+++ pkg/messages/predefined.go
@@ -95,8 +95,8 @@ const (
 
 var (
 	// Generic.
-	ErrBadRequest     = APIError{"invalid request: %v", "ERR_BAD_REQUEST", http.StatusBadRequest, grpcCodes.InvalidArgument}
-	ErrAPIUnavailable = APIError{"this API is currently not available", "ERR_API_UNAVAILABLE", http.StatusNotImplemented, grpcCodes.Unimplemented}
+	ErrBadRequest       = APIError{"invalid request: %v", "ERR_BAD_REQUEST", http.StatusBadRequest, grpcCodes.InvalidArgument}
+	ErrAPIUnimplemented = APIError{"this API is currently not implemented", "ERR_API_UNIMPLEMENTED", http.StatusNotImplemented, grpcCodes.Unimplemented}
 
 	// State.
 	ErrStateStoresNotConfigured = APIError{"state store is not configured", "ERR_STATE_STORE_NOT_CONFIGURED", http.StatusInternalServerError, grpcCodes.FailedPrecondition}
