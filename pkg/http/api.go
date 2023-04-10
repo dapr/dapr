18bc50aa46ada72ad245309795a5fba24874899f
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2023-04-09 22:28:22 -0700

    [Crypto] Add SubtleCrypto APIs for HTTP and gRPC (#6137)
    * Add SubtleCrypto APIs for HTTP and gRPC
    
    Implements the Subtle Crypto APIs (low-level) as defined in dapr/proposals#3
    
    These APIs are implemented as universal APIs. They include support for resiliency and metrics.
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Gate APIs behind a build-time flag
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Changed per review feedback
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Make CryptoValidateRequest available also in the included module (needed by high-level API)
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * 💄
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * 💄
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    Co-authored-by: Mukundan Sundararajan <65565396+mukundansundar@users.noreply.github.com>

--- pkg/http/api.go
+++ pkg/http/api.go
@@ -233,6 +233,7 @@ func NewAPI(opts APIOpts) API {
 	api.endpoints = append(api.endpoints, api.constructShutdownEndpoints()...)
 	api.endpoints = append(api.endpoints, api.constructBindingsEndpoints()...)
 	api.endpoints = append(api.endpoints, api.constructConfigurationEndpoints()...)
+	api.endpoints = append(api.endpoints, api.constructSubtleCryptoEndpoints()...)
 	api.endpoints = append(api.endpoints, healthEndpoints...)
 	api.endpoints = append(api.endpoints, api.constructDistributedLockEndpoints()...)
 	api.endpoints = append(api.endpoints, api.constructWorkflowEndpoints()...)
