bc9bcf328cbf72f18ea922ba2b7e9a779ff3b685
ashiquemd <noorani.ashique5@gmail.com>
2023-04-12 09:51:29 +0530

    Fixed merge conflict
    Signed-off-by: ashiquemd <noorani.ashique5@gmail.com>

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -545,9 +545,6 @@ setup-test-components: setup-app-configurations
 	$(KUBECTL) apply -f ./tests/config/dapr_tracing_config.yaml --namespace $(DAPR_TEST_NAMESPACE)
 	$(KUBECTL) apply -f ./tests/config/dapr_cron_binding.yaml --namespace $(DAPR_TEST_NAMESPACE)
 
-	# TODO: Remove once AppHealthCheck feature is finalized
-	$(KUBECTL) apply -f ./tests/config/app_healthcheck.yaml --namespace $(DAPR_TEST_NAMESPACE)
-
 	# Show the installed components
 	$(KUBECTL) get components --namespace $(DAPR_TEST_NAMESPACE)
 
