f3aa3bed18637f2a5120d1301db2fec35ab494b0
joshvanl <me@joshvanl.dev>
2023-06-01 15:22:24 +0100

    Adds third party URI path normalization, removing segment decoding
    Signed-off-by: joshvanl <me@joshvanl.dev>

---
+++ utils/nethttpadaptor/uri/uri_unix.go
@@ -0,0 +1,13 @@
+//go:build !windows
+// +build !windows
+
+package uri
+
+func addLeadingSlash(dst, src []byte) []byte {
+	// add leading slash for unix paths
+	if len(src) == 0 || src[0] != '/' {
+		dst = append(dst, '/')
+	}
+
+	return dst
+}
