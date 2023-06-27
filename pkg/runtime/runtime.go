164762265b1347242e9ec5310815a1c32b85971e
Anton Troshin <anton@diagrid.io>
2023-06-27 17:07:03 -0500

    Fix lint issue
    Signed-off-by: Anton Troshin <anton@diagrid.io>

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -2548,7 +2548,7 @@ func (a *DaprRuntime) initActors() error {
 		HealthHTTPClient:   a.appHTTPClient,
 		HealthEndpoint:     a.getAppHTTPEndpoint(),
 		AppChannelAddress:  a.runtimeConfig.appConnectionConfig.ChannelAddress,
-    PodName:            a.getPodName(),
+		PodName:            a.getPodName(),
 	})
 
 	act := actors.NewActors(actors.ActorsOpts{
