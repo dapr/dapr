df4a9dccf6e685827f2a56421ca74131ca287e45
joshvanl <me@joshvanl.dev>
2023-08-21 18:17:50 -0600

    Read Trust Anchors file in full rather than open and io.ReadAll
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/security/x509source.go
+++ pkg/security/x509source.go
@@ -24,7 +24,6 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -98,10 +97,8 @@ func newX509Source(ctx context.Context, clock clock.Clock, cptd spiffeid.TrustDo
 	rootPEMs := opts.TrustAnchors
 
 	if len(rootPEMs) == 0 {
-		var err error
-		var f *os.File
 		for {
-			f, err = os.Open(opts.TrustAnchorsFile)
+			_, err := os.Stat(opts.TrustAnchorsFile)
 			if err == nil {
 				break
 			}
@@ -120,10 +117,10 @@ func newX509Source(ctx context.Context, clock clock.Clock, cptd spiffeid.TrustDo
 
 		log.Infof("Trust anchors file '%s' found", opts.TrustAnchorsFile)
 
-		defer f.Close()
-		rootPEMs, err = io.ReadAll(f)
+		var err error
+		rootPEMs, err = os.ReadFile(opts.TrustAnchorsFile)
 		if err != nil {
-			return nil, fmt.Errorf("failed to read trust anchors file %q: %w", opts.TrustAnchorsFile, err)
+			return nil, fmt.Errorf("failed to read trust anchors file '%s': %w", opts.TrustAnchorsFile, err)
 		}
 	}
 
