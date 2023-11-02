19e2995044eb371195bb1facbfa871244fc8ce23
Phillip Hoff <phillip@orst.edu>
2023-11-02 12:33:12 -0700

    Add robustness to monitoring of workflow URLs. (#7094)
    * Add robustness to monitoring of workflow URLs.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Update formatting.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    ---------
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    Co-authored-by: Mukundan Sundararajan <65565396+mukundansundar@users.noreply.github.com>
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- pkg/diagnostics/http_monitoring.go
+++ pkg/diagnostics/http_monitoring.go
@@ -17,6 +17,7 @@ import (
 	"context"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"go.opencensus.io/stats"
@@ -236,4 +237,76 @@ func (h *httpMetrics) HTTPMiddleware(next http.Handler) http.Handler {
 		// Record the request
 		h.ServerRequestCompleted(r.Context(), method, status, reqContentSize, respSize, elapsed)
 	})
+}
+
+// convertPathToMetricLabel removes the variant parameters in URL path for low cardinality label space
+// For example, it removes {keys} param from /v1/state/statestore/{keys}.
+func (h *httpMetrics) convertPathToMetricLabel(path string) string {
+	if path == "" {
+		return path
+	}
+
+	p := path
+	if p[0] == '/' {
+		p = path[1:]
+	}
+
+	// Split up to 6 delimiters in 'v1/actors/DemoActor/1/timer/name'
+	parsedPath := strings.SplitN(p, "/", 6)
+
+	if len(parsedPath) < 3 {
+		return path
+	}
+
+	// Replace actor id with {id} for appcallback url - 'actors/DemoActor/1/method/method1'
+	if parsedPath[0] == "actors" {
+		parsedPath[2] = "{id}"
+		return strings.Join(parsedPath, "/")
+	}
+
+	switch parsedPath[1] {
+	case "state", "secrets":
+		// state api: Concat 3 items(v1, state, statestore) in /v1/state/statestore/key
+		// secrets api: Concat 3 items(v1, secrets, keyvault) in /v1/secrets/keyvault/name
+		return "/" + strings.Join(parsedPath[0:3], "/")
+
+	case "actors":
+		if len(parsedPath) < 5 {
+			return path
+		}
+		// ignore id part
+		parsedPath[3] = "{id}"
+		// Concat 5 items(v1, actors, DemoActor, {id}, timer) in /v1/actors/DemoActor/1/timer/name
+		return "/" + strings.Join(parsedPath[0:5], "/")
+	case "workflows":
+		if len(parsedPath) < 4 {
+			return path
+		}
+
+		// v1.0-alpha1/workflows/<workflowComponentName>/<instanceId>
+		if len(parsedPath) == 4 {
+			parsedPath[3] = "{instanceId}"
+			return "/" + strings.Join(parsedPath[0:4], "/")
+		}
+
+		// v1.0-alpha1/workflows/<workflowComponentName>/<workflowName>/start[?instanceID=<instanceID>]
+		if len(parsedPath) == 5 && parsedPath[4] != "" && strings.HasPrefix(parsedPath[4], "start") {
+			// not obfuscating the workflow name, just the possible instanceID
+			return "/" + strings.Join(parsedPath[0:4], "/") + "/start"
+		} else {
+			// v1.0-alpha1/workflows/<workflowComponentName>/<instanceId>/terminate
+			// v1.0-alpha1/workflows/<workflowComponentName>/<instanceId>/pause
+			// v1.0-alpha1/workflows/<workflowComponentName>/<instanceId>/resume
+			// v1.0-alpha1/workflows/<workflowComponentName>/<instanceId>/purge
+			parsedPath[3] = "{instanceId}"
+			// v1.0-alpha1/workflows/<workflowComponentName>/<instanceID>/raiseEvent/<eventName>
+			if len(parsedPath) == 6 && parsedPath[4] == "raiseEvent" && parsedPath[5] != "" {
+				parsedPath[5] = "{eventName}"
+				return "/" + strings.Join(parsedPath[0:6], "/")
+			}
+		}
+		return "/" + strings.Join(parsedPath[0:5], "/")
+	}
+
+	return path
 }
--- pkg/diagnostics/http_monitoring_test.go
+++ pkg/diagnostics/http_monitoring_test.go
@@ -87,6 +87,49 @@ func TestHTTPMiddlewareWhenMetricsDisabled(t *testing.T) {
 	assert.Nil(t, rows)
 }
 
+func TestConvertPathToMethodName(t *testing.T) {
+	convertTests := []struct {
+		in  string
+		out string
+	}{
+		{"/v1/state/statestore/key", "/v1/state/statestore"},
+		{"/v1/state/statestore", "/v1/state/statestore"},
+		{"/v1/secrets/keyvault/name", "/v1/secrets/keyvault"},
+		{"/v1/publish/topic", "/v1/publish/topic"},
+		{"/v1/bindings/kafka", "/v1/bindings/kafka"},
+		{"/healthz", "/healthz"},
+		{"/v1/actors/DemoActor/1/state/key", "/v1/actors/DemoActor/{id}/state"},
+		{"/v1/actors/DemoActor/1/reminder/name", "/v1/actors/DemoActor/{id}/reminder"},
+		{"/v1/actors/DemoActor/1/timer/name", "/v1/actors/DemoActor/{id}/timer"},
+		{"/v1/actors/DemoActor/1/timer/name?query=string", "/v1/actors/DemoActor/{id}/timer"},
+		{"v1/actors/DemoActor/1/timer/name", "/v1/actors/DemoActor/{id}/timer"},
+		{"actors/DemoActor/1/method/method1", "actors/DemoActor/{id}/method/method1"},
+		{"actors/DemoActor/1/method/timer/timer1", "actors/DemoActor/{id}/method/timer/timer1"},
+		{"actors/DemoActor/1/method/remind/reminder1", "actors/DemoActor/{id}/method/remind/reminder1"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/mywf/start?instanceID=1234", "/v1.0-alpha1/workflows/workflowComponentName/mywf/start"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/mywf/start", "/v1.0-alpha1/workflows/workflowComponentName/mywf/start"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234/start/value1/value2", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}/start"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234/terminate", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}/terminate"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234/terminate/value1/value2", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}/terminate"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234/raiseEvent/foobaz", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}/raiseEvent/{eventName}"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234/pause", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}/pause"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234/resume", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}/resume"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234/purge", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}/purge"},
+		{"/v1.0-alpha1/workflows/workflowComponentName/1234", "/v1.0-alpha1/workflows/workflowComponentName/{instanceId}"},
+		{"/v1.0-alpha1/workflows/workflowComponentName", "/v1.0-alpha1/workflows/workflowComponentName"},
+		{"/v1.0-alpha1/workflows", "/v1.0-alpha1/workflows"},
+		{"", ""},
+	}
+
+	testHTTP := newHTTPMetrics()
+	for _, tt := range convertTests {
+		t.Run(tt.in, func(t *testing.T) {
+			lowCardinalityName := testHTTP.convertPathToMetricLabel(tt.in)
+			assert.Equal(t, tt.out, lowCardinalityName)
+		})
+	}
+}
+
 func fakeHTTPRequest(body string) *http.Request {
 	req, err := http.NewRequest(http.MethodPost, "http://dapr.io/invoke/method/testmethod", strings.NewReader(body))
 	if err != nil {
