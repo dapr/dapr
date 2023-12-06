0c083196f91152d77110b393759f83a1029b7e47
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2023-12-06 23:03:04 +0530

    Scheduler Service bare bone
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

---
+++ cmd/scheduler/main.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dapr/dapr/cmd/scheduler/options"
+	"github.com/dapr/dapr/pkg/buildinfo"
+	"github.com/dapr/dapr/pkg/health"
+	"github.com/dapr/dapr/pkg/metrics"
+	"github.com/dapr/dapr/pkg/modes"
+	"github.com/dapr/dapr/pkg/placement/monitoring"
+	"github.com/dapr/dapr/pkg/scheduler/server"
+	"github.com/dapr/dapr/pkg/security"
+	"github.com/dapr/kit/concurrency"
+	"github.com/dapr/kit/logger"
+	"github.com/dapr/kit/signals"
+)
+
+var log = logger.NewLogger("dapr.scheduler")
+
+func main() {
+	opts := options.New()
+
+	// Apply options to all loggers.
+	if err := logger.ApplyOptionsToLoggers(&opts.Logger); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Infof("Starting Dapr Scheduler Service -- version %s -- commit %s", buildinfo.Version(), buildinfo.Commit())
+	log.Infof("Log level set to: %s", opts.Logger.OutputLevel)
+
+	metricsExporter := metrics.NewExporterWithOptions(log, metrics.DefaultMetricNamespace, opts.Metrics)
+
+	err := monitoring.InitMetrics()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := signals.Context()
+	secProvider, err := security.New(ctx, security.Options{
+		SentryAddress:           opts.SentryAddress,
+		ControlPlaneTrustDomain: opts.TrustDomain,
+		ControlPlaneNamespace:   security.CurrentNamespace(),
+		TrustAnchorsFile:        opts.TrustAnchorsFile,
+		AppID:                   "dapr-scheduler",
+		MTLSEnabled:             opts.TLSEnabled,
+		Mode:                    modes.DaprMode(opts.Mode),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = concurrency.NewRunnerManager(
+		metricsExporter.Run,
+		secProvider.Run,
+		func(ctx context.Context) error {
+			_, serr := secProvider.Handler(ctx)
+			if serr != nil {
+				return serr
+			}
+			return server.Start(ctx, server.SchedulerServiceOpts{})
+		},
+		func(ctx context.Context) error {
+			healthzServer := health.NewServer(log)
+			healthzServer.Ready()
+			if healthzErr := healthzServer.Run(ctx, opts.HealthzPort); healthzErr != nil {
+				return fmt.Errorf("failed to start healthz server: %w", healthzErr)
+			}
+			return nil
+		},
+	).Run(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info("Scheduler service shut down gracefully")
+}
---
+++ cmd/scheduler/options/options.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package options
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/dapr/dapr/pkg/metrics"
+	"github.com/dapr/dapr/pkg/modes"
+	"github.com/dapr/dapr/pkg/security"
+	securityConsts "github.com/dapr/dapr/pkg/security/consts"
+	"github.com/dapr/dapr/pkg/sentry/config"
+	"github.com/dapr/kit/logger"
+)
+
+const (
+	//nolint:gosec
+	defaultCredentialsPath = "/var/run/secrets/dapr.io/credentials"
+
+	// defaultDaprSystemConfigName is the default resource object name for Dapr System Config.
+	defaultDaprSystemConfigName = "daprsystem"
+)
+
+type Options struct {
+	Port        int
+	HealthzPort int
+
+	TLSEnabled       bool
+	TrustDomain      string
+	TrustAnchorsFile string
+	SentryAddress    string
+	Mode             string
+
+	Logger  logger.Options
+	Metrics *metrics.Options
+}
+
+func New() *Options {
+	var opts Options
+
+	flag.IntVar(&opts.Port, "port", config.DefaultPort, "The port for the scheduler server to listen on")
+	flag.IntVar(&opts.HealthzPort, "healthz-port", 8080, "The port for the healthz server to listen on")
+
+	flag.BoolVar(&opts.TLSEnabled, "tls-enabled", false, "Should TLS be enabled for the scheduler gRPC server")
+	flag.StringVar(&opts.TrustDomain, "trust-domain", "localhost", "Trust domain for the Dapr control plane")
+	flag.StringVar(&opts.TrustAnchorsFile, "trust-anchors-file", securityConsts.ControlPlaneDefaultTrustAnchorsPath, "Filepath to the trust anchors for the Dapr control plane")
+	flag.StringVar(&opts.SentryAddress, "sentry-address", fmt.Sprintf("dapr-sentry.%s.svc:443", security.CurrentNamespace()), "Address of the Sentry service")
+	flag.StringVar(&opts.Mode, "mode", string(modes.StandaloneMode), "Runtime mode for Placement")
+
+	opts.Logger = logger.DefaultOptions()
+	opts.Logger.AttachCmdFlags(flag.StringVar, flag.BoolVar)
+
+	opts.Metrics = metrics.DefaultMetricOptions()
+	opts.Metrics.AttachCmdFlags(flag.StringVar, flag.BoolVar)
+
+	flag.Parse()
+
+	return &opts
+}
