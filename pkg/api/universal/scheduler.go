86883b925c6bf50786573fe421101f1b56c49ae7
Cassandra Coyle <cassie@diagrid.io>
2023-12-07 18:36:04 -0600

    call scheduler apis
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- pkg/api/universal/scheduler.go
+++ pkg/api/universal/scheduler.go
@@ -15,8 +15,8 @@ package universal
 
 import (
 	"context"
-	"errors"
 	runtimev1pb "github.com/dapr/dapr/pkg/proto/runtime/v1"
+	schedulerv1pb "github.com/dapr/dapr/pkg/proto/scheduler/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -25,33 +25,100 @@ import (
 func (a *Universal) ScheduleJob(ctx context.Context, inReq *runtimev1pb.ScheduleJobRequest) (*emptypb.Empty, error) {
 	//validate job details, date, schedule, etc??
 	var err error
-	//req := scheduler.ScheduleJobRequest{
-	//	Job: scheduler.Job{
-	//		Name:     inReq.GetJob().GetName(),
-	//		Schedule: inReq.GetJob().GetSchedule(),
-	//		Data:     inReq.GetJob().GetData(),
-	//		Repeats:  inReq.GetJob().GetRepeats(),
-	//		DueTime:  inReq.GetJob().GetDueTime(),
-	//		TTL:      inReq.GetJob().GetTtl(),
-	//	},
-	//}
+
+	internalScheduleJobReq := &schedulerv1pb.ScheduleJobRequest{
+		Job: &runtimev1pb.Job{
+			Name:     inReq.GetJob().GetName(),
+			Schedule: inReq.GetJob().GetSchedule(),
+			Data:     inReq.GetJob().GetData(),
+			Repeats:  inReq.GetJob().GetRepeats(),
+			DueTime:  inReq.GetJob().GetDueTime(),
+			Ttl:      inReq.GetJob().GetTtl(),
+		},
+		Namespace: "",  //TODO
+		Metadata:  nil, //TODO: this should I think generate key if jobStateStore is configured
+	}
+
+	//TODO: do something with following response?
+	_, err = a.schedulerClient.ScheduleJob(ctx, internalScheduleJobReq)
+	if err != nil {
+		a.logger.Errorf("Error Scheduling job %v", err)
+		return &emptypb.Empty{}, err
+	}
 
 	return &emptypb.Empty{}, err
 }
 
 func (a *Universal) DeleteJob(ctx context.Context, inReq *runtimev1pb.DeleteJobRequest) (*emptypb.Empty, error) {
+	var err error
 
-	return &emptypb.Empty{}, errors.New("DeleteJob is unimplemented in universal api")
+	if inReq.GetName() == "" {
+		a.logger.Errorf("Job name empty %v", err)
+		return &emptypb.Empty{}, err
+	}
+
+	internalDeleteJobReq := &schedulerv1pb.JobRequest{
+		JobName: inReq.GetName(),
+	}
+
+	//TODO: do something with following response?
+	_, err = a.schedulerClient.DeleteJob(ctx, internalDeleteJobReq)
+	if err != nil {
+		a.logger.Errorf("Error Deleting job %v", err)
+		return &emptypb.Empty{}, err
+	}
+
+	return &emptypb.Empty{}, err
 }
 
 func (a *Universal) GetJob(ctx context.Context, inReq *runtimev1pb.GetJobRequest) (*runtimev1pb.GetJobResponse, error) {
 	var response *runtimev1pb.GetJobResponse
+	var internalResp *schedulerv1pb.GetJobResponse
+	var err error
 
-	return response, errors.New("GetJob is unimplemented in universal api")
+	if inReq.GetName() == "" {
+		a.logger.Errorf("Job name empty %v", err)
+		return response, err
+	}
+
+	internalGetJobReq := &schedulerv1pb.JobRequest{
+		JobName: inReq.GetName(),
+	}
+
+	internalResp, err = a.schedulerClient.GetJob(ctx, internalGetJobReq)
+	if err != nil {
+		a.logger.Errorf("Error Getting job %v", err)
+		return response, err
+	}
+
+	response.Job = internalResp.Job
+
+	return response, err
 }
 
 func (a *Universal) ListJobs(ctx context.Context, inReq *runtimev1pb.ListJobsRequest) (*runtimev1pb.ListJobsResponse, error) {
 	var response *runtimev1pb.ListJobsResponse
+	var internalListResp *schedulerv1pb.ListJobsResponse
+	var err error
 
-	return response, errors.New("ListJobs is unimplemented in universal api")
+	if inReq.GetAppId() == "" {
+		a.logger.Errorf("Job appID empty %v", err)
+		return response, err
+	}
+
+	internalListReq := &schedulerv1pb.ListJobsRequest{
+		AppId: inReq.GetAppId(),
+	}
+
+	internalListResp, err = a.schedulerClient.ListJobs(ctx, internalListReq)
+	if err != nil {
+		a.logger.Errorf("Error Listing jobs for app %v", err)
+		return response, err
+	}
+
+	if len(internalListResp.GetJobs()) > 0 {
+		response.Jobs = internalListResp.Jobs
+	}
+
+	return response, err
 }
