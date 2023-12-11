1771c5c525b936e6813393a328ae3ce8dc1d2b3c
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-12-11 17:42:29 +0000

    Should fix integration tests
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- cmd/injector/options/options.go
+++ cmd/injector/options/options.go
@@ -30,8 +30,23 @@ type Options struct {
 	Metrics     *metrics.Options
 }
 
-func New(args []string) *Options {
+func New(origArgs []string) *Options {
 	var opts Options
+
+	// We are using pflag to parse the CLI flags
+	// pflag is a drop-in replacement for the standard library's "flag" package, however…
+	// There's one key difference: with the stdlib's "flag" package, there are no short-hand options so options can be defined with a single slash (such as "daprd -mode").
+	// With pflag, single slashes are reserved for shorthands.
+	// So, we are doing this thing where we iterate through all args and double-up the slash if it's single
+	// This works *as long as* we don't start using shorthand flags (which haven't been in use so far).
+	args := make([]string, len(origArgs))
+	for i, a := range origArgs {
+		if len(a) > 2 && a[0] == '-' && a[1] != '-' {
+			args[i] = "-" + a
+		} else {
+			args[i] = a
+		}
+	}
 
 	// Create a flag set
 	fs := pflag.NewFlagSet("sentry", pflag.ExitOnError)
--- cmd/sentry/options/options.go
+++ cmd/sentry/options/options.go
@@ -47,7 +47,22 @@ type Options struct {
 	IssuerKeyFilename  string
 }
 
-func New(args []string) *Options {
+func New(origArgs []string) *Options {
+	// We are using pflag to parse the CLI flags
+	// pflag is a drop-in replacement for the standard library's "flag" package, however…
+	// There's one key difference: with the stdlib's "flag" package, there are no short-hand options so options can be defined with a single slash (such as "daprd -mode").
+	// With pflag, single slashes are reserved for shorthands.
+	// So, we are doing this thing where we iterate through all args and double-up the slash if it's single
+	// This works *as long as* we don't start using shorthand flags (which haven't been in use so far).
+	args := make([]string, len(origArgs))
+	for i, a := range origArgs {
+		if len(a) > 2 && a[0] == '-' && a[1] != '-' {
+			args[i] = "-" + a
+		} else {
+			args[i] = a
+		}
+	}
+
 	var opts Options
 
 	// Create a flag set
