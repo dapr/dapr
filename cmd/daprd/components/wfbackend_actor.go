e93a05a8f6eb87455a918e8b20f7b0a00faf227e
MD Ashique <noorani.ashique5@gmail.com>
2023-12-18 16:43:45 +0530

    fix unit tests
    Signed-off-by: MD Ashique <noorani.ashique5@gmail.com>

--- cmd/daprd/components/wfbackend_actor.go
+++ cmd/daprd/components/wfbackend_actor.go
@@ -1,7 +1,7 @@
 //go:build allcomponents
 
 /*
-Copyright 2021 The Dapr Authors
+Copyright 2023 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
