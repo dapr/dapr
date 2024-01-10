e03ed62a6423aad31ddad0e7bbc9efd6e80ed46a
Cassandra Coyle <cassie@diagrid.io>
2024-01-10 15:56:27 -0700

    wip, tweak etcd loglevel, tweaks to apis
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- pkg/scheduler/server/config.go
+++ pkg/scheduler/server/config.go
@@ -35,12 +35,15 @@ func parseEtcdUrls(strs []string) ([]url.URL, error) {
 
 func conf() *embed.Config {
 	config := embed.NewConfig()
-	config.Name = "localhost"
+	config.Name = "scheduler-etcd"
 	config.Dir = "/tmp/my-embedded-ectd-cluster"
 	// config.LPUrls = parseEtcdUrls([]string{"http://0.0.0.0:2380"})
 	// config.LCUrls = parseEtcdUrls([]string{"http://0.0.0.0:2379"})
 	// config.APUrls = parseEtcdUrls([]string{"http://localhost:2380"})
 	// config.ACUrls = parseEtcdUrls([]string{"http://localhost:2379"})
 	config.InitialCluster = "localhost=http://localhost:2380"
+
+	config.LogLevel = "error" // Only supports debug, info, warn, error, panic, or fatal. Default 'info'.
+	//TODO: Look into etcd config and if we need to do any raft compacting
 	return config
 }
