f440f9d164ac0b5f2c758476ca440c9fc2406641
Elena Kolevska <elena@kolevska.com>
2024-01-10 14:34:42 +0000

    Removes log line
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- pkg/placement/membership.go
+++ pkg/placement/membership.go
@@ -384,7 +384,6 @@ func (p *Service) performTablesUpdate(ctx context.Context, hosts []placementGRPC
 		return fmt.Errorf("dissemination of 'unlock' failed: %v", err)
 	}
 
-	log.Debugf("\n\n\n-------------------------- %v\n\n\n", p.clock.Since(startedAt))
 	logfile, _ := os.OpenFile("/Users/elenakolevska/placement-work/dissemination_time_with_vnodes.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	logfile.WriteString(fmt.Sprintf("%d, %d, %d\n", len(hosts), len(newTable.GetEntries()), p.clock.Since(startedAt).Microseconds()))
 	logfile.Close()
