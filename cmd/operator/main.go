7002814a84af8e7f1b8a4688bd2a7349681b72e2
Josh van Leeuwen <me@joshvanl.dev>
2024-01-12 20:38:51 +0000

    Move all main implementation into app package for all binaries (#7106)
    * Move all main implementation into app package for all binaries
    
    Because in Go you cannot import the main package, it makes it difficult
    in integration tests to build from source the dapr binaries from the
    previous version. With this change, in v1.14 we can import the app
    package and build the binaries from source for v1.13.
    
    PR updates binaries to have a minimal main package that calls into the
    app package. PR has no functional changes.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Move options and components to original package location
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Updates copyright to 2023
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    Co-authored-by: Dapr Bot <56698301+dapr-bot@users.noreply.github.com>

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Dapr Authors
+Copyright 2023 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,61 +14,9 @@ limitations under the License.
 package main
 
 import (
-	"github.com/dapr/dapr/cmd/operator/options"
-	"github.com/dapr/dapr/pkg/buildinfo"
-	"github.com/dapr/dapr/pkg/metrics"
-	"github.com/dapr/dapr/pkg/operator"
-	"github.com/dapr/dapr/pkg/operator/monitoring"
-	"github.com/dapr/kit/concurrency"
-	"github.com/dapr/kit/logger"
-	"github.com/dapr/kit/signals"
+	"github.com/dapr/dapr/cmd/operator/app"
 )
 
-var log = logger.NewLogger("dapr.operator")
-
 func main() {
-	opts := options.New()
-
-	// Apply options to all loggers.
-	if err := logger.ApplyOptionsToLoggers(&opts.Logger); err != nil {
-		log.Fatal(err)
-	}
-
-	log.Infof("Starting Dapr Operator -- version %s -- commit %s", buildinfo.Version(), buildinfo.Commit())
-	log.Infof("Log level set to: %s", opts.Logger.OutputLevel)
-
-	metricsExporter := metrics.NewExporterWithOptions(log, metrics.DefaultMetricNamespace, opts.Metrics)
-
-	if err := monitoring.InitMetrics(); err != nil {
-		log.Fatal(err)
-	}
-
-	ctx := signals.Context()
-	op, err := operator.NewOperator(ctx, operator.Options{
-		Config:                              opts.Config,
-		TrustAnchorsFile:                    opts.TrustAnchorsFile,
-		LeaderElection:                      !opts.DisableLeaderElection,
-		WatchdogMaxRestartsPerMin:           opts.MaxPodRestartsPerMinute,
-		WatchNamespace:                      opts.WatchNamespace,
-		ServiceReconcilerEnabled:            !opts.DisableServiceReconciler,
-		ArgoRolloutServiceReconcilerEnabled: opts.EnableArgoRolloutServiceReconciler,
-		WatchdogEnabled:                     opts.WatchdogEnabled,
-		WatchdogInterval:                    opts.WatchdogInterval,
-		WatchdogCanPatchPodLabels:           opts.WatchdogCanPatchPodLabels,
-		APIPort:                             opts.APIPort,
-		HealthzPort:                         opts.HealthzPort,
-	})
-	if err != nil {
-		log.Fatalf("error creating operator: %v", err)
-	}
-
-	err = concurrency.NewRunnerManager(
-		metricsExporter.Run,
-		op.Run,
-	).Run(ctx)
-	if err != nil {
-		log.Fatalf("error running operator: %v", err)
-	}
-
-	log.Info("operator shut down gracefully")
+	app.Run()
 }
