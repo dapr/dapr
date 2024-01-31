31deca722fba91737198aa08ffcaf61e22519713
Elena Kolevska <elena@kolevska.com>
2024-01-31 00:11:56 +0000

    Adds unit tests for the updated cluster api level calculation
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- pkg/placement/raft/state_test.go
+++ pkg/placement/raft/state_test.go
@@ -301,48 +301,97 @@ func TestRestoreHashingTables(t *testing.T) {
 }
 
 func TestUpdateAPILevel(t *testing.T) {
-	// arrange
-	s := newDaprHostMemberState(DaprHostMemberStateConfig{
-		replicationFactor: 10,
-		minAPILevel:       0,
-		maxAPILevel:       100,
-	})
-	s.upsertMember(&DaprHostMember{
-		Name:      "127.0.0.1:8080",
-		AppID:     "FakeID1",
-		Entities:  []string{"actorTypeOne", "actorTypeTwo"},
-		UpdatedAt: 1,
-		APILevel:  10,
-	})
-	s.upsertMember(&DaprHostMember{
-		Name:      "127.0.0.1:8081",
-		AppID:     "FakeID2",
-		Entities:  []string{"actorTypeThree", "actorTypeFour"},
-		UpdatedAt: 2,
-		APILevel:  20,
-	})
-	s.upsertMember(&DaprHostMember{
-		Name:      "127.0.0.1:8082",
-		AppID:     "FakeID3",
-		Entities:  []string{"actorTypeFive"},
-		UpdatedAt: 3,
-		APILevel:  30,
+	t.Run("no min nor max api levels arguments", func(t *testing.T) {
+		s := newDaprHostMemberState(DaprHostMemberStateConfig{
+			replicationFactor: 10,
+			minAPILevel:       0,
+			maxAPILevel:       100,
+		})
+
+		s.upsertMember(&DaprHostMember{
+			Name:      "127.0.0.1:8080",
+			AppID:     "FakeID1",
+			Entities:  []string{"actorTypeOne", "actorTypeTwo"},
+			UpdatedAt: 1,
+			APILevel:  10,
+		})
+		s.upsertMember(&DaprHostMember{
+			Name:      "127.0.0.1:8081",
+			AppID:     "FakeID2",
+			Entities:  []string{"actorTypeThree", "actorTypeFour"},
+			UpdatedAt: 2,
+			APILevel:  20,
+		})
+		s.upsertMember(&DaprHostMember{
+			Name:      "127.0.0.1:8082",
+			AppID:     "FakeID3",
+			Entities:  []string{"actorTypeFive"},
+			UpdatedAt: 3,
+			APILevel:  30,
+		})
+
+		assert.Equal(t, uint32(10), s.data.APILevel)
+
+		s.removeMember(&DaprHostMember{
+			Name: "127.0.0.1:8080",
+		})
+		assert.Equal(t, uint32(20), s.data.APILevel)
+
+		s.removeMember(&DaprHostMember{
+			Name: "127.0.0.1:8081",
+		})
+		assert.Equal(t, uint32(30), s.data.APILevel)
+
+		s.removeMember(&DaprHostMember{
+			Name: "127.0.0.1:8082",
+		})
+		assert.Equal(t, uint32(30), s.data.APILevel)
 	})
 
-	assert.Equal(t, uint32(10), s.data.APILevel)
+	t.Run("min api levels set", func(t *testing.T) {
+		s := newDaprHostMemberState(DaprHostMemberStateConfig{
+			replicationFactor: 10,
+			minAPILevel:       20,
+			maxAPILevel:       100,
+		})
+		s.upsertMember(&DaprHostMember{
+			Name:      "127.0.0.1:8080",
+			AppID:     "FakeID1",
+			Entities:  []string{"actorTypeOne", "actorTypeTwo"},
+			UpdatedAt: 1,
+			APILevel:  10,
+		})
+		assert.Equal(t, uint32(20), s.data.APILevel)
 
-	s.removeMember(&DaprHostMember{
-		Name: "127.0.0.1:8080",
-	})
-	assert.Equal(t, uint32(20), s.data.APILevel)
+		s.upsertMember(&DaprHostMember{
+			Name:      "127.0.0.1:8081",
+			AppID:     "FakeID1",
+			Entities:  []string{"actorTypeOne", "actorTypeTwo"},
+			UpdatedAt: 2,
+			APILevel:  30,
+		})
+		assert.Equal(t, uint32(20), s.data.APILevel)
 
-	s.removeMember(&DaprHostMember{
-		Name: "127.0.0.1:8081",
+		s.removeMember(&DaprHostMember{
+			Name: "127.0.0.1:8080",
+		})
+		assert.Equal(t, uint32(30), s.data.APILevel)
 	})
-	assert.Equal(t, uint32(30), s.data.APILevel)
 
-	s.removeMember(&DaprHostMember{
-		Name: "127.0.0.1:8082",
+	t.Run("max api levels set", func(t *testing.T) {
+		s := newDaprHostMemberState(DaprHostMemberStateConfig{
+			replicationFactor: 10,
+			minAPILevel:       0,
+			maxAPILevel:       20,
+		})
+		s.upsertMember(&DaprHostMember{
+			Name:      "127.0.0.1:8080",
+			AppID:     "FakeID1",
+			Entities:  []string{"actorTypeOne", "actorTypeTwo"},
+			UpdatedAt: 1,
+			APILevel:  30,
+		})
+
+		assert.Equal(t, uint32(20), s.data.APILevel)
 	})
-	assert.Equal(t, uint32(30), s.data.APILevel)
 }
