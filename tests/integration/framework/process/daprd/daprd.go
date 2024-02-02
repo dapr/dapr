ee90519cff44792a49e4d74e232720a4eea3427a
Artur Souza <asouza.pro@gmail.com>
2024-02-01 20:59:25 -0800

    Use 127.0.0.1 in daprd for IT.
    Signed-off-by: Artur Souza <asouza.pro@gmail.com>

--- tests/integration/framework/process/daprd/daprd.go
+++ tests/integration/framework/process/daprd/daprd.go
@@ -193,7 +193,7 @@ func (d *Daprd) WaitUntilTCPReady(t *testing.T, ctx context.Context) {
 func (d *Daprd) WaitUntilRunning(t *testing.T, ctx context.Context) {
 	client := util.HTTPClient(t)
 	require.Eventually(t, func() bool {
-		req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://localhost:%d/v1.0/healthz", d.httpPort), nil)
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://127.0.0.1:%d/v1.0/healthz", d.httpPort), nil)
 		if err != nil {
 			return false
 		}
@@ -211,7 +211,7 @@ func (d *Daprd) WaitUntilAppHealth(t *testing.T, ctx context.Context) {
 	case "http":
 		client := util.HTTPClient(t)
 		assert.Eventually(t, func() bool {
-			req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://localhost:%d/v1.0/healthz", d.httpPort), nil)
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://127.0.0.1:%d/v1.0/healthz", d.httpPort), nil)
 			if err != nil {
 				return false
 			}
@@ -244,7 +244,7 @@ func (d *Daprd) WaitUntilAppHealth(t *testing.T, ctx context.Context) {
 }
 
 func (d *Daprd) GRPCConn(t *testing.T, ctx context.Context) *grpc.ClientConn {
-	conn, err := grpc.DialContext(ctx, fmt.Sprintf("localhost:%d", d.GRPCPort()),
+	conn, err := grpc.DialContext(ctx, fmt.Sprintf("127.0.0.1:%d", d.GRPCPort()),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock(),
 	)
@@ -271,7 +271,7 @@ func (d *Daprd) AppPort() int {
 }
 
 func (d *Daprd) AppAddress() string {
-	return "localhost:" + strconv.Itoa(d.AppPort())
+	return "127.0.0.1:" + strconv.Itoa(d.AppPort())
 }
 
 func (d *Daprd) GRPCPort() int {
@@ -279,7 +279,7 @@ func (d *Daprd) GRPCPort() int {
 }
 
 func (d *Daprd) GRPCAddress() string {
-	return "localhost:" + strconv.Itoa(d.GRPCPort())
+	return "127.0.0.1:" + strconv.Itoa(d.GRPCPort())
 }
 
 func (d *Daprd) HTTPPort() int {
@@ -287,7 +287,7 @@ func (d *Daprd) HTTPPort() int {
 }
 
 func (d *Daprd) HTTPAddress() string {
-	return "localhost:" + strconv.Itoa(d.HTTPPort())
+	return "127.0.0.1:" + strconv.Itoa(d.HTTPPort())
 }
 
 func (d *Daprd) InternalGRPCPort() int {
@@ -295,7 +295,7 @@ func (d *Daprd) InternalGRPCPort() int {
 }
 
 func (d *Daprd) InternalGRPCAddress() string {
-	return "localhost:" + strconv.Itoa(d.InternalGRPCPort())
+	return "127.0.0.1:" + strconv.Itoa(d.InternalGRPCPort())
 }
 
 func (d *Daprd) PublicPort() int {
