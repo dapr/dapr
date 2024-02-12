68e5df0d401d74d847f0a30aa1fd5f5f574c0750
joshvanl <me@joshvanl.dev>
2024-02-12 13:32:36 +0000

    Adds daprd healthz mtls check
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/framework/process/daprd/options.go
+++ tests/integration/framework/process/daprd/options.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/dapr/dapr/tests/integration/framework/process/exec"
 	"github.com/dapr/dapr/tests/integration/framework/process/logline"
+	"github.com/dapr/dapr/tests/integration/framework/process/sentry"
 )
 
 // Option is a function that configures the dapr process.
@@ -278,5 +279,13 @@ func WithDaprBlockShutdownDuration(duration string) Option {
 func WithControlPlaneTrustDomain(trustDomain string) Option {
 	return func(o *options) {
 		o.controlPlaneTrustDomain = &trustDomain
+	}
+}
+
+func WithSentry(t *testing.T, sentry *sentry.Sentry) Option {
+	return func(o *options) {
+		WithExecOptions(exec.WithEnvVars(t, "DAPR_TRUST_ANCHORS", string(sentry.CABundle().TrustAnchors)))(o)
+		WithSentryAddress(sentry.Address())(o)
+		WithEnableMTLS(true)(o)
 	}
 }
