fd5a7bc68f50706cdafd2b57d7c7c13301c1a122
Josh van Leeuwen <me@joshvanl.dev>
2024-02-21 13:10:33 +0000

    Actor Reminders: Default JSON serialization. (#7548)
    * Actor Reminders: Default JSON serialization.
    
    To support downgrades to 1.12 from 1.13, this PR changes the reminder
    serialization storage format back to JSON by default. This means a 1.12
    actor reminder client can read reminders written by 1.13 actors.
    
    1.13 will continue to understand both JSON and protobuf. Protobuf
    serialization can be enabled with the `ActorReminderStorageProtobuf`
    feature gate. The actor "API Level" has been changed back to 10.
    
    Adds test to ensure the default serialization is JSON.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Remove ActorReminderStorageProtobuf feature gate in favour of using API
    level
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Fix api level tests
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/integration/suite/actors/reminders/serialization/default.go
+++ tests/integration/suite/actors/reminders/serialization/default.go
@@ -35,12 +35,11 @@ import (
 )
 
 func init() {
-	suite.Register(new(jsonFormat))
+	suite.Register(new(defaultS))
 }
 
-// jsonFormat tests:
-// - That reminders are serialized to JSON when the Actors API level in the cluster is < 20
-type jsonFormat struct {
+// defaultS ensures that reminders are stored as JSON by default.
+type defaultS struct {
 	daprd   *daprd.Daprd
 	srv     *prochttp.HTTP
 	handler *httpServer
@@ -48,62 +47,46 @@ type jsonFormat struct {
 	db      *sqlite.SQLite
 }
 
-func (j *jsonFormat) Setup(t *testing.T) []framework.Option {
+func (d *defaultS) Setup(t *testing.T) []framework.Option {
 	if runtime.GOOS == "windows" {
 		t.Skip("Skipping test on Windows due to SQLite limitations")
 	}
 
-	// Init placement with a maximum API level of 10
-	// We need to set the max API level to 10, because levels 20 and up with serialise as protobuf
-	j.place = placement.New(t,
-		placement.WithMaxAPILevel(10),
-	)
+	d.place = placement.New(t)
 
-	// Create a SQLite database
-	j.db = sqlite.New(t, sqlite.WithActorStateStore(true))
+	d.db = sqlite.New(t, sqlite.WithActorStateStore(true))
 
-	// Init daprd and the HTTP server
-	j.handler = &httpServer{}
-	j.srv = prochttp.New(t, prochttp.WithHandler(j.handler.NewHandler()))
-	j.daprd = daprd.New(t,
-		daprd.WithResourceFiles(j.db.GetComponent(t)),
-		daprd.WithPlacementAddresses("127.0.0.1:"+strconv.Itoa(j.place.Port())),
-		daprd.WithAppPort(j.srv.Port()),
+	d.handler = new(httpServer)
+	d.srv = prochttp.New(t, prochttp.WithHandler(d.handler.NewHandler()))
+	d.daprd = daprd.New(t,
+		daprd.WithResourceFiles(d.db.GetComponent(t)),
+		daprd.WithPlacementAddresses("127.0.0.1:"+strconv.Itoa(d.place.Port())),
+		daprd.WithAppPort(d.srv.Port()),
 	)
 
 	return []framework.Option{
-		framework.WithProcesses(j.db, j.place, j.srv, j.daprd),
+		framework.WithProcesses(d.db, d.place, d.srv, d.daprd),
 	}
 }
 
-func (j *jsonFormat) Run(t *testing.T, ctx context.Context) {
-	// Wait for placement to be ready
-	j.place.WaitUntilRunning(t, ctx)
-
-	// Wait for daprd to be ready
-	j.daprd.WaitUntilRunning(t, ctx)
-
-	// Wait for actors to be ready
-	err := j.handler.WaitForActorsReady(ctx)
-	require.NoError(t, err)
+func (d *defaultS) Run(t *testing.T, ctx context.Context) {
+	d.place.WaitUntilRunning(t, ctx)
+	d.daprd.WaitUntilRunning(t, ctx)
+	require.NoError(t, d.handler.WaitForActorsReady(ctx))
 
 	client := util.HTTPClient(t)
-	baseURL := fmt.Sprintf("http://localhost:%d/v1.0/actors/myactortype/myactorid", j.daprd.HTTPPort())
+	baseURL := fmt.Sprintf("http://localhost:%d/v1.0/actors/myactortype/myactorid", d.daprd.HTTPPort())
 
-	// Invoke an actor to confirm everything is ready to go
 	invokeActor(t, ctx, baseURL, client)
 
-	// Store a reminder
-	// This causes the data in the state store to be updated
 	storeReminder(t, ctx, baseURL, client)
 
 	// Check the data in the SQLite database
 	// The value must begin with `[{`, which indicates it was serialized as JSON
-	storedVal := loadRemindersFromDB(t, ctx, j.db.GetConnection(t))
+	storedVal := loadRemindersFromDB(t, ctx, d.db.GetConnection(t))
 	assert.Truef(t, strings.HasPrefix(storedVal, "[{"), "Prefix not found in value: '%v'", storedVal)
 
-	// Ensure the reminder was invoked at least once
 	assert.Eventually(t, func() bool {
-		return j.handler.remindersInvokeCount.Load() > 0
+		return d.handler.remindersInvokeCount.Load() > 0
 	}, 5*time.Second, 10*time.Millisecond, "Reminder was not invoked at least once")
 }
