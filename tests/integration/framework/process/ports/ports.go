417f5284c58aeaf1e14893c7c411e4a322652f05
Josh van Leeuwen <me@joshvanl.dev>
2024-03-26 12:32:32 +0100

    Test Integration: reserve ports in ordered blocks (#7623)
    * Test Integration: reserve ports in ordered blocks
    
    Dapr opens a lot of ports. Integration tests use upwards of a 1000 ports
    through a run. Tests are run in parallel and ports are reserved on
    demand through port 0 then being released before being used by a test.
    There was a high chance that a port would attempted to be used by
    multiple tests/processes at the same time causing conflict errors and
    false test failures.
    
    To prevent these conflicts, the ports utils has been moved to a process
    which will reserve 300 blocks of ports to be used by tests. These ports
    are reserved by incrementing through port ranges 1024 to 65535 in a
    ring. This always prevents conflicts on this test runner, and should
    also decrease port conflicts across multiple simultaneous test runners.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Rename intPort to port
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    Co-authored-by: Loong Dai <long.dai@intel.com>
    Co-authored-by: Dapr Bot <56698301+dapr-bot@users.noreply.github.com>
    Signed-off-by: Joey Freeland <joey@free.land>

---
+++ tests/integration/framework/process/ports/ports.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2024 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ports
+
+import (
+	"context"
+	"errors"
+	"net"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	lock     sync.Mutex
+	resvPLen int
+	resvPIx  int
+	last     = 1024
+	resvP    []*reservedPort
+)
+
+// Ports reserves network ports, and then frees them when the test is ready to
+// run so a  process can bind to them at runtime.
+type Ports struct {
+	idx   int
+	start int
+	end   int
+	freed atomic.Bool
+}
+
+type reservedPort struct {
+	ln   net.Listener
+	port int
+}
+
+func Reserve(t *testing.T, count int) *Ports {
+	t.Helper()
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	require.GreaterOrEqual(t, count, 1)
+	require.LessOrEqual(t, count, 10)
+
+	resvPLen -= count
+	if count > resvPLen || resvPLen < 10 {
+		t.Logf("reserving 300 more ports")
+		for i := 0; i < 300; i++ {
+			last++
+			if last+i > 65535 {
+				last = 1024
+			}
+			ln, err := net.Listen("tcp", "127.0.0.1:"+strconv.Itoa(last))
+			if err != nil {
+				i--
+				continue
+			}
+			tcp, ok := ln.Addr().(*net.TCPAddr)
+			require.True(t, ok)
+			resvP = append(resvP, &reservedPort{ln, tcp.Port})
+		}
+		resvPLen += 300
+	}
+
+	p := &Ports{
+		idx:   resvPIx,
+		start: resvPIx,
+		end:   resvPIx + count,
+	}
+	resvPIx += count
+
+	t.Cleanup(func() { p.Free(t) })
+
+	return p
+}
+
+func (p *Ports) Port(t *testing.T) int {
+	t.Helper()
+
+	lock.Lock()
+	defer func() {
+		p.idx++
+		lock.Unlock()
+	}()
+
+	require.Less(t, p.idx, p.end, "request for more ports than reserved")
+
+	return resvP[p.idx].port
+}
+
+func (p *Ports) Listener(t *testing.T) net.Listener {
+	t.Helper()
+
+	lock.Lock()
+	defer func() {
+		p.idx++
+		lock.Unlock()
+	}()
+
+	require.Less(t, p.idx, p.end, "request for more ports than reserved")
+
+	return resvP[p.idx].ln
+}
+
+func (p *Ports) Free(t *testing.T) {
+	t.Helper()
+	if p.freed.CompareAndSwap(false, true) {
+		lock.Lock()
+		defer lock.Unlock()
+		for i := p.start; i < p.end; i++ {
+			err := resvP[i].ln.Close()
+			if !errors.Is(err, net.ErrClosed) {
+				require.NoError(t, err)
+			}
+		}
+	}
+}
+
+func (p *Ports) Run(t *testing.T, _ context.Context) {
+	t.Helper()
+	p.Free(t)
+}
+
+func (p *Ports) Cleanup(t *testing.T) {
+	p.Free(t)
+}
