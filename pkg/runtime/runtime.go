09841498cf43a1716325b6d3b9972ef67b481642
Josh van Leeuwen <me@joshvanl.dev>
2024-04-11 19:58:30 +0100

    Subscriptions: Declarative Go Generic Loader (#7582)
    * Subscriptions: Declarative Go Generic Loader
    
    Following Components and HTTPEndpoints, creates a generic disk &
    kubernetes loader for Subscriptions. Removes the adhok loaders from
    existing runtime pubsub packages. This is required for adding
    Subscriptions to the hot reloader reconciler and making a more
    consistent & testable manifest loader package.
    
    The disk loader loads both `v1alpha1` and `v2alpha1` Subscriptions,
    converting `v1alpha1` to `v2alpha1` on successful load. Since the
    operator returns only `v2alpha1` Subscriptions (Kubernetes API converts
    `v1alpha1` to `v2alpha1`), the Kubernetes loader only loads `v2alpha1`.
    
    `APIVersion() string` func has been added to the generic `meta.Resource`
    interface type to allow for the generic loader to determine between
    resource versions, supporting differentiating `v1alpha1` and `v2alpha1`
    Subscription resource versions.
    
    To ensure backwards compatibility of the previous disk Subscription
    loader, the generic disk loader now tracks the order in which manifests
    are loaded. This ensures that, even though `v1alpha1` and `v2alpha1`
    Subscriptions use separate loaders, their file position order is
    preserved once `v1alpha1` Subscriptions are converted to `v2alpha1`.
    Subscription backwards compatibility of parsing & ingestion priority is
    covered by existing extensive integration tests.
    
    Notice that _ZERO_ of the comprehensive subscription integration tests
    have been modified, proving no behaviour change has occurred for loading
    and actuating Subscriptions.
    
    Explanation of integration test changes:
    - daprd/hotreload/selfhosted/crypto: loader now mandates `apiVersion` on
      disk manifests which is more correct. Previous behaviour ignoring this
      field should be considered a bug. Since we use Kubernetes resource
      API schema, `apiVersion` is (and has been) _always_ required.
    - framework/process/grpc/operator/server.go: returning non-nil gRPC
      object for mocked operator Subscription RPC call prevents underlying
      gRPC library unmarshal nil errors- more correct than previous
      implementation.
    
    Part of [Subscription hot-reloading](https://github.com/dapr/dapr/issues/7139).
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Update errS to patherrs to avoid confusion
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Updates sub import to use bare version string
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Remove unneeded code comment
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Change test manifests to have unique names
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    Co-authored-by: Dapr Bot <56698301+dapr-bot@users.noreply.github.com>
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -72,6 +72,7 @@ import (
 	"github.com/dapr/dapr/pkg/runtime/hotreload"
 	"github.com/dapr/dapr/pkg/runtime/meta"
 	"github.com/dapr/dapr/pkg/runtime/processor"
+	"github.com/dapr/dapr/pkg/runtime/processor/wfbackend"
 	"github.com/dapr/dapr/pkg/runtime/processor/workflow"
 	"github.com/dapr/dapr/pkg/runtime/registry"
 	runtimeScheduler "github.com/dapr/dapr/pkg/runtime/scheduler"
@@ -679,14 +680,14 @@ func (a *DaprRuntime) initWorkflowEngine(ctx context.Context) error {
 		ComponentStore: a.compStore,
 		Meta:           a.meta,
 	})
-	if err := wfe.Init(ctx, wfengine.ComponentDefinition()); err != nil {
+	if err := wfe.Init(ctx, wfbackend.ComponentDefinition()); err != nil {
 		return fmt.Errorf("failed to initialize Dapr workflow component: %w", err)
 	}
 
 	log.Info("Workflow engine initialized.")
 
 	return a.runnerCloser.AddCloser(func() error {
-		return wfe.Close(wfengine.ComponentDefinition())
+		return wfe.Close(wfbackend.ComponentDefinition())
 	})
 }
 
@@ -1023,7 +1024,7 @@ func (a *DaprRuntime) loadComponents(ctx context.Context) error {
 			PodName:   a.podName,
 		})
 	case modes.StandaloneMode:
-		loader = disk.New[compapi.Component](a.runtimeConfig.standalone.ResourcesPath...)
+		loader = disk.NewComponents(a.runtimeConfig.standalone.ResourcesPath...)
 	default:
 		return nil
 	}
@@ -1087,7 +1088,7 @@ func (a *DaprRuntime) loadHTTPEndpoints(ctx context.Context) error {
 			PodName:   a.podName,
 		})
 	case modes.StandaloneMode:
-		loader = disk.New[endpointapi.HTTPEndpoint](a.runtimeConfig.standalone.ResourcesPath...)
+		loader = disk.NewHTTPEndpoints(a.runtimeConfig.standalone.ResourcesPath...)
 	default:
 		return nil
 	}
