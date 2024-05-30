c92d400020cd77f5f15683a489a3a2b79724f326
Josh van Leeuwen <me@joshvanl.dev>
2024-05-30 15:29:58 +0100

    Adds trigger performance tests (#36)
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/apps/perf/actorfeatures/go.mod
+++ tests/apps/perf/actorfeatures/go.mod
@@ -2,4 +2,20 @@ module github.com/dapr/dapr/tests/apps/perf/tester
 
 go 1.22
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/dapr/dapr v1.13.4
+	github.com/go-chi/chi/v5 v5.0.11
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231212172506-995d672761c0 // indirect
+	google.golang.org/grpc v1.60.1 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
--- tests/perf/actor_reminder/actor_reminder_test.go
+++ tests/perf/actor_reminder/actor_reminder_test.go
@@ -20,6 +20,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -32,17 +33,32 @@ import (
 	kube "github.com/dapr/dapr/tests/platforms/kubernetes"
 	"github.com/dapr/dapr/tests/runner"
 	"github.com/dapr/dapr/tests/runner/summary"
+	"github.com/dapr/kit/ptr"
 )
 
 const (
-	numHealthChecks  = 60 // Number of times to check for endpoint health per app.
-	actorType        = "PerfTestActorReminder"
-	appName          = "perf-actor-reminder-service"
-	appNameScheduler = "perf-actor-reminder-scheduler-service"
+	numHealthChecks    = 60 // Number of times to check for endpoint health per app.
+	actorType          = "PerfTestActorReminder"
+	actorTypeScheduler = "PerfTestActorReminderSchduler"
+	appName            = "perf-actor-reminder-service"
+	appNameScheduler   = "perf-actor-reminder-scheduler-service"
 
 	// Target for the QPS
-	targetQPS                  = 56
+	targetQPS          float64 = 56
 	targetSchedulerQPS float64 = 2850
+
+	// Target for the QPS to trigger reminders.
+	targetTriggerQPS          float64 = 4700
+	targetSchedulerTriggerQPS float64 = 3800
+
+	// reminderCount is the number of reminders to register.
+	reminderCount          = 5000
+	reminderCountScheduler = 50000
+
+	// dueTime is the time in seconds to execute the reminders. This covers the
+	// time to register the reminders and the time to trigger them.
+	dueTime          = 90
+	dueTimeScheduler = 25
 )
 
 var tr *runner.TestRunner
@@ -60,7 +76,7 @@ func TestMain(m *testing.M) {
 			AppPort:           3000,
 			DaprCPULimit:      "4.0",
 			DaprCPURequest:    "0.1",
-			DaprMemoryLimit:   "512Mi",
+			DaprMemoryLimit:   "2GiB",
 			DaprMemoryRequest: "250Mi",
 			AppCPULimit:       "4.0",
 			AppCPURequest:     "0.1",
@@ -79,14 +95,14 @@ func TestMain(m *testing.M) {
 			AppPort:           3000,
 			DaprCPULimit:      "4.0",
 			DaprCPURequest:    "0.1",
-			DaprMemoryLimit:   "512Mi",
+			DaprMemoryLimit:   "2GiB",
 			DaprMemoryRequest: "250Mi",
 			AppCPULimit:       "4.0",
 			AppCPURequest:     "0.1",
 			AppMemoryLimit:    "800Mi",
 			AppMemoryRequest:  "2500Mi",
 			AppEnv: map[string]string{
-				"TEST_APP_ACTOR_TYPE": actorType,
+				"TEST_APP_ACTOR_TYPE": actorTypeScheduler,
 			},
 			Config: "featureactorreminderscheduler",
 		},
@@ -188,7 +204,7 @@ func TestActorReminderRegistrationPerformance(t *testing.T) {
 	assert.True(t, daprResult.ActualQPS > targetQPS)
 }
 
-func TestActorReminderSchedulerTriggerPerformance(t *testing.T) {
+func TestActorReminderSchedulerRegistrationPerformance(t *testing.T) {
 	p := perf.Params(
 		perf.WithQPS(5000),
 		perf.WithConnections(8),
@@ -206,7 +222,7 @@ func TestActorReminderSchedulerTriggerPerformance(t *testing.T) {
 	require.NoError(t, err)
 
 	// Perform dapr test
-	endpoint := fmt.Sprintf("http://127.0.0.1:3500/v1.0/actors/%v/{uuid}/reminders/myreminder", actorType)
+	endpoint := fmt.Sprintf("http://127.0.0.1:3500/v1.0/actors/%v/{uuid}/reminders/myreminder", actorTypeScheduler)
 	p.TargetEndpoint = endpoint
 	p.Payload = `{"dueTime":"24h","period":"24h"}`
 	body, err := json.Marshal(&p)
@@ -281,4 +297,154 @@ func TestActorReminderSchedulerTriggerPerformance(t *testing.T) {
 	assert.Equal(t, 0, daprResult.RetCodes.Num500)
 	assert.Equal(t, 0, restarts)
 	assert.GreaterOrEqual(t, daprResult.ActualQPS, targetSchedulerQPS)
+}
+
+type actorReminderRequest struct {
+	DueTime *string `json:"dueTime,omitempty"`
+	Period  *string `json:"period,omitempty"`
+	Ttl     *string `json:"ttl,omitempty"`
+}
+
+func TestActorReminderTriggerPerformance(t *testing.T) {
+	// Get the ingress external url of test app
+	testAppURL := tr.Platform.AcquireAppExternalURL(appName)
+	require.NotEmpty(t, testAppURL, "test app external URL must not be empty")
+
+	// Check if test app endpoint is available
+	t.Logf("test app url: %s", testAppURL+"/health")
+	_, err := utils.HTTPGetNTimes(testAppURL+"/health", numHealthChecks)
+	require.NoError(t, err)
+
+	t.Logf("invoking actor reminder scheduler")
+	_, err = utils.HTTPGet(fmt.Sprintf("%s/actors/%s/abc/method/foo", testAppURL, actorType))
+	require.NoError(t, err)
+
+	t.Logf("registering actor reminders")
+	reminder := &actorReminderRequest{
+		DueTime: ptr.Of(time.Now().Add(time.Second * dueTime).Format(time.RFC3339)),
+		Period:  ptr.Of("1s"),
+		Ttl:     ptr.Of("5s"),
+	}
+	reminderB, err := json.Marshal(reminder)
+	require.NoError(t, err)
+
+	worker := func(i int) {
+		_, err = utils.HTTPPost(fmt.Sprintf("%s/actors/%s/abc/reminders/myreminder%d", testAppURL, actorType, i), reminderB)
+		require.NoError(t, err)
+
+		if (i+1)%100 == 0 {
+			fmt.Printf("Reminders registered: %d\n", i+1)
+		}
+	}
+
+	ch := make(chan int)
+	for j := 0; j < 50; j++ {
+		go func() {
+			for {
+				i, ok := <-ch
+				if !ok {
+					return
+				}
+				worker(i)
+			}
+		}()
+	}
+
+	now := time.Now()
+	for i := 0; i < reminderCount; i++ {
+		ch <- i
+	}
+	done := time.Since(now)
+	t.Logf("Created %d reminders in %s (%.1fqps)", reminderCount, done, float64(reminderCount)/done.Seconds())
+
+	require.GreaterOrEqualf(t, dueTime*time.Second, done, "expected to create reminders %d in less than %ds", reminderCount, dueTime)
+
+	t.Logf("Waiting %s for reminders to trigger", (dueTime*time.Second)-done)
+	time.Sleep((dueTime * time.Second) - done)
+
+	t.Logf("Waiting for %d reminders to trigger", reminderCount*5)
+	start := time.Now()
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		resp, err := utils.HTTPGet(fmt.Sprintf("%s/remindersCount", testAppURL))
+		require.NoError(t, err)
+		gotCount, err := strconv.Atoi(strings.TrimSpace(string(resp)))
+		require.NoError(t, err)
+		assert.GreaterOrEqual(c, gotCount, reminderCount*5)
+	}, 100*time.Second, time.Millisecond*100)
+	done = time.Since(start)
+	qps := float64(reminderCount*5) / done.Seconds()
+	t.Logf("Triggered %d reminders in %s (%.1fqps)", reminderCount*5, done, qps)
+	assert.GreaterOrEqual(t, qps, targetTriggerQPS)
+}
+
+func TestActorReminderSchedulerTriggerPerformance(t *testing.T) {
+	// Get the ingress external url of test app
+	testAppURL := tr.Platform.AcquireAppExternalURL(appNameScheduler)
+	require.NotEmpty(t, testAppURL, "test app external URL must not be empty")
+
+	// Check if test app endpoint is available
+	t.Logf("test app url: %s", testAppURL+"/health")
+	_, err := utils.HTTPGetNTimes(testAppURL+"/health", numHealthChecks)
+	require.NoError(t, err)
+
+	t.Logf("invoking actor reminder scheduler")
+	_, err = utils.HTTPGet(fmt.Sprintf("%s/actors/%s/abc/method/foo", testAppURL, actorTypeScheduler))
+	require.NoError(t, err)
+
+	t.Logf("registering actor reminders")
+	reminder := &actorReminderRequest{
+		DueTime: ptr.Of(time.Now().Add(time.Second * dueTimeScheduler).Format(time.RFC3339)),
+		Period:  ptr.Of("1s"),
+		Ttl:     ptr.Of("5s"),
+	}
+	reminderB, err := json.Marshal(reminder)
+	require.NoError(t, err)
+
+	worker := func(i int) {
+		_, err = utils.HTTPPost(fmt.Sprintf("%s/actors/%s/abc/reminders/myreminder%d", testAppURL, actorTypeScheduler, i), reminderB)
+		require.NoError(t, err)
+
+		if (i+1)%10000 == 0 {
+			fmt.Printf("Reminders registered: %d\n", i+1)
+		}
+	}
+
+	ch := make(chan int)
+	for j := 0; j < 50; j++ {
+		go func() {
+			for {
+				i, ok := <-ch
+				if !ok {
+					return
+				}
+				worker(i)
+			}
+		}()
+	}
+
+	now := time.Now()
+	for i := 0; i < reminderCountScheduler; i++ {
+		ch <- i
+	}
+	done := time.Since(now)
+	t.Logf("Created %d reminders in %s (%.1fqps)", reminderCountScheduler, done, float64(reminderCountScheduler)/done.Seconds())
+
+	require.GreaterOrEqualf(t, dueTimeScheduler*time.Second, done, "expected to create reminders %d in less than %ss", reminderCountScheduler, dueTimeScheduler)
+
+	t.Logf("Waiting %s for reminders to trigger", (dueTimeScheduler*time.Second)-done)
+	time.Sleep((dueTimeScheduler * time.Second) - done)
+
+	t.Logf("Waiting for %d reminders to trigger", reminderCountScheduler*5)
+	start := time.Now()
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		resp, err := utils.HTTPGet(fmt.Sprintf("%s/remindersCount", testAppURL))
+		require.NoError(t, err)
+		gotCount, err := strconv.Atoi(strings.TrimSpace(string(resp)))
+		require.NoError(t, err)
+		assert.GreaterOrEqual(c, gotCount, reminderCountScheduler*5)
+	}, 100*time.Second, time.Millisecond*100)
+	done = time.Since(start)
+	qps := float64(reminderCountScheduler*5) / done.Seconds()
+	t.Logf("Triggered %d reminders in %s (%.1fqps)", reminderCountScheduler*5, done, qps)
+	assert.GreaterOrEqual(t, qps, targetSchedulerTriggerQPS)
 }
