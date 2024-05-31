a06d906d3bbe8d7b8fb61d4648f08f26e19686a3
joshvanl <me@joshvanl.dev>
2024-05-31 22:05:53 +0100

    Remove type meta from k6 operator create resource
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/runner/loadtest/k6.go
+++ tests/runner/loadtest/k6.go
@@ -259,10 +259,6 @@ func (k6 *K6) k8sRun(k8s *runner.KubeTestPlatform) error {
 		testplatform.TestAppLabelKey: k6.name,
 	}
 	k6Test := k6api.K6{
-		TypeMeta: v1.TypeMeta{
-			Kind:       "K6",
-			APIVersion: "testruns.k6.io/v1alpha1",
-		},
 		ObjectMeta: v1.ObjectMeta{
 			Name:      k6.name,
 			Namespace: k6.namespace,
