b607239c331763664ff1050a40b38569962d039b
Josh van Leeuwen <me@joshvanl.dev>
2024-06-03 22:24:58 +0100

    Feat dist scheduler perf tune (#43)
    * Increase dueTime for reminder
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Adds scheduler app as scope to config state store
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/perf/actor_reminder/actor_reminder_test.go
+++ tests/perf/actor_reminder/actor_reminder_test.go
@@ -57,7 +57,7 @@ const (
 
 	// dueTime is the time in seconds to execute the reminders. This covers the
 	// time to register the reminders and the time to trigger them.
-	dueTime          = 90
+	dueTime          = 110
 	dueTimeScheduler = 25
 )
 
