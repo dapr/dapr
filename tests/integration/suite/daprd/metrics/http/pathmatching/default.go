872cffa7dd261ef136ca92b07466fb16a544c639
Nelson Parente <nelson_parente@live.com.pt>
2024-06-14 02:13:18 +0100

    HTTP Metrics Path Matching (#7746)
    * feat: http path normalization
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * fix: add tags based on config
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * test: add config and http unit tests
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * test: add integration tests
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: rename catch all path bucket
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: refactor virtual mux register
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * fix: lint errors
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: small refactor and tests to initPathNormalization
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * fix: return custom writer ref normalized path
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * codereview: request initialization, remove tags allocation, config return
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * codereview: config definition and test
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: add method with path normalization
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * fix: remove redundant nil check
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * test: add path normalization benchmark tests
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * nit: remove comments and add const
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * fix: lint errors
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: remove enabled option
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: change unmacthed path placeholder
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: rename normalization to matching
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: path matching code review refactor
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: add more test cases with different path configurations
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * fix: lint errs
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: code review changes
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: remove tags path matching register
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * fix: golangci lint errors
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * code review comments
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    * feat: clean and sort paths
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    
    ---------
    
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>
    Co-authored-by: Artur Souza <asouza.pro@gmail.com>
    Signed-off-by: Annu Singh <annu4444.as@gmail.com>

---
+++ tests/integration/suite/daprd/metrics/http/pathmatching/default.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2024 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pathmatching
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/daprd"
+	"github.com/dapr/dapr/tests/integration/framework/process/http/app"
+	"github.com/dapr/dapr/tests/integration/suite"
+)
+
+func init() {
+	suite.Register(new(defaultCardinality))
+}
+
+// defaultCardinality tests daprd metrics for the HTTP server configured with path matching and default cardinality.
+type defaultCardinality struct {
+	daprd *daprd.Daprd
+}
+
+func (h *defaultCardinality) Setup(t *testing.T) []framework.Option {
+	app := app.New(t,
+		app.WithHandlerFunc("/orders/{orderID}", func(w http.ResponseWriter, _ *http.Request) {
+			fmt.Fprint(w, "OK")
+		}),
+	)
+
+	h.daprd = daprd.New(t,
+		daprd.WithAppPort(app.Port()),
+		daprd.WithAppProtocol("http"),
+		daprd.WithAppID("myapp"),
+		daprd.WithInMemoryStateStore("mystore"),
+		daprd.WithConfigManifests(t, `
+apiVersion: dapr.io/v1alpha1
+kind: Configuration
+metadata:
+  name: pathMatching
+spec:
+  metrics:
+    enabled: true
+`),
+	)
+
+	return []framework.Option{
+		framework.WithProcesses(app, h.daprd),
+	}
+}
+
+func (h *defaultCardinality) Run(t *testing.T, ctx context.Context) {
+	h.daprd.WaitUntilRunning(t, ctx)
+
+	t.Run("service invocation - default", func(t *testing.T) {
+		h.daprd.HTTPGet2xx(t, ctx, "/v1.0/invoke/myapp/method/orders/123")
+		metrics := h.daprd.Metrics(t, ctx)
+		assert.Equal(t, 1, int(metrics["dapr_http_server_request_count|app_id:myapp|method:InvokeService/myapp|status:200"]))
+	})
+}
