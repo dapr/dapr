27c27e7d16e317480fc87b0fe0b8c874af1a600b
Cassandra Coyle <cassie@diagrid.io>
2024-06-21 14:27:25 -0500

    reset int test timeout
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/integration/integration.go
+++ tests/integration/integration.go
@@ -74,7 +74,7 @@ func RunIntegrationTests(t *testing.T) {
 			options := tcase.Setup(t)
 
 			t.Logf("setting up test case")
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+			ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
 			t.Cleanup(cancel)
 
 			framework.Run(t, ctx, options...)
