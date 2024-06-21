875f4ddb32f6735413c484df0a22c62ddcc02334
Cassandra Coyle <cassie@diagrid.io>
2024-06-21 13:00:31 -0500

    up test time
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/integration/suite/scheduler/quorum/notls.go
+++ tests/integration/suite/scheduler/quorum/notls.go
@@ -170,7 +170,7 @@ func getEtcdKeys(t *testing.T, port string) []*mvccpb.KeyValue {
 	require.NoError(t, err)
 	defer client.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	// Get keys with prefix
