fdfb2b42b18bf269493255b408154b9615e589b5
Josh van Leeuwen <me@joshvanl.dev>
2024-07-03 16:12:32 +0100

    Scheduler: Delete workflow reminders when cancelled (#7881)
    * Scheduler: Delete workflow reminders when cancelled
    
    Updates the scheduler reminder invocation logic to delete the reminder
    when it has been cancelled by workflows, matching the existing actor
    reminder handler.
    
    Adds integration test to ensure job is deleted from the backing
    scheduler ETCD store.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Add integration test fixes
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Always run unit tests with `--race`. Fix unit test race conditions
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Linting
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Make daprd healthz int test eventually on HTTP check
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Revert unit test changes
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Use background context when deleting reminder
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Make metrics eventually
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Make assert with longer timeout
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Mark scheduler ready once ETCd db has started
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Adds eventually to metrics check
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Return assert if failed
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    Co-authored-by: Artur Souza <asouza.pro@gmail.com>
    Signed-off-by: Annu Singh <annu4444.as@gmail.com>

--- tests/integration/suite/daprd/metrics/http/pathmatching/default.go
+++ tests/integration/suite/daprd/metrics/http/pathmatching/default.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -69,7 +70,9 @@ func (h *defaultCardinality) Run(t *testing.T, ctx context.Context) {
 
 	t.Run("service invocation - default", func(t *testing.T) {
 		h.daprd.HTTPGet2xx(t, ctx, "/v1.0/invoke/myapp/method/orders/123")
-		metrics := h.daprd.Metrics(t, ctx)
-		assert.Equal(t, 1, int(metrics["dapr_http_server_request_count|app_id:myapp|method:GET|path:/v1.0/invoke/myapp/method/orders/123|status:200"]))
+		assert.EventuallyWithT(t, func(c *assert.CollectT) {
+			metrics := h.daprd.Metrics(t, ctx)
+			assert.Equal(c, 1, int(metrics["dapr_http_server_request_count|app_id:myapp|method:GET|path:/v1.0/invoke/myapp/method/orders/123|status:200"]))
+		}, time.Second*10, time.Millisecond*10)
 	})
 }
