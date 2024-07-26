6258fccb6cec77f81014279535a2849cc856e02a
Cassandra Coyle <cassie@diagrid.io>
2024-07-26 08:33:31 -0500

    up dueTime again
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/perf/actor_reminder/actor_reminder_test.go
+++ tests/perf/actor_reminder/actor_reminder_test.go
@@ -57,7 +57,7 @@ const (
 
 	// dueTime is the time in seconds to execute the reminders. This covers the
 	// time to register the reminders and the time to trigger them.
-	dueTime          = 310
+	dueTime          = 410
 	dueTimeScheduler = 110
 )
 
