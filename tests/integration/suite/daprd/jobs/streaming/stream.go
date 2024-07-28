f55a685a08824586fb1690871361f7a501d4f861
yaron2 <schneider.yaron@live.com>
2024-07-28 16:51:52 -0700

    setup time
    Signed-off-by: yaron2 <schneider.yaron@live.com>

--- tests/integration/suite/daprd/jobs/streaming/stream.go
+++ tests/integration/suite/daprd/jobs/streaming/stream.go
@@ -105,6 +105,8 @@ func (s *streaming) Run(t *testing.T, ctx context.Context) {
 	s.schedulers[1].WaitUntilRunning(t, ctx)
 	s.schedulers[2].WaitUntilRunning(t, ctx)
 
+	time.Sleep(time.Second * 10)
+
 	s.daprdA.WaitUntilRunning(t, ctx)
 	s.daprdB.WaitUntilRunning(t, ctx)
 
