30f9d738e3ab54fb87bde2e917eaba9018c2c787
joshvanl <me@joshvanl.dev>
2024-08-13 18:38:55 -0500

    Tune Reg due time
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/perf/actor_reminder/actor_reminder_test.go
+++ tests/perf/actor_reminder/actor_reminder_test.go
@@ -63,7 +63,7 @@ const (
 	// dueTime is the time in seconds to execute the reminders. This covers the
 	// time to register the reminders and the time to trigger them.
 	dueTime          = 300
-	dueTimeScheduler = 110
+	dueTimeScheduler = 220
 )
 
 var tr *runner.TestRunner
