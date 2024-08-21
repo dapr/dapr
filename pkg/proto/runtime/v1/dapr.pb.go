02e8dcc6d10922332b1ae0b72fa35709dc6444ba
Mike Nguyen <hey@mike.ee>
2024-08-21 02:56:50 +0100

    docs(protos): add escapes (#8009)
    * doc(protos): add escapes
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    * doc(protos): gen-proto
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    ---------
    
    Signed-off-by: mikeee <hey@mike.ee>

--- pkg/proto/runtime/v1/dapr.pb.go
+++ pkg/proto/runtime/v1/dapr.pb.go
@@ -6467,14 +6467,14 @@ type Job struct {
 	// "0 15 3 * * *" - every day at 03:15
 	//
 	// Period string expressions:
-	// Entry                  | Description                                | Equivalent To
-	// -----                  | -----------                                | -------------
-	// @every <duration>      | Run every <duration> (e.g. '@every 1h30m') | N/A
-	// @yearly (or @annually) | Run once a year, midnight, Jan. 1st        | 0 0 0 1 1 *
-	// @monthly               | Run once a month, midnight, first of month | 0 0 0 1 * *
-	// @weekly                | Run once a week, midnight on Sunday        | 0 0 0 * * 0
-	// @daily (or @midnight)  | Run once a day, midnight                   | 0 0 0 * * *
-	// @hourly                | Run once an hour, beginning of hour        | 0 0 * * * *
+	// Entry                  | Description                                  | Equivalent To
+	// -----                  | -----------                                  | -------------
+	// @every `<duration>`    | Run every `<duration>` (e.g. '@every 1h30m') | N/A
+	// @yearly (or @annually) | Run once a year, midnight, Jan. 1st          | 0 0 0 1 1 *
+	// @monthly               | Run once a month, midnight, first of month   | 0 0 0 1 * *
+	// @weekly                | Run once a week, midnight on Sunday          | 0 0 0 * * 0
+	// @daily (or @midnight)  | Run once a day, midnight                     | 0 0 0 * * *
+	// @hourly                | Run once an hour, beginning of hour          | 0 0 * * * *
 	Schedule *string `protobuf:"bytes,2,opt,name=schedule,proto3,oneof" json:"schedule,omitempty"`
 	// repeats is the optional number of times in which the job should be
 	// triggered. If not set, the job will run indefinitely or until expiration.
