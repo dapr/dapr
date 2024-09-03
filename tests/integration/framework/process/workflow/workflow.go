ca411ccd824f60a2d7286ff39c9c7f4d3bd64102
joshvanl <me@joshvanl.dev>
2024-09-04 00:17:35 +0100

    Workflows: Fix daprd memory leak
    Fixes a memory leak in Workflows whereby the internal actor state held
    by daprd would never be released- meaning daprd would grow in memory
    indefinitely after each workflow execution. This memory leak occurs for
    both scheduler and non-scheduler reminders.
    
    Specifically, the actor table and internal actor workflow state maps
    where never deleted after workflow orchestration and activity
    completion. PR updates the workflow and activity actors to track whether
    it has been completed. This is signalled to the actor runtime after a
    workflow reminder execution, and the actor runtime will then delete the
    actor state.
    
    Adds integration tests to ensure daprd memory usage does not grow for
    both scheduler and non-scheduler workflows.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

---
+++ tests/integration/framework/process/workflow/workflow.go
@@ -0,0 +1,135 @@
+/*
+Copyright 2024 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package workflow
+
+import (
+	"context"
+	"testing"
+
+	"github.com/microsoft/durabletask-go/client"
+	"github.com/microsoft/durabletask-go/task"
+	"github.com/stretchr/testify/require"
+
+	rtv1 "github.com/dapr/dapr/pkg/proto/runtime/v1"
+	"github.com/dapr/dapr/tests/integration/framework/iowriter/logger"
+	"github.com/dapr/dapr/tests/integration/framework/process/daprd"
+	"github.com/dapr/dapr/tests/integration/framework/process/http/app"
+	"github.com/dapr/dapr/tests/integration/framework/process/placement"
+	"github.com/dapr/dapr/tests/integration/framework/process/scheduler"
+	"github.com/dapr/dapr/tests/integration/framework/process/sqlite"
+)
+
+type Workflow struct {
+	registry *task.TaskRegistry
+	app      *app.App
+	db       *sqlite.SQLite
+	place    *placement.Placement
+	sched    *scheduler.Scheduler
+	daprd    *daprd.Daprd
+}
+
+func New(t *testing.T, fopts ...Option) *Workflow {
+	t.Helper()
+
+	opts := options{
+		registry: task.NewTaskRegistry(),
+	}
+	for _, fopt := range fopts {
+		fopt(&opts)
+	}
+
+	app := app.New(t)
+	db := sqlite.New(t,
+		sqlite.WithActorStateStore(true),
+		sqlite.WithCreateStateTables(),
+	)
+	place := placement.New(t)
+
+	dopts := []daprd.Option{
+		daprd.WithAppPort(app.Port()),
+		daprd.WithPlacementAddresses(place.Address()),
+		daprd.WithResourceFiles(db.GetComponent(t)),
+	}
+
+	var sched *scheduler.Scheduler
+	if opts.enableScheduler {
+		sched = scheduler.New(t)
+		dopts = append(dopts,
+			daprd.WithScheduler(sched),
+			daprd.WithConfigManifests(t, `
+apiVersion: dapr.io/v1alpha1
+kind: Configuration
+metadata:
+  name: appconfig
+spec:
+  features:
+  - name: SchedulerReminders
+    enabled: true
+`))
+	}
+
+	return &Workflow{
+		registry: opts.registry,
+		app:      app,
+		db:       db,
+		place:    place,
+		sched:    sched,
+		daprd:    daprd.New(t, dopts...),
+	}
+}
+
+func (w *Workflow) Run(t *testing.T, ctx context.Context) {
+	w.app.Run(t, ctx)
+	w.db.Run(t, ctx)
+	w.place.Run(t, ctx)
+	if w.sched != nil {
+		w.sched.Run(t, ctx)
+	}
+	w.daprd.Run(t, ctx)
+}
+
+func (w *Workflow) Cleanup(t *testing.T) {
+	w.daprd.Cleanup(t)
+	if w.sched != nil {
+		w.sched.Cleanup(t)
+	}
+	w.place.Cleanup(t)
+	w.db.Cleanup(t)
+	w.app.Cleanup(t)
+}
+
+func (w *Workflow) WaitUntilRunning(t *testing.T, ctx context.Context) {
+	w.place.WaitUntilRunning(t, ctx)
+	if w.sched != nil {
+		w.sched.WaitUntilRunning(t, ctx)
+	}
+	w.daprd.WaitUntilRunning(t, ctx)
+}
+
+func (w *Workflow) BackendClient(t *testing.T, ctx context.Context) *client.TaskHubGrpcClient {
+	t.Helper()
+	backendClient := client.NewTaskHubGrpcClient(w.daprd.GRPCConn(t, ctx), logger.New(t))
+	require.NoError(t, backendClient.StartWorkItemListener(ctx, w.registry))
+	return backendClient
+}
+
+func (w *Workflow) GRPCClient(t *testing.T, ctx context.Context) rtv1.DaprClient {
+	t.Helper()
+	return w.daprd.GRPCClient(t, ctx)
+}
+
+func (w *Workflow) Metrics(t *testing.T, ctx context.Context) map[string]float64 {
+	t.Helper()
+	return w.daprd.Metrics(t, ctx)
+}
