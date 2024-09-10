7bb2bce560ad38e6dd19a5b2250f0aa73f3d1d97
Anton Troshin <anton@diagrid.io>
2024-09-10 17:17:07 -0500

    CR fixes
    Signed-off-by: Anton Troshin <anton@diagrid.io>

--- tests/integration/suite/daprd/outbox/http/delete.go
+++ tests/integration/suite/daprd/outbox/http/delete.go
@@ -1,3 +1,17 @@
+/*
+Copyright 2024 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implieh.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package http
 
 import (
@@ -14,10 +28,9 @@ import (
 	"github.com/dapr/components-contrib/state"
 	"github.com/dapr/dapr/tests/integration/framework"
 	"github.com/dapr/dapr/tests/integration/framework/client"
-	procdaprd "github.com/dapr/dapr/tests/integration/framework/process/daprd"
-	prochttp "github.com/dapr/dapr/tests/integration/framework/process/http"
+	"github.com/dapr/dapr/tests/integration/framework/process/daprd"
+	"github.com/dapr/dapr/tests/integration/framework/process/http/app"
 	"github.com/dapr/dapr/tests/integration/suite"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -28,42 +41,23 @@ func init() {
 
 type delete struct {
 	appTestCalled atomic.Int32
-	daprd         *procdaprd.Daprd
+	daprd         *daprd.Daprd
+	msg           atomic.Value
 }
 
-var msg atomic.Value
-
 func (o *delete) Setup(t *testing.T) []framework.Option {
-	newHTTPServer := func() *prochttp.HTTP {
-		handler := http.NewServeMux()
-
-		handler.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-			o.appTestCalled.Add(1)
-			defer r.Body.Close()
-			b, err := io.ReadAll(r.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			msg.Store(b)
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("ok"))
-		})
-
-		handler.HandleFunc("/getValue", func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			m := msg.Load()
-			if m == nil {
-				return
-			}
-			w.Write(msg.Load().([]byte))
-		})
-
-		return prochttp.New(t, prochttp.WithHandler(handler))
-	}
-	srv1 := newHTTPServer()
-
-	o.daprd = procdaprd.New(t, procdaprd.WithAppID("outboxtest"), procdaprd.WithAppPort(srv1.Port()), procdaprd.WithResourceFiles(`
+	app := app.New(t,
+		app.WithHandlerFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+			defer o.appTestCalled.Add(1)
+			var ce map[string]string
+			require.NoError(t, json.NewDecoder(r.Body).Decode(&ce))
+			o.msg.Store(ce)
+		}),
+	)
+	o.daprd = daprd.New(t,
+		daprd.WithAppID("outboxtest"),
+		daprd.WithAppPort(app.Port()),
+		daprd.WithResourceFiles(`
 apiVersion: dapr.io/v1alpha1
 kind: Component
 metadata:
@@ -77,7 +71,7 @@ spec:
   - name: outboxPublishTopic
     value: "test"
 `,
-		`
+			`
 apiVersion: dapr.io/v1alpha1
 kind: Component
 metadata:
@@ -86,7 +80,7 @@ spec:
   type: pubsub.in-memory
   version: v1
 `,
-		`
+			`
 apiVersion: dapr.io/v2alpha1
 kind: Subscription
 metadata:
@@ -99,114 +93,77 @@ spec:
 scopes:
 - outboxtest
 `))
-
 	return []framework.Option{
-		framework.WithProcesses(srv1, o.daprd),
+		framework.WithProcesses(app, o.daprd),
 	}
 }
-
 func (o *delete) Run(t *testing.T, ctx context.Context) {
 	o.daprd.WaitUntilRunning(t, ctx)
-
-	postURL := fmt.Sprintf("http://localhost:%d/v1.0/state/mystore/transaction", o.daprd.HTTPPort())
-	stateReq := state.SetRequest{
-		Key:      "1",
-		Value:    "2",
-		Metadata: map[string]string{"outbox.cloudevent.myapp": "myapp1", "data": "a", "id": "b"},
-	}
-
-	tr := stateTransactionRequestBody{
+	transactionRequest, err := json.Marshal(&stateTransactionRequestBody{
 		Operations: []stateTransactionRequestBodyOperation{
 			{
 				Operation: "upsert",
-				Request:   stateReq,
+				Request: state.SetRequest{
+					Key:      "1",
+					Value:    "2",
+					Metadata: map[string]string{"outbox.cloudevent.myapp": "myapp1", "data": "a", "id": "b"},
+				},
 			},
 		},
-	}
-
-	b, err := json.Marshal(&tr)
+	})
 	require.NoError(t, err)
-
-	httpClient := client.HTTP(t)
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, postURL, bytes.NewReader(b))
+	client := client.HTTP(t)
+	postURL := fmt.Sprintf("http://%s/v1.0/state/mystore/transaction", o.daprd.HTTPAddress())
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, postURL, bytes.NewReader(transactionRequest))
 	require.NoError(t, err)
-	resp, err := httpClient.Do(req)
+	resp, err := client.Do(req)
 	require.NoError(t, err)
 	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
 	body, err := io.ReadAll(resp.Body)
 	require.NoError(t, err)
 	require.NoError(t, resp.Body.Close())
 	assert.Empty(t, string(body))
-
 	assert.EventuallyWithT(t, func(c *assert.CollectT) {
-		req, err = http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://localhost:%v/getValue", o.daprd.AppPort(t)), nil)
-		require.NoError(c, err)
-		resp, err = httpClient.Do(req)
-		require.NoError(c, err)
-		t.Cleanup(func() {
-			require.NoError(t, resp.Body.Close())
-		})
-		body, err = io.ReadAll(resp.Body)
-		require.NoError(c, err)
-
-		var ce map[string]string
-		err = json.Unmarshal(body, &ce)
-		//nolint:testifylint
-		assert.NoError(c, err)
+		ce, ok := o.msg.Load().(map[string]string)
+		if !assert.True(c, ok) {
+			return
+		}
 		assert.Equal(c, "2", ce["data"])
 		assert.Equal(c, "myapp1", ce["outbox.cloudevent.myapp"])
 		assert.Contains(c, ce["id"], "outbox-")
-	}, time.Second*60, time.Millisecond*10)
-
+	}, time.Second*10, time.Millisecond*10)
 	assert.Equal(t, int32(1), o.appTestCalled.Load())
-
-	dr := stateTransactionRequestBody{
+	transactionRequest, err = json.Marshal(&stateTransactionRequestBody{
 		Operations: []stateTransactionRequestBodyOperation{
 			{
 				Operation: "delete",
 				Request: state.DeleteRequest{
-					Key:      stateReq.Key,
+					Key:      "1",
 					Metadata: map[string]string{"outbox.cloudevent.myapp": "myapp2", "data": "a", "id": "b"},
 				},
 			},
 		},
-	}
-
-	b, err = json.Marshal(&dr)
+	})
 	require.NoError(t, err)
-
-	req, err = http.NewRequestWithContext(ctx, http.MethodPost, postURL, bytes.NewReader(b))
+	req, err = http.NewRequestWithContext(ctx, http.MethodPost, postURL, bytes.NewReader(transactionRequest))
 	require.NoError(t, err)
-	resp, err = httpClient.Do(req)
+	resp, err = client.Do(req)
 	require.NoError(t, err)
 	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
 	body, err = io.ReadAll(resp.Body)
 	require.NoError(t, err)
 	require.NoError(t, resp.Body.Close())
 	assert.Empty(t, string(body))
-
 	//
 	assert.EventuallyWithT(t, func(c *assert.CollectT) {
-		req, err = http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://localhost:%v/getValue", o.daprd.AppPort(t)), nil)
-		require.NoError(c, err)
-		resp, err = httpClient.Do(req)
-		require.NoError(c, err)
-		t.Cleanup(func() {
-			require.NoError(t, resp.Body.Close())
-		})
-		body, err = io.ReadAll(resp.Body)
-		require.NoError(c, err)
-
-		var ce map[string]string
-		err = json.Unmarshal(body, &ce)
-		//nolint:testifylint
-		assert.NoError(c, err)
+		ce, ok := o.msg.Load().(map[string]string)
+		if !assert.True(c, ok) {
+			return
+		}
 		assert.Equal(c, "2", ce["data"])
 		assert.Equal(c, "myapp1", ce["outbox.cloudevent.myapp"])
 		assert.Contains(c, ce["id"], "outbox-")
-	}, time.Second*60, time.Millisecond*10)
-
+	}, time.Second*10, time.Millisecond*10)
 	// expecting the delete to not call the test app topic and calls counter stay the same
 	assert.Equal(t, int32(1), o.appTestCalled.Load())
 }
