1d7e84d6b2311ee1c5784ccaa59679098478e60d
joshvanl <me@joshvanl.dev>
2024-09-18 19:32:59 +0100

    Lint
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/suite/actors/reminders/scheduler/hop.go
+++ tests/integration/suite/actors/reminders/scheduler/hop.go
@@ -119,7 +119,7 @@ func (h *hop) Run(t *testing.T, ctx context.Context) {
 			DueTime:   "0s",
 			Data:      []byte("reminderdata"),
 		})
-		require.NoError(c, err)
+		require.NoError(t, err)
 	}
 
 	assert.Eventually(t, func() bool {
