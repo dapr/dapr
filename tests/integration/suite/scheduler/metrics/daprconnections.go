fde72c71e6d7605da715f503b58c481b9d82d41d
Cassandra Coyle <cassie@diagrid.io>
2024-09-30 15:29:27 -0500

    update daprconnections test to rm logline
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/integration/suite/scheduler/metrics/daprconnections.go
+++ tests/integration/suite/scheduler/metrics/daprconnections.go
@@ -16,14 +16,13 @@ package metrics
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/dapr/dapr/tests/integration/framework"
 	"github.com/dapr/dapr/tests/integration/framework/process/daprd"
-	"github.com/dapr/dapr/tests/integration/framework/process/exec"
 	"github.com/dapr/dapr/tests/integration/framework/process/grpc/app"
-	"github.com/dapr/dapr/tests/integration/framework/process/logline"
 	"github.com/dapr/dapr/tests/integration/framework/process/scheduler"
 	"github.com/dapr/dapr/tests/integration/suite"
 )
@@ -34,25 +33,30 @@ func init() {
 
 type daprconnections struct {
 	scheduler *scheduler.Scheduler
-	daprd     *daprd.Daprd
-
-	connectionlogline *logline.LogLine
+	daprdA    *daprd.Daprd
+	daprdB    *daprd.Daprd
+	daprdC    *daprd.Daprd
 }
 
 func (c *daprconnections) Setup(t *testing.T) []framework.Option {
-	c.connectionlogline = logline.New(t,
-		logline.WithStdoutLineContains(
-			`Adding a Sidecar connection to Scheduler for appID: default/C`,
-		),
-	)
-	c.scheduler = scheduler.New(t,
-		scheduler.WithLogLevel("debug"),
-		scheduler.WithExecOptions(
-			exec.WithStdout(c.connectionlogline.Stdout()),
-		),
-	)
+	c.scheduler = scheduler.New(t)
 	srv := app.New(t)
-	c.daprd = daprd.New(t,
+
+	c.daprdA = daprd.New(t,
+		daprd.WithSchedulerAddresses(c.scheduler.Address()),
+		daprd.WithAppProtocol("grpc"),
+		daprd.WithAppPort(srv.Port(t)),
+		daprd.WithAppID("A"),
+	)
+
+	c.daprdB = daprd.New(t,
+		daprd.WithSchedulerAddresses(c.scheduler.Address()),
+		daprd.WithAppProtocol("grpc"),
+		daprd.WithAppPort(srv.Port(t)),
+		daprd.WithAppID("B"),
+	)
+
+	c.daprdC = daprd.New(t,
 		daprd.WithSchedulerAddresses(c.scheduler.Address()),
 		daprd.WithAppProtocol("grpc"),
 		daprd.WithAppPort(srv.Port(t)),
@@ -60,18 +64,43 @@ func (c *daprconnections) Setup(t *testing.T) []framework.Option {
 	)
 
 	return []framework.Option{
-		framework.WithProcesses(c.connectionlogline, srv, c.scheduler, c.daprd),
+		framework.WithProcesses(srv, c.scheduler),
 	}
 }
 
 func (c *daprconnections) Run(t *testing.T, ctx context.Context) {
 	c.scheduler.WaitUntilRunning(t, ctx)
-	c.daprd.WaitUntilRunning(t, ctx)
 
 	t.Run("ensure dapr connection with scheduler metric", func(t *testing.T) {
+		// 0 sidecars connected
 		metrics := c.scheduler.Metrics(t, ctx)
-		assert.Equal(t, 1, int(metrics["dapr_scheduler_sidecars_connected_total"]))
-	})
+		assert.Equal(t, 0, int(metrics["dapr_scheduler_sidecars_connected_total"]))
 
-	c.connectionlogline.EventuallyFoundAll(t)
+		// 1 sidecar connected
+		c.daprdA.Run(t, ctx)
+		t.Cleanup(func() { c.daprdA.Cleanup(t) })
+		c.daprdA.WaitUntilRunning(t, ctx)
+		metrics = c.scheduler.Metrics(t, ctx)
+		assert.EventuallyWithT(t, func(c *assert.CollectT) {
+			assert.Equal(t, 1, int(metrics["dapr_scheduler_sidecars_connected_total"]))
+		}, 10*time.Second, 10*time.Millisecond, "daprdA sidecar didn't connect to Scheduler in time") // nolint:mnd
+
+		// 2 sidecars connected
+		c.daprdB.Run(t, ctx)
+		t.Cleanup(func() { c.daprdB.Cleanup(t) })
+		c.daprdB.WaitUntilRunning(t, ctx)
+		metrics = c.scheduler.Metrics(t, ctx)
+		assert.EventuallyWithT(t, func(c *assert.CollectT) {
+			assert.Equal(t, 2, int(metrics["dapr_scheduler_sidecars_connected_total"]))
+		}, 10*time.Second, 10*time.Millisecond, "daprdB sidecar didn't connect to Scheduler in time") // nolint:mnd
+
+		// 3 sidecars connected
+		c.daprdC.Run(t, ctx)
+		t.Cleanup(func() { c.daprdC.Cleanup(t) })
+		c.daprdC.WaitUntilRunning(t, ctx)
+		metrics = c.scheduler.Metrics(t, ctx)
+		assert.EventuallyWithT(t, func(c *assert.CollectT) {
+			assert.Equal(t, 3, int(metrics["dapr_scheduler_sidecars_connected_total"]))
+		}, 10*time.Second, 10*time.Millisecond, "daprdC sidecar didn't connect to Scheduler in time") // nolint:mnd
+	})
 }
