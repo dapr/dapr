1b79cc8e2a801568c05f1ab9c7066d35516e38db
yaron2 <schneider.yaron@live.com>
2024-10-22 11:25:56 -0700

    ensure dapr-api-token gets removed from metadata after validaton
    Signed-off-by: yaron2 <schneider.yaron@live.com>

--- pkg/api/grpc/auth.go
+++ pkg/api/grpc/auth.go
@@ -4,44 +4,55 @@ import (
 	"context"
 	"net/http"
 
-	"google.golang.org/grpc"
-
 	"github.com/dapr/dapr/pkg/api/grpc/metadata"
 	invokev1 "github.com/dapr/dapr/pkg/messaging/v1"
+	"google.golang.org/grpc"
+	grpc_metadata "google.golang.org/grpc/metadata"
 )
+
+type wrappedStream struct {
+	grpc.ServerStream
+	ctx context.Context
+}
+
+func (s *wrappedStream) Context() context.Context {
+	return s.ctx
+}
 
 func getAPIAuthenticationMiddlewares(apiToken, authHeader string) (grpc.UnaryServerInterceptor, grpc.StreamServerInterceptor) {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
-			err := checkAPITokenInContext(ctx, apiToken, authHeader)
+			authCtx, err := checkAPITokenInContext(ctx, apiToken, authHeader)
 			if err != nil {
 				return nil, err
 			}
-			return handler(ctx, req)
+			return handler(authCtx, req)
 		},
 		func(srv any, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-			err := checkAPITokenInContext(stream.Context(), apiToken, authHeader)
+			authCtx, err := checkAPITokenInContext(stream.Context(), apiToken, authHeader)
 			if err != nil {
 				return err
 			}
-			return handler(srv, stream)
+
+			return handler(srv, &wrappedStream{stream, authCtx})
 		}
 }
 
 // Checks if the API token in the gRPC request's context is valid; returns an error otherwise.
-func checkAPITokenInContext(ctx context.Context, apiToken, authHeader string) error {
+func checkAPITokenInContext(ctx context.Context, apiToken, authHeader string) (context.Context, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return invokev1.ErrorFromHTTPResponseCode(http.StatusUnauthorized, "missing metadata in request")
+		return ctx, invokev1.ErrorFromHTTPResponseCode(http.StatusUnauthorized, "missing metadata in request")
 	}
 
 	if len(md[authHeader]) == 0 {
-		return invokev1.ErrorFromHTTPResponseCode(http.StatusUnauthorized, "missing api token in request metadata")
+		return ctx, invokev1.ErrorFromHTTPResponseCode(http.StatusUnauthorized, "missing api token in request metadata")
 	}
 
 	if md[authHeader][0] != apiToken {
-		return invokev1.ErrorFromHTTPResponseCode(http.StatusUnauthorized, "authentication error: api token mismatch")
+		return ctx, invokev1.ErrorFromHTTPResponseCode(http.StatusUnauthorized, "authentication error: api token mismatch")
 	}
 
-	md.Set(authHeader, "")
-	return nil
+	md.Delete(authHeader)
+	ctx = grpc_metadata.NewIncomingContext(ctx, md.Copy())
+	return ctx, nil
 }
