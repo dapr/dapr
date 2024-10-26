49382a80a1c7cca7cce802f85176e6efe11d4c2f
Cassandra Coyle <cassie@diagrid.io>
2024-10-25 21:10:50 -0500

    restore tests/platforms/kubernetes/logs.go
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/platforms/kubernetes/logs.go
+++ tests/platforms/kubernetes/logs.go
@@ -56,10 +56,6 @@ func StreamContainerLogsToDisk(ctx context.Context, appName string, podClient v1
 			loop:
 				for {
 					filename := filepath.Join(logPrefix, fmt.Sprintf("%s.%s.log", pod, container))
-					if strings.HasSuffix(container, "k6") {
-						// add the appname to the k6 filename to be able to know what app those logs belong to
-						filename = filepath.Join(logPrefix, fmt.Sprintf("%s.%s.%s.log", appName, pod, container))
-					}
 					log.Printf("Streaming Kubernetes logs to %s", filename)
 					req := podClient.GetLogs(pod, &apiv1.PodLogOptions{
 						Container: container,
