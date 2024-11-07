2b7872ace4bdb778fc944c9dfbf9cff83f4b4add
Luis Rascao <luis.rascao@gmail.com>
2024-11-07 15:05:19 +0000

    fixup! Add a resource reporter
    Signed-off-by: Luis Rascao <luis.rascao@gmail.com>

--- pkg/runtime/processor/components.go
+++ pkg/runtime/processor/components.go
@@ -97,7 +97,8 @@ func (p *Processor) Close(comp componentsapi.Component) error {
 		message = ptr.Of(closeErr.Error())
 	}
 
-	if err := p.reporter(context.TODO(), comp,
+	ctx := context.WithTimeout(context.Background(), 5*time.Second)
+	if err := p.reporter(ctx, comp,
 		operatorv1.ResourceResult{
 			ResourceType:        operatorv1.ResourceType_RESOURCE_COMPONENT,
 			EventType:           operatorv1.EventType_EVENT_CLOSE,
