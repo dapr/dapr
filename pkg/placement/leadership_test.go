53e91b78bb727bf6fcdeb3154b47d080d1c30ad2
Elena Kolevska <elena@kolevska.com>
2024-12-31 11:36:11 +0000

    Extends leadership test to prove we have a problem
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- pkg/placement/leadership_test.go
+++ pkg/placement/leadership_test.go
@@ -19,6 +19,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	hashicorpRaft "github.com/hashicorp/raft"
@@ -62,7 +63,6 @@ func TestMonitorLeadership(t *testing.T) {
 	numServers := len(raftClusterOpts)
 	placementServers := make([]*Service, numServers)
 	cleanupFns := make([]context.CancelFunc, numServers)
-	leaderChannels := make([]<-chan bool, numServers)
 	underlyingRaftServers := make([]*hashicorpRaft.Raft, numServers)
 
 	// Setup Raft and placement servers
@@ -72,44 +72,65 @@ func TestMonitorLeadership(t *testing.T) {
 		raft, err := placementServers[i].raftNode.Raft(ctx)
 		require.NoError(t, err)
 
-		leaderChannels[i] = raft.LeaderCh()
 		underlyingRaftServers[i] = raft
 
 		go placementServers[i].MonitorLeadership(ctx)
 	}
 
-	// Find the initial leader
+	// firstServerID is the initial leader
 	i := 0
-	var leaderIdx int
-	require.Eventually(t, func() bool {
+	var firstServerID int
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
 		idx := i % numServers
 		i++
-		if placementServers[idx].raftNode.IsLeader() && placementServers[idx].hasLeadership.Load() {
-			leaderIdx = idx
-			return true
-		}
-		return false
+		assert.True(c, placementServers[idx].raftNode.IsLeader())
+		assert.True(c, placementServers[idx].hasLeadership.Load())
+		firstServerID = idx
 	}, time.Second*15, 10*time.Millisecond, "leader was not elected in time")
 
-	secondServerID := (leaderIdx + 1) % numServers
+	secondServerID := (firstServerID + 1) % numServers
+	thirdServerID := (secondServerID + 1) % numServers
 
-	// We need to verify that the placement server will properly follow the raft leadership
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.False(c, placementServers[secondServerID].raftNode.IsLeader())
+		assert.False(c, placementServers[secondServerID].hasLeadership.Load())
+
+		assert.False(c, placementServers[thirdServerID].raftNode.IsLeader())
+		assert.False(c, placementServers[thirdServerID].hasLeadership.Load())
+	}, time.Second*15, 10*time.Millisecond, "leader was not elected in time")
+
 	// Transfer leadership away from the original leader to the second server
-	underlyingRaftServers[leaderIdx].LeadershipTransferToServer(hashicorpRaft.ServerID(placementServers[secondServerID].raftNode.GetID()), hashicorpRaft.ServerAddress(placementServers[secondServerID].raftNode.GetRaftBind()))
-	require.Eventually(t, func() bool {
-		return placementServers[secondServerID].raftNode.IsLeader() && placementServers[secondServerID].hasLeadership.Load()
-	}, time.Second*15, 10*time.Millisecond, "raft server two was not set as leader in time")
+	underlyingRaftServers[firstServerID].LeadershipTransferToServer(hashicorpRaft.ServerID(placementServers[secondServerID].raftNode.GetID()), hashicorpRaft.ServerAddress(placementServers[secondServerID].raftNode.GetRaftBind()))
+
+	// Verify that the placement server will properly follow the raft leadership
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.True(c, placementServers[secondServerID].raftNode.IsLeader())
+		assert.True(c, placementServers[secondServerID].hasLeadership.Load())
+
+		assert.False(c, placementServers[firstServerID].raftNode.IsLeader())
+		assert.False(c, placementServers[firstServerID].hasLeadership.Load())
+
+		assert.False(c, placementServers[thirdServerID].raftNode.IsLeader())
+		assert.False(c, placementServers[thirdServerID].hasLeadership.Load())
+	}, time.Second*15, 10*time.Millisecond, "raft server was not properly re-elected in time")
 
 	// Transfer leadership back to the original leader
 	var future hashicorpRaft.Future
 	require.Eventually(t, func() bool {
-		future = underlyingRaftServers[secondServerID].LeadershipTransferToServer(hashicorpRaft.ServerID(placementServers[leaderIdx].raftNode.GetID()), hashicorpRaft.ServerAddress(placementServers[leaderIdx].raftNode.GetRaftBind()))
+		future = underlyingRaftServers[secondServerID].LeadershipTransferToServer(hashicorpRaft.ServerID(placementServers[firstServerID].raftNode.GetID()), hashicorpRaft.ServerAddress(placementServers[firstServerID].raftNode.GetRaftBind()))
 		return future.Error() == nil
 	}, time.Second*15, 500*time.Millisecond, "raft leadership transfer timeout")
 
-	require.Eventually(t, func() bool {
-		return placementServers[leaderIdx].raftNode.IsLeader() && placementServers[leaderIdx].hasLeadership.Load()
-	}, time.Second*15, 10*time.Millisecond, "server was not properly re-elected in time")
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.True(c, placementServers[firstServerID].raftNode.IsLeader())
+		assert.True(c, placementServers[firstServerID].hasLeadership.Load())
+
+		assert.False(c, placementServers[secondServerID].raftNode.IsLeader())
+		assert.False(c, placementServers[secondServerID].hasLeadership.Load())
+
+		assert.False(c, placementServers[thirdServerID].raftNode.IsLeader())
+		assert.False(c, placementServers[thirdServerID].hasLeadership.Load())
+	}, time.Second*15, 10*time.Millisecond, "raft server was not properly re-elected in time")
 
 	t.Cleanup(func() {
 		for i := range numServers {
