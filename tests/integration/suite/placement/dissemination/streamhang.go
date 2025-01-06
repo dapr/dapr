109f3ed5fe7f560176181081c4a66363e38a4298
Elena Kolevska <elena@kolevska.com>
2025-01-06 11:22:57 +0000

    stream hang test
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

---
+++ tests/integration/suite/placement/dissemination/streamhang.go
@@ -0,0 +1,232 @@
+/*
+Copyright 2024 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dissemination
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+
+	v1pb "github.com/dapr/dapr/pkg/proto/placement/v1"
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/placement"
+	"github.com/dapr/dapr/tests/integration/suite"
+)
+
+func init() {
+	suite.Register(new(streamHang))
+}
+
+type streamHang struct {
+	place *placement.Placement
+}
+
+func (n *streamHang) Setup(t *testing.T) []framework.Option {
+	n.place = placement.New(t)
+
+	return []framework.Option{
+		framework.WithProcesses(n.place),
+	}
+}
+
+func (n *streamHang) Run(t *testing.T, ctx context.Context) {
+	n.place.WaitUntilRunning(t, ctx)
+
+	// Set up host1 and stream1 (not reading from stream)
+	host1 := &v1pb.Host{
+		Name:      "local/myapp1",
+		Namespace: "default",
+		Port:      1232,
+		Entities:  getActorsList(4000, "host1"),
+		Id:        "myapp1",
+		ApiLevel:  uint32(20),
+	}
+
+	ctx1, cancel1 := context.WithCancel(ctx)
+	t.Cleanup(cancel1)
+	stream1, streamCancel1 := n.getStream(t, ctx1)
+	t.Cleanup(streamCancel1)
+	err := stream1.Send(host1)
+	require.NoError(t, err, "Failed to send host1")
+
+	// Intentionally not reading from stream1 to simulate a hang on the larger message after host 1 connects
+
+	time.Sleep(2 * time.Second) // Wait until first dissemination is triggered
+
+	host2 := &v1pb.Host{
+		Name:      "local/myapp2",
+		Namespace: "default",
+		Port:      1231,
+		Entities:  getActorsList(10, "host2"),
+		Id:        "myapp2",
+		ApiLevel:  uint32(20),
+	}
+
+	ctx2, cancel2 := context.WithCancel(ctx)
+	t.Cleanup(cancel2)
+	stream2, streamCancel2 := n.getStream(t, ctx2)
+	t.Cleanup(streamCancel2)
+	err = stream2.Send(host2)
+	require.NoError(t, err, "Failed to send host2")
+
+	// Start reading from stream2
+	var wg sync.WaitGroup
+	wg.Add(1)
+	updateCh2 := make(chan *v1pb.PlacementTables)
+	go func() {
+		defer wg.Done()
+		defer close(updateCh2)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+
+			fmt.Println("Reading from stream2...")
+			resp, err := stream2.Recv()
+			fmt.Println("Received message from stream2 \t\t\t\t", resp.GetOperation(), len(resp.GetTables().GetEntries()), err, time.Now())
+			if err != nil {
+				break
+			}
+
+			if resp.GetOperation() == "update" {
+				updateCh2 <- resp.GetTables()
+			}
+		}
+		fmt.Println("Reading from stream2 done")
+	}()
+
+	// The placement tables message that the Placement server will try to disseminate to stream1 will
+	// contain 9001 actors and around 90Kb - higher than the default stream write buffer size of 32KBs
+	// So around this moment of the test, we're expecting the stream1 to hang and subsequently get removed
+	// Then stream 2 should receive a new dissemination message containing only the actors from host2
+	// Because of the retry mechanism on the placement side (3x1sec) plus the dissemination window
+	// this takes a while
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		placementTables, ok := <-updateCh2
+		require.True(t, ok)
+		fmt.Println("Placement tables received on stream2", len(placementTables.GetEntries()))
+		assert.Len(c, placementTables.GetEntries(), 10)
+		assert.Contains(c, placementTables.GetEntries(), "host2-0")
+	}, 20*time.Second, 100*time.Millisecond)
+
+	time.Sleep(1 * time.Second) // Wait until first dissemination is triggered
+
+	// Set up host3 and stream3 (reading from stream)
+	host3 := &v1pb.Host{
+		Name:      "local/myapp3",
+		Namespace: "default",
+		Port:      1233,
+		Entities:  getActorsList(3, "host3"),
+		Id:        "myapp3",
+		ApiLevel:  uint32(20),
+	}
+
+	ctx3, cancel3 := context.WithCancel(ctx)
+	t.Cleanup(cancel3)
+	stream3, streamCancel3 := n.getStream(t, ctx3)
+	t.Cleanup(streamCancel3)
+	err = stream3.Send(host3)
+	require.NoError(t, err, "Failed to send host3")
+
+	err = stream2.Send(host2)
+	require.NoError(t, err, "Failed to send host2")
+
+	// Start reading from stream3
+	wg.Add(1)
+	updateCh3 := make(chan *v1pb.PlacementTables)
+	go func() {
+		defer wg.Done()
+		defer close(updateCh3)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+
+			resp, err := stream3.Recv()
+			fmt.Println("Received message from stream3\t\t\t\t\t\t", resp.GetOperation(), len(resp.GetTables().GetEntries()), err, time.Now())
+			if err != nil {
+				break
+			}
+
+			if resp.GetOperation() == "update" {
+				updateCh3 <- resp.GetTables()
+			}
+		}
+		fmt.Println("Reading from stream3 done")
+	}()
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		placementTables, ok := <-updateCh3
+		require.True(t, ok)
+		assert.Len(c, placementTables.GetEntries(), 13)
+		assert.Contains(c, placementTables.GetEntries(), "host2-0")
+		assert.Contains(c, placementTables.GetEntries(), "host3-0")
+		assert.NotContains(c, placementTables.GetEntries(), "host1-0")
+	}, 15*time.Second, 500*time.Millisecond)
+
+	// Clean up
+	fmt.Println("Closing stream 1")
+	streamCancel1()
+	fmt.Println("Closing stream 2")
+	streamCancel2()
+	fmt.Println("Closing stream 3")
+	streamCancel3()
+	fmt.Println("Closed all streams. Waiting on wg.")
+	wg.Wait()
+	fmt.Println("wg.Wait done")
+}
+
+func (n *streamHang) getStream(t *testing.T, ctx context.Context) (v1pb.Placement_ReportDaprStatusClient, func()) {
+	t.Helper()
+
+	//nolint:staticcheck
+	conn, err := grpc.DialContext(ctx, n.place.Address(),
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+	client := v1pb.NewPlacementClient(conn)
+	ctx = metadata.AppendToOutgoingContext(ctx, "dapr-accept-vnodes", "false")
+
+	stream, err := client.ReportDaprStatus(ctx)
+	require.NoError(t, err)
+
+	cancel := func() {
+		fmt.Println("Closing stream")
+		stream.CloseSend()
+		conn.Close()
+	}
+
+	return stream, cancel
+}
+
+func getActorsList(numActors int, prefix string) []string {
+	var actors []string
+	for i := range numActors {
+		actors = append(actors, fmt.Sprintf("%s-%d", prefix, i))
+	}
+	return actors
+}
