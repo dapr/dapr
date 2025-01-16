98da89c80fbe17eb406afe8822f25c8454cec611
joshvanl <me@joshvanl.dev>
2025-01-16 14:13:41 +0000

    Fix streaming staged test for slow CI
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/suite/daprd/jobs/streaming/staged.go
+++ tests/integration/suite/daprd/jobs/streaming/staged.go
@@ -106,13 +106,6 @@ func (s *staged) Run(t *testing.T, ctx context.Context) {
 	})
 	require.NoError(t, err)
 
-	time.Sleep(2 * time.Second)
-	assert.Equal(t, []string{"test"}, s.triggered.Slice())
-
-	s.daprdB.Run(t, ctx)
-	t.Cleanup(func() { s.daprdB.Cleanup(t) })
-	s.daprdB.WaitUntilRunning(t, ctx)
-
 	numOf := func(ss []string, k string) int {
 		var j int
 		for _, s := range ss {
@@ -123,6 +116,14 @@ func (s *staged) Run(t *testing.T, ctx context.Context) {
 
 		return j
 	}
+
+	time.Sleep(2 * time.Second)
+	assert.GreaterOrEqual(t, numOf(s.triggered.Slice(), "test"), 2)
+	assert.Equal(t, 0, numOf(s.triggered.Slice(), "test2"))
+
+	s.daprdB.Run(t, ctx)
+	t.Cleanup(func() { s.daprdB.Cleanup(t) })
+	s.daprdB.WaitUntilRunning(t, ctx)
 
 	assert.EventuallyWithT(t, func(c *assert.CollectT) {
 		// Allow for greater that three "test"s because a trigger may have failed
