95f8c2bcf3295780c02673d0e9ea3a33626e3a8a
Cassie Coyle <cassie.i.coyle@gmail.com>
2025-01-23 10:56:09 -0600

    Update tests/e2e/actor_features/actor_features_test.go
    Co-authored-by: Josh van Leeuwen <me@joshvanl.dev>
    Signed-off-by: Cassie Coyle <cassie.i.coyle@gmail.com>

--- tests/e2e/actor_features/actor_features_test.go
+++ tests/e2e/actor_features/actor_features_test.go
@@ -383,7 +383,7 @@ func TestActorFeatures(t *testing.T) {
 		req := actorReminderOrTimer{
 			Data:    "reminderdata",
 			DueTime: "1s",
-			Period:  "20s",
+			Period:  "10s",
 		}
 		reqBody, _ := json.Marshal(req)
 		res, err = httpPost(fmt.Sprintf(actorInvokeURLFormat, externalURL, actorID, "reminders", reminderName), reqBody)
