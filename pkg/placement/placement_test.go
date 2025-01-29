0bce1d4f8e13b954419519ccf9df9e81c268742f
Elena Kolevska <elena@kolevska.com>
2025-01-29 13:03:58 +0000

    Increase test timeout
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- pkg/placement/placement_test.go
+++ pkg/placement/placement_test.go
@@ -71,13 +71,13 @@ func newTestPlacementServer(t *testing.T, raftOptions raft.Options) (string, *Se
 			conn.Close()
 		}
 		return err == nil
-	}, time.Second*10, time.Millisecond, "server did not start in time")
+	}, time.Second*20, time.Millisecond, "server did not start in time")
 
 	cleanUpFn := func() {
 		cancel()
 		select {
 		case <-serverStopped:
-		case <-time.After(time.Second * 10):
+		case <-time.After(time.Second * 20):
 			t.Error("server did not stop in time")
 		}
 	}
