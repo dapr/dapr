df14067d38d7922a203918e9191969b75227e74b
joshvanl <me@joshvanl.dev>
2025-02-01 17:57:37 -0800

    Adds gate to workflow backend orchestration create until types are fully registered
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/runtime/wfengine/backends/actors/actors.go
+++ pkg/runtime/wfengine/backends/actors/actors.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 
 	"google.golang.org/grpc/codes"
@@ -73,6 +74,9 @@ type Actors struct {
 
 	orchestrationWorkItemChan chan *backend.OrchestrationWorkItem
 	activityWorkItemChan      chan *backend.ActivityWorkItem
+
+	registeredCh chan struct{}
+	lock         sync.Mutex
 }
 
 func New(opts Options) (*Actors, error) {
@@ -85,6 +89,7 @@ func New(opts Options) (*Actors, error) {
 		schedulerReminders:        opts.SchedulerReminders,
 		orchestrationWorkItemChan: make(chan *backend.OrchestrationWorkItem, 1),
 		activityWorkItemChan:      make(chan *backend.ActivityWorkItem, 1),
+		registeredCh:              make(chan struct{}),
 	}, nil
 }
 
@@ -148,6 +153,8 @@ func (abe *Actors) RegisterActors(ctx context.Context) error {
 		},
 	)
 
+	close(abe.registeredCh)
+
 	return nil
 }
 
@@ -156,6 +163,12 @@ func (abe *Actors) UnRegisterActors(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
+	defer func() {
+		abe.lock.Lock()
+		abe.registeredCh = make(chan struct{})
+		abe.lock.Unlock()
+	}()
 
 	return table.UnRegisterActorTypes(abe.workflowActorType, abe.activityActorType)
 }
@@ -166,6 +179,16 @@ func (abe *Actors) UnRegisterActors(ctx context.Context) error {
 // request is saved into the actor's "inbox" and then executed via a reminder thread. If the app is
 // scaled out across multiple replicas, the actor might get assigned to a replicas other than this one.
 func (abe *Actors) CreateOrchestrationInstance(ctx context.Context, e *backend.HistoryEvent, opts ...backend.OrchestrationIdReusePolicyOptions) error {
+	abe.lock.Lock()
+	ch := abe.registeredCh
+	abe.lock.Unlock()
+
+	select {
+	case <-ch:
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
 	var workflowInstanceID string
 	if es := e.GetExecutionStarted(); es == nil {
 		return errors.New("the history event must be an ExecutionStartedEvent")
