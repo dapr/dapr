3f11b17e10357aec09e6e3651ed39c5c3f0eaab1
joshvanl <me@joshvanl.dev>
2025-02-21 14:35:14 -0300

    Increase timeout of invoke test loop
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/suite/daprd/serviceinvocation/http/httpendpoints.go
+++ tests/integration/suite/daprd/serviceinvocation/http/httpendpoints.go
@@ -161,7 +161,7 @@ func (h *httpendpoints) Run(t *testing.T, ctx context.Context) {
 				assert.NoError(t, resp.Body.Close())
 				endpoints, ok := body["httpEndpoints"]
 				_ = assert.True(t, ok) && assert.Len(t, endpoints.([]any), 2)
-			}, time.Second*5, time.Millisecond*10)
+			}, time.Second*10, time.Millisecond*10)
 		}
 
 		t.Run("invoke http endpoint", func(t *testing.T) {
