6f3a20c111bdc9ad30bf47c1f05c99254cec19b6
nelson.parente <nelson_parente@live.com.pt>
2025-03-24 19:13:38 +0000

    test: add integration test
    Signed-off-by: nelson.parente <nelson_parente@live.com.pt>

---
+++ tests/integration/suite/sentry/validator/kubernetes/standalone.go
@@ -0,0 +1,90 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubernetes
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/dapr/dapr/pkg/modes"
+	sentrypbv1 "github.com/dapr/dapr/pkg/proto/sentry/v1"
+	"github.com/dapr/dapr/pkg/sentry/server/ca"
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/sentry"
+	"github.com/dapr/dapr/tests/integration/suite"
+)
+
+func init() {
+	suite.Register(new(standalone))
+}
+
+// standalone tests Sentry in standalone mode in kubernetes
+type standalone struct {
+	sentry *sentry.Sentry
+}
+
+func (k *standalone) Setup(t *testing.T) []framework.Option {
+	rootKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+	bundle, err := ca.GenerateBundle(rootKey, "integration.test.dapr.io", time.Second*5, nil)
+	require.NoError(t, err)
+
+	kubeAPI := kubeAPI(t, kubeAPIOptions{
+		bundle:         bundle,
+		namespace:      "mynamespace",
+		serviceAccount: "myserviceaccount",
+		appID:          "myappid",
+	})
+
+	k.sentry = sentry.New(t,
+		sentry.WithNamespace("sentrynamespace"),
+		sentry.WithMode(string(modes.StandaloneMode)),
+		sentry.WithCABundle(bundle),
+		sentry.WithTrustDomain("integration.test.dapr.io"),
+	)
+
+	return []framework.Option{
+		framework.WithProcesses(k.sentry, kubeAPI),
+	}
+}
+
+func (k *standalone) Run(t *testing.T, ctx context.Context) {
+	k.sentry.WaitUntilRunning(t, ctx)
+
+	conn := k.sentry.DialGRPC(t, ctx, "spiffe://integration.test.dapr.io/ns/sentrynamespace/dapr-sentry")
+	client := sentrypbv1.NewCAClient(conn)
+
+	pk, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+	csrDer, err := x509.CreateCertificateRequest(rand.Reader, new(x509.CertificateRequest), pk)
+	require.NoError(t, err)
+
+	resp, err := client.SignCertificate(ctx, &sentrypbv1.SignCertificateRequest{
+		Id:                        "myappid",
+		Namespace:                 "mynamespace",
+		CertificateSigningRequest: pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE REQUEST", Bytes: csrDer}),
+		TokenValidator:            sentrypbv1.SignCertificateRequest_INSECURE,
+		Token:                     `{"kubernetes.io":{"pod":{"name":"mypod"}}}`,
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, resp.GetWorkloadCertificate())
+}
