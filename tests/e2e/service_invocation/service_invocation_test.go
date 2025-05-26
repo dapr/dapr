1341469c969bba4b818fcc9cc8e708b9f53764fd
joshvanl <me@joshvanl.dev>
2025-05-27 00:38:21 +0100

    Update service invocation e2e test string check
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/e2e/service_invocation/service_invocation_test.go
+++ tests/e2e/service_invocation/service_invocation_test.go
@@ -1557,7 +1557,7 @@ func TestNegativeCases(t *testing.T) {
 				// Valid errors are:
 				// - `rpc error: code = Internal desc = failed to invoke, id: serviceinvocation-callee-0, err: rpc error: code = Internal desc = error invoking app channel: Post \"http://127.0.0.1:3000/timeouterror\": context deadline exceeded``
 				// - `rpc error: code = DeadlineExceeded desc = context deadline exceeded`
-				assert.Contains(t, testResults.RawError, "rpc error:")
+				assert.Contains(t, testResults.RawError, "rpc error: code = DeadlineExceeded")
 				assert.NotContains(t, testResults.RawError, "Client waited longer than it should have.")
 			})
 
