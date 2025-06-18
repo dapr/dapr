e63b2b0cbae33cddbcbb57550bcc42cb504a1706
joshvanl <me@joshvanl.dev>
2025-06-18 14:20:42 +0100

    Reduce the number of fuzz tests from 1000 -> 500
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/suite/daprd/serviceinvocation/grpc/fuzz.go
+++ tests/integration/suite/daprd/serviceinvocation/grpc/fuzz.go
@@ -50,7 +50,7 @@ type fuzzgrpc struct {
 }
 
 func (f *fuzzgrpc) Setup(t *testing.T) []framework.Option {
-	const numTests = 1000
+	const numTests = 500
 
 	onInvoke := func(ctx context.Context, in *commonv1.InvokeRequest) (*commonv1.InvokeResponse, error) {
 		return &commonv1.InvokeResponse{
