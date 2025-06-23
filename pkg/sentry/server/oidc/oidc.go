61235fd26bac48b1c741a6f76d87e9e85fee4dbe
Jonathan Collinge <jonathancollinge@live.com>
2025-06-23 21:48:37 +0100

    Add name to healthz
    Signed-off-by: Jonathan Collinge <jonathancollinge@live.com>

--- pkg/sentry/server/oidc/oidc.go
+++ pkg/sentry/server/oidc/oidc.go
@@ -3,7 +3,7 @@ Copyright 2025 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -160,7 +160,7 @@ func New(opts Options) (*Server, error) {
 		port:              opts.Port,
 		listenAddress:     opts.ListenAddress,
 		jwks:              opts.JWKS,
-		htarget:           opts.Healthz.AddTarget(),
+		htarget:           opts.Healthz.AddTarget("oidc-server"),
 		jwksURI:           opts.JWKSURI,
 		allowedHosts:      opts.AllowedHosts,
 		tlsConfig:         opts.TLSConfig,
