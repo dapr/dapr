4d0691ed5974af37e84bf51d7c931251e9faf47b
joshvanl <me@joshvanl.dev>
2025-07-08 13:14:56 -0300

    Reduce test cases for slow CI runners
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/suite/daprd/jobs/streaming/reconnect3.go
+++ tests/integration/suite/daprd/jobs/streaming/reconnect3.go
@@ -111,7 +111,7 @@ func (r *reconnect3) Run(t *testing.T, ctx context.Context) {
 		assert.Len(c, r.daprd.GetMetaScheduler(c, ctx).GetConnectedAddresses(), 3)
 	}, time.Second*10, time.Millisecond*10)
 
-	for i := range 100 {
+	for i := range 10 {
 		_, err := r.daprd.GRPCClient(t, ctx).ScheduleJobAlpha1(ctx, &runtimev1pb.ScheduleJobRequest{
 			Job: &runtimev1pb.Job{
 				Name:     strconv.Itoa(i),
@@ -123,7 +123,7 @@ func (r *reconnect3) Run(t *testing.T, ctx context.Context) {
 
 	require.EventuallyWithT(t, func(c *assert.CollectT) {
 		r.lock.Lock()
-		assert.Len(c, r.jobCalledMap, 100)
+		assert.Len(c, r.jobCalledMap, 10)
 		r.lock.Unlock()
 	}, time.Second*5, time.Millisecond*10)
 
@@ -142,7 +142,7 @@ func (r *reconnect3) Run(t *testing.T, ctx context.Context) {
 
 	assert.EventuallyWithT(t, func(c *assert.CollectT) {
 		r.lock.Lock()
-		assert.Len(c, r.jobCalledMap, 100)
+		assert.Len(c, r.jobCalledMap, 10)
 		r.lock.Unlock()
 	}, time.Second*20, time.Millisecond*10)
 }
