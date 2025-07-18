49b39cfa71a930b64228e453312b6089988d863a
Keerthan KK <tkthulasimandiram@gmail.com>
2025-07-18 15:29:43 +0530

    Added integration tests for getState endpoint
    Signed-off-by: Keerthan KK <tkthulasimandiram@gmail.com>

--- tests/integration/suite/daprd/state/http/http.go
+++ tests/integration/suite/daprd/state/http/http.go
@@ -14,5 +14,118 @@ limitations under the License.
 package http
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/client"
+	procdaprd "github.com/dapr/dapr/tests/integration/framework/process/daprd"
+	"github.com/dapr/dapr/tests/integration/suite"
 	_ "github.com/dapr/dapr/tests/integration/suite/daprd/state/http/memory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func init() {
+	suite.Register(new(httpTest))
+	fmt.Println("Registering HTTP state test")
+}
+
+type httpTest struct {
+	daprd *procdaprd.Daprd
+}
+
+func (h *httpTest) Setup(t *testing.T) []framework.Option {
+	h.daprd = procdaprd.New(t, procdaprd.WithInMemoryStateStore("mystore"))
+
+	return []framework.Option{
+		framework.WithProcesses(h.daprd),
+	}
+}
+
+func (h *httpTest) Run(t *testing.T, ctx context.Context) {
+	h.daprd.WaitUntilRunning(t, ctx)
+
+	postURL := fmt.Sprintf("http://localhost:%d/v1.0/state/mystore", h.daprd.HTTPPort())
+
+	httpClient := client.HTTP(t)
+
+	testData := map[string]string{
+		"key1":     "value1",
+		"key2/key": "value2",
+	}
+
+	for key, value := range testData {
+		saveKey(t, httpClient, postURL, key, value, ctx)
+	}
+
+	t.Run("Successful retrieval", func(t *testing.T) {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, postURL+"/key1", nil)
+		require.NoError(t, err)
+		resp, err := httpClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		body, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+
+		assert.Equal(t, `"value1"`, string(body))
+
+	})
+
+	t.Run("Slash containing keys", func(t *testing.T) {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, postURL+"?key=key2/key", nil)
+		require.NoError(t, err)
+		resp, err := httpClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		body, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		assert.Equal(t, `"value2"`, string(body))
+	})
+
+	t.Run("Non-existent key", func(t *testing.T) {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, postURL+"/non-existent-key", nil)
+		require.NoError(t, err)
+		resp, err := httpClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+
+		assert.Equal(t, 204, resp.StatusCode)
+
+		body, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		assert.Equal(t, 0, len(body), "Always give empty body with 204")
+	})
+
+}
+
+func saveKey(t *testing.T, httpClient *http.Client, postURL, key string, value string, ctx context.Context) {
+	stateArray := []map[string]string{
+		{
+			"key":   key,
+			"value": value,
+		},
+	}
+
+	data, err := json.Marshal(stateArray)
+	require.NoError(t, err)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, postURL, bytes.NewReader(data))
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := httpClient.Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+}
