08c0b35873e2c3dcb1ea4b17586848ffcd0559c3
joshvanl <me@joshvanl.dev>
2025-11-27 13:14:03 +0000

    Lint
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/actors/internal/scheduler/scheduler.go
+++ pkg/actors/internal/scheduler/scheduler.go
@@ -200,7 +200,7 @@ func (s *scheduler) Get(ctx context.Context, req *api.GetReminderRequest) (*api.
 
 	var expirationTime time.Time
 	if job.Job.Ttl != nil {
-		expirationTime, err = time.Parse(time.RFC3339, *job.Job.Ttl)
+		expirationTime, err = time.Parse(time.RFC3339, job.GetJob().GetTtl())
 		if err != nil {
 			log.Errorf("Error parsing expiration time for reminder job %s due to: %s", req.Name, err)
 		}
