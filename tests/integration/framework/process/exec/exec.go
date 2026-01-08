a98c7ca4f1ae22bf5210ff813e341433430ec092
joshvanl <me@joshvanl.dev>
2026-01-08 18:32:00 +0000

    Update UID check to check only root
    Problem
    
    By default when running in Kubernetes, Dapr had a check where if the process was running with a UID or GID which was not 65532, the program would exit with an error.
    
    Impact
    
    This causes issues for users running in Openshift/OKD environments where the platform assigns random UIDs to pods for security reasons.
    
    Root Cause
    
    The check was too strict, as it did not account for scenarios where only the root user (UID 0) should be allowed, while other UIDs should be blocked.
    
    Solution
    
    Only check for root (UID 0) and allow all other UIDs to run Dapr.
    
    Fixes https://github.com/dapr/dapr/issues/9132
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/framework/process/exec/exec.go
+++ tests/integration/framework/process/exec/exec.go
@@ -71,9 +71,6 @@ func New(t *testing.T, binPath string, args []string, fopts ...Option) *Exec {
 			}
 		},
 		exitCode: defaultExitCode,
-		envs: map[string]string{
-			"DAPR_UNSAFE_SKIP_CONTAINER_UID_GID_CHECK": "true",
-		},
 	}
 
 	if hostIPOverride := os.Getenv(utils.HostIPEnvVar); hostIPOverride != "" {
