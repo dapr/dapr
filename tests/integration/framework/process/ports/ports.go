b0a8c4470a4cb33bdbedf97c8a9fea56e220987f
joshvanl <me@joshvanl.dev>
2026-01-19 16:23:54 +0000

    Increase block size to 1000 to prevent port bind conflict
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/framework/process/ports/ports.go
+++ tests/integration/framework/process/ports/ports.go
@@ -33,7 +33,7 @@ var (
 	resvP    []*reservedPort
 )
 
-const blockSize = 500
+const blockSize = 1000
 
 // Ports reserves network ports, and then frees them when the test is ready to
 // run so a  process can bind to them at runtime.
