0881f5de1f00437bc7496d3ec5ad6461bd02dfee
joshvanl <me@joshvanl.dev>
2026-01-21 14:41:24 +0000

    Subscriptions: Resiliency Integration Tests
    Adds integration testing for using resiliency policy with Dapr
    subscriptions.
    
    Updates components-contrib to use HEAD version where the in-memory
    pubsub component does not attempt to do an internal retry loop.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

---
+++ tests/integration/suite/daprd/resiliency/subscriptions/grpc.go
@@ -0,0 +1,119 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implieh.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package subscriptions
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	rtv1 "github.com/dapr/dapr/pkg/proto/runtime/v1"
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/daprd"
+	"github.com/dapr/dapr/tests/integration/framework/process/grpc/app"
+	"github.com/dapr/dapr/tests/integration/suite"
+)
+
+func init() {
+	suite.Register(new(grpc))
+}
+
+type grpc struct {
+	daprd *daprd.Daprd
+	app   *app.App
+
+	called atomic.Int32
+}
+
+func (g *grpc) Setup(t *testing.T) []framework.Option {
+	app := app.New(t,
+		app.WithOnTopicEventFn(func(ctx context.Context, in *rtv1.TopicEventRequest) (*rtv1.TopicEventResponse, error) {
+			g.called.Add(1)
+			return &rtv1.TopicEventResponse{Status: rtv1.TopicEventResponse_RETRY}, nil
+		}),
+		app.WithListTopicSubscriptions(func(context.Context, *emptypb.Empty) (*rtv1.ListTopicSubscriptionsResponse, error) {
+			return &rtv1.ListTopicSubscriptionsResponse{
+				Subscriptions: []*rtv1.TopicSubscription{
+					{
+						PubsubName: "mypub",
+						Topic:      "a",
+						Routes: &rtv1.TopicRoutes{
+							Default: "/a",
+						},
+					},
+				},
+			}, nil
+		}),
+	)
+
+	g.daprd = daprd.New(t,
+		daprd.WithAppPort(app.Port(t)),
+		daprd.WithAppProtocol("grpc"),
+		daprd.WithResourceFiles(`
+apiVersion: dapr.io/v1alpha1
+kind: Resiliency
+metadata:
+  name: dapr-resiliency
+spec:
+  policies:
+    retries:
+      retryAppCall:
+        duration: 10ms
+        maxRetries: 4
+        policy: constant
+  targets:
+    components:
+      mypub:
+        inbound:
+          retry: retryAppCall
+`,
+			`apiVersion: dapr.io/v1alpha1
+kind: Component
+metadata:
+  name: mypub
+spec:
+  type: pubsub.in-memory
+  version: v1
+`,
+		),
+	)
+
+	return []framework.Option{
+		framework.WithProcesses(app, g.daprd),
+	}
+}
+
+func (g *grpc) Run(t *testing.T, ctx context.Context) {
+	g.daprd.WaitUntilRunning(t, ctx)
+
+	client := g.daprd.GRPCClient(t, ctx)
+	_, err := client.PublishEvent(ctx, &rtv1.PublishEventRequest{
+		PubsubName: "mypub", Topic: "a",
+		Data:            []byte(`{"status": "completed"}`),
+		DataContentType: "application/json",
+	})
+	require.NoError(t, err)
+
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.Equal(c, int32(5), g.called.Load())
+	}, time.Second*10, time.Millisecond*10)
+
+	time.Sleep(time.Second)
+	assert.Equal(t, int32(5), g.called.Load())
+}
