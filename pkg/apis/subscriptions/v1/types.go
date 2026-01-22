6bd12aa7769bda0af56c96144aed9304885b720f
backtrack5r3 <flennertom@gmail.com>
2026-01-22 22:08:33 +0100

    chore(8224): bump subscriptions CRDs apiVersion to v1
    Signed-off-by: backtrack5r3 <flennertom@gmail.com>

--- pkg/apis/subscriptions/v1/types.go
+++ pkg/apis/subscriptions/v1/types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Dapr Authors
+Copyright 2026 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v2alpha1
+package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,12 +23,13 @@ import (
 
 const (
 	Kind    = "Subscription"
-	Version = "v2alpha1"
+	Version = "v1"
 )
 
 // +genclient
 // +genclient:noStatus
 // +kubebuilder:object:root=true
+// +kubebuilder:storageversion
 
 // Subscription describes an pub/sub event subscription.
 //
