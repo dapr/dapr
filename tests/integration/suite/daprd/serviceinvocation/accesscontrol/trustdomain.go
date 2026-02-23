dapr/dapr

commit ff68dbd870f253057e2e3ec43598755c5bc44e62

[1.17] feat: secretRef for configuration otel headers (#9399) (#9523)
* [1.17]feat: secretRef for configruation otel header



* generate crd + add tests



* Update pkg/operator/api/configurations.go




* Update pkg/apis/configuration/v1alpha1/types.go




* update internal config type & address review



* update test



* Update pkg/config/configuration.go




* update on review



* add integration tests & lint



* update marshal logic



* add tc



* verify headers sent



* check header in k8s mode



* Update tests/integration/suite/operator/api/getconfiguration/secretref.go




* Update tests/integration/suite/operator/api/getconfiguration/secretref.go




* Update tests/integration/suite/daprd/tracing/otel/headers.go




* Update tests/integration/suite/operator/api/getconfiguration/secretref.go




* update on review



* lint



* refactor unmarshal + update on review



* lint



* nil guard



* Update pkg/config/configuration.go




* Update pkg/config/configuration.go




* concreate type



---------

Signed-off-by: Eileen Yu <eileenylj@gmail.com>
Signed-off-by: Eileen Yu <48944635+Eileen-Yu@users.noreply.github.com>
Co-authored-by: Eileen Yu <48944635+Eileen-Yu@users.noreply.github.com>

pkg/config/configuration.go
@@ -33,6 +33,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 
+	configapi "github.com/dapr/dapr/pkg/apis/configuration/v1alpha1"
 	"github.com/dapr/dapr/pkg/buildinfo"
 	env "github.com/dapr/dapr/pkg/config/env"
 	operatorv1pb "github.com/dapr/dapr/pkg/proto/operator/v1"
@@ -284,16 +285,43 @@ type OtelSpec struct {
 	EndpointAddress string `json:"endpointAddress,omitempty" yaml:"endpointAddress,omitempty"`
 	// Defaults to true
 	IsSecure *bool `json:"isSecure,omitempty" yaml:"isSecure,omitempty"`
-	// Headers to add to the request
-	Headers string `json:"headers,omitempty" yaml:"headers,omitempty"`
-	// Timeout for the request in milliseconds
-	Timeout int `json:"timeout,omitempty" yaml:"timeout,omitempty"` // Defaults to 10000
+	// Headers to add to the OTLP trace exporter request
+	Headers []string `json:"headers,omitempty" yaml:"headers,omitempty"`
+	// Timeout for the OTLP trace exporter request
+	Timeout *time.Duration `json:"timeout,omitempty" yaml:"timeout,omitempty"`
 }
 
 // GetIsSecure returns true if the connection should be secured.
-func (o OtelSpec) GetIsSecure() bool {
+func (o *OtelSpec) GetIsSecure() bool {
 	// Defaults to true if nil
 	return o.IsSecure == nil || *o.IsSecure
+}
+
+// UnmarshalJSON handles both the internal config format
+// and the Kubernetes CRD format sent by the operator.
+func (o *OtelSpec) UnmarshalJSON(data []byte) error {
+	var crd configapi.OtelSpec
+	if err := json.Unmarshal(data, &crd); err != nil {
+		return err
+	}
+
+	o.Protocol = crd.Protocol
+	o.EndpointAddress = crd.EndpointAddress
+	o.IsSecure = crd.IsSecure
+
+	if crd.Headers != nil {
+		o.Headers = make([]string, 0, len(crd.Headers))
+		for _, p := range crd.Headers {
+			o.Headers = append(o.Headers, p.Name+"="+p.Value.String())
+		}
+	}
+
+	if crd.Timeout != nil {
+		d := crd.Timeout.Duration
+		o.Timeout = &d
+	}
+
+	return nil
 }
 
 // MetricSpec configuration for metrics.
@@ -572,9 +600,8 @@ func LoadKubernetesConfiguration(config string, namespace string, podName string
 	if len(b) == 0 {
 		return nil, fmt.Errorf("configuration %s not found", config)
 	}
-	conf := LoadDefaultConfiguration()
-	err = json.Unmarshal(b, conf)
-	if err != nil {
+	var conf Configuration
+	if err = json.Unmarshal(b, &conf); err != nil {
 		return nil, err
 	}
 
@@ -585,11 +612,18 @@ func LoadKubernetesConfiguration(config string, namespace string, podName string
 
 	conf.sortMetricsSpec()
 	conf.SetDefaultFeatures()
-	return conf, nil
+	return &conf, nil
 }
 
 // Update configuration from Otlp Environment Variables, if they exist.
 func SetTracingSpecFromEnv(conf *Configuration) error {
+	if conf.Spec.TracingSpec == nil {
+		conf.Spec.TracingSpec = &TracingSpec{}
+	}
+	if conf.Spec.TracingSpec.Otel == nil {
+		conf.Spec.TracingSpec.Otel = &OtelSpec{}
+	}
+
 	// If Otel Endpoint is already set, then don't override.
 	if conf.Spec.TracingSpec.Otel.EndpointAddress != "" {
 		return nil
@@ -641,7 +675,13 @@ func SetTracingSpecFromEnv(conf *Configuration) error {
 	}
 
 	if headers != "" {
-		conf.Spec.TracingSpec.Otel.Headers = headers
+		headersMap, err := StringToHeader(headers)
+		if err != nil {
+			return err
+		}
+		for name, value := range headersMap {
+			conf.Spec.TracingSpec.Otel.Headers = append(conf.Spec.TracingSpec.Otel.Headers, name+"="+value)
+		}
 	}
 
 	var timeoutMs int
@@ -660,7 +700,8 @@ func SetTracingSpecFromEnv(conf *Configuration) error {
 	}
 
 	if timeoutMs > 0 {
-		conf.Spec.TracingSpec.Otel.Timeout = timeoutMs
+		timeout := time.Duration(timeoutMs) * time.Millisecond
+		conf.Spec.TracingSpec.Otel.Timeout = &timeout
 	}
 	return nil
 }

charts/dapr/crds/configuration.yaml
@@ -473,9 +473,46 @@ spec:
                     properties:
                       endpointAddress:
                         type: string
+                      headers:
+                        description: |-
+                          Headers to add to the OTLP trace exporter request.
+                          Each header can contain plaintext values, reference secrets, or reference environment variables.
+                        items:
+                          description: NameValuePair is a name/value pair.
+                          properties:
+                            envRef:
+                              description: EnvRef is the name of an environmental
+                                variable to read the value from.
+                              type: string
+                            name:
+                              description: Name of the property.
+                              type: string
+                            secretKeyRef:
+                              description: SecretKeyRef is the reference of a value
+                                in a secret store component.
+                              properties:
+                                key:
+                                  description: Field in the secret.
+                                  type: string
+                                name:
+                                  description: Secret name.
+                                  type: string
+                              required:
+                              - name
+                              type: object
+                            value:
+                              description: Value of the property, in plaintext.
+                              x-kubernetes-preserve-unknown-fields: true
+                          required:
+                          - name
+                          type: object
+                        type: array
                       isSecure:
                         type: boolean
                       protocol:
+                        type: string
+                      timeout:
+                        description: Timeout for the OTLP trace exporter request.
                         type: string
                     required:
                     - endpointAddress

pkg/apis/configuration/v1alpha1/types.go
@@ -19,6 +19,8 @@ import (
 
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/dapr/dapr/pkg/apis/common"
 )
 
 // +genclient
@@ -236,6 +238,13 @@ type OtelSpec struct {
 	Protocol        string `json:"protocol" yaml:"protocol"`
 	EndpointAddress string `json:"endpointAddress" yaml:"endpointAddress"`
 	IsSecure        *bool  `json:"isSecure" yaml:"isSecure"`
+	// Headers to add to the OTLP trace exporter request.
+	// Each header can contain plaintext values, reference secrets, or reference environment variables.
+	// +optional
+	Headers []common.NameValuePair `json:"headers,omitempty" yaml:"headers,omitempty"`
+	// Timeout for the OTLP trace exporter request.
+	// +optional
+	Timeout *metav1.Duration `json:"timeout,omitempty" yaml:"timeout,omitempty"`
 }
 
 // ZipkinSpec defines Zipkin trace configurations.

pkg/apis/configuration/v1alpha1/zz_generated.deepcopy.go
@@ -21,6 +21,8 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"github.com/dapr/dapr/pkg/apis/common"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	timex "time"
 )
@@ -588,6 +590,18 @@ func (in *OtelSpec) DeepCopyInto(out *OtelSpec) {
 	if in.IsSecure != nil {
 		in, out := &in.IsSecure, &out.IsSecure
 		*out = new(bool)
+		**out = **in
+	}
+	if in.Headers != nil {
+		in, out := &in.Headers, &out.Headers
+		*out = make([]common.NameValuePair, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.Timeout != nil {
+		in, out := &in.Timeout, &out.Timeout
+		*out = new(v1.Duration)
 		**out = **in
 	}
 }

pkg/config/configuration_test.go
@@ -19,6 +19,7 @@ import (
 	"reflect"
 	"slices"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -218,11 +219,14 @@ func TestLoadStandaloneConfiguration(t *testing.T) {
 		assert.Equal(t, "1h", mtlsSpec.AllowedClockSkew)
 	})
 
-	t.Run("tracing spec headers value as string", func(t *testing.T) {
+	t.Run("tracing spec headers and timeout", func(t *testing.T) {
 		config, err := LoadStandaloneConfiguration("./testdata/tracing_config.yaml")
 		require.NoError(t, err)
-		assert.Equal(t, "header1=value1,header2=value2", config.Spec.TracingSpec.Otel.Headers)
-		assert.Equal(t, 5000, config.Spec.TracingSpec.Otel.Timeout)
+		require.Len(t, config.Spec.TracingSpec.Otel.Headers, 2)
+		assert.Equal(t, "header1=value1", config.Spec.TracingSpec.Otel.Headers[0])
+		assert.Equal(t, "header2=value2", config.Spec.TracingSpec.Otel.Headers[1])
+		require.NotNil(t, config.Spec.TracingSpec.Otel.Timeout)
+		assert.Equal(t, 5*time.Second, *config.Spec.TracingSpec.Otel.Timeout)
 	})
 
 	t.Run("tracing invalid spec", func(t *testing.T) {
@@ -475,14 +479,16 @@ func TestSetTracingSpecFromEnv(t *testing.T) {
 	assert.Equal(t, "otlpendpoint:1234", conf.Spec.TracingSpec.Otel.EndpointAddress)
 	assert.Equal(t, "http", conf.Spec.TracingSpec.Otel.Protocol)
 	require.False(t, conf.Spec.TracingSpec.Otel.GetIsSecure())
-	assert.Equal(t, "api-key1=value1,api-key2=value2", conf.Spec.TracingSpec.Otel.Headers)
+	require.Len(t, conf.Spec.TracingSpec.Otel.Headers, 2)
+	assert.Contains(t, conf.Spec.TracingSpec.Otel.Headers, "api-key1=value1")
+	assert.Contains(t, conf.Spec.TracingSpec.Otel.Headers, "api-key2=value2")
 
 	// Spec from config file should not be overridden
 	conf = LoadDefaultConfiguration()
 	conf.Spec.TracingSpec.Otel.EndpointAddress = "configfileendpoint:4321"
 	conf.Spec.TracingSpec.Otel.Protocol = "grpc"
 	conf.Spec.TracingSpec.Otel.IsSecure = ptr.Of(true)
-	conf.Spec.TracingSpec.Otel.Headers = "another-key1=value1,another-key2=value2"
+	conf.Spec.TracingSpec.Otel.Headers = []string{"another-key1=value1"}
 
 	// set tracing spec from env
 	err = SetTracingSpecFromEnv(conf)
@@ -491,7 +497,8 @@ func TestSetTracingSpecFromEnv(t *testing.T) {
 	assert.Equal(t, "configfileendpoint:4321", conf.Spec.TracingSpec.Otel.EndpointAddress)
 	assert.Equal(t, "grpc", conf.Spec.TracingSpec.Otel.Protocol)
 	require.True(t, conf.Spec.TracingSpec.Otel.GetIsSecure())
-	assert.Equal(t, "another-key1=value1,another-key2=value2", conf.Spec.TracingSpec.Otel.Headers)
+	require.Len(t, conf.Spec.TracingSpec.Otel.Headers, 1)
+	assert.Equal(t, "another-key1=value1", conf.Spec.TracingSpec.Otel.Headers[0])
 }
 
 func TestTracingPrecedenceFromEnv(t *testing.T) {
@@ -510,8 +517,40 @@ func TestTracingPrecedenceFromEnv(t *testing.T) {
 
 	assert.Equal(t, "tracesendpoint:4321", conf.Spec.TracingSpec.Otel.EndpointAddress)
 	assert.Equal(t, "grpc", conf.Spec.TracingSpec.Otel.Protocol)
-	assert.Equal(t, "traces-key1=value1,traces-key2=value2", conf.Spec.TracingSpec.Otel.Headers)
-	assert.Equal(t, 2000, conf.Spec.TracingSpec.Otel.Timeout)
+	require.Len(t, conf.Spec.TracingSpec.Otel.Headers, 2)
+	assert.Contains(t, conf.Spec.TracingSpec.Otel.Headers, "traces-key1=value1")
+	assert.Contains(t, conf.Spec.TracingSpec.Otel.Headers, "traces-key2=value2")
+	require.NotNil(t, conf.Spec.TracingSpec.Otel.Timeout)
+	assert.Equal(t, 2*time.Second, *conf.Spec.TracingSpec.Otel.Timeout)
+}
+
+func TestTracingConfigHeadersPrecedenceOverEnv(t *testing.T) {
+	// When endpointAddress is already set in config, SetTracingSpecFromEnv
+	// returns early and env var headers/timeout are not applied
+	t.Setenv(env.OtlpExporterTracesHeaders, "env-key=env-value")
+	t.Setenv(env.OtlpExporterTracesTimeout, "5000")
+
+	conf := &Configuration{
+		Spec: ConfigurationSpec{
+			TracingSpec: &TracingSpec{
+				Otel: &OtelSpec{
+					EndpointAddress: "already-set:4317",
+					Protocol:        "grpc",
+					Headers:         []string{"config-key=config-value"},
+				},
+			},
+		},
+	}
+
+	err := SetTracingSpecFromEnv(conf)
+	require.NoError(t, err)
+
+	// Config headers should be unchanged; env var headers should NOT be appended
+	require.Len(t, conf.Spec.TracingSpec.Otel.Headers, 1)
+	assert.Equal(t, "config-key=config-value", conf.Spec.TracingSpec.Otel.Headers[0])
+
+	// Timeout should remain nil since env vars were not applied
+	assert.Nil(t, conf.Spec.TracingSpec.Otel.Timeout)
 }
 
 func TestTracingTimeoutFromEnv(t *testing.T) {
@@ -520,13 +559,13 @@ func TestTracingTimeoutFromEnv(t *testing.T) {
 	err := SetTracingSpecFromEnv(conf)
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "invalid syntax")
-	assert.Equal(t, 0, conf.Spec.TracingSpec.Otel.Timeout)
+	assert.Nil(t, conf.Spec.TracingSpec.Otel.Timeout)
 
 	t.Setenv(env.OtlpExporterTracesTimeout, "-1")
 	conf = LoadDefaultConfiguration()
 	err = SetTracingSpecFromEnv(conf)
 	require.NoError(t, err)
-	assert.Equal(t, 0, conf.Spec.TracingSpec.Otel.Timeout)
+	assert.Nil(t, conf.Spec.TracingSpec.Otel.Timeout)
 }
 
 func TestAPIAccessRules(t *testing.T) {

pkg/config/testdata/tracing_config.yaml
@@ -6,5 +6,7 @@ metadata:
 spec:
   tracing:
     otel:
-      timeout: 5000
-      headers: "header1=value1,header2=value2"
+      timeout: 5s
+      headers:
+        - "header1=value1"
+        - "header2=value2"

pkg/operator/api/api_test.go
@@ -28,6 +28,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsV1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -35,6 +36,7 @@ import (
 
 	commonapi "github.com/dapr/dapr/pkg/apis/common"
 	componentsapi "github.com/dapr/dapr/pkg/apis/components/v1alpha1"
+	configurationapi "github.com/dapr/dapr/pkg/apis/configuration/v1alpha1"
 	httpendpointapi "github.com/dapr/dapr/pkg/apis/httpEndpoint/v1alpha1"
 	resiliencyapi "github.com/dapr/dapr/pkg/apis/resiliency/v1alpha1"
 	subscriptionsapiV2alpha1 "github.com/dapr/dapr/pkg/apis/subscriptions/v2alpha1"
@@ -190,6 +192,268 @@ func TestProcessComponentSecrets(t *testing.T) {
 		jsonEnc, _ := json.Marshal(enc)
 
 		assert.JSONEq(t, string(jsonEnc), string(c.Spec.Metadata[0].Value.Raw))
+	})
+}
+
+func TestProcessConfigurationSecrets(t *testing.T) {
+	t.Run("no secret ref, no error", func(t *testing.T) {
+		config := configurationapi.Configuration{
+			Spec: configurationapi.ConfigurationSpec{
+				TracingSpec: &configurationapi.TracingSpec{
+					Otel: &configurationapi.OtelSpec{
+						EndpointAddress: "otel.example.com:4317",
+						Protocol:        "grpc",
+						Headers: []commonapi.NameValuePair{
+							{
+								Name: "plain-header",
+								Value: commonapi.DynamicValue{
+									JSON: apiextensionsV1.JSON{Raw: []byte(`"plain-value"`)},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		err := processConfigurationSecrets(t.Context(), &config, "default", nil)
+		require.NoError(t, err)
+		require.Len(t, config.Spec.TracingSpec.Otel.Headers, 1)
+		assert.Equal(t, "plain-header", config.Spec.TracingSpec.Otel.Headers[0].Name)
+		assert.Equal(t, "plain-value", config.Spec.TracingSpec.Otel.Headers[0].Value.String())
+	})
+
+	t.Run("secret ref exists, secret extracted", func(t *testing.T) {
+		config := configurationapi.Configuration{
+			Spec: configurationapi.ConfigurationSpec{
+				TracingSpec: &configurationapi.TracingSpec{
+					Otel: &configurationapi.OtelSpec{
+						EndpointAddress: "otel.example.com:4317",
+						Protocol:        "grpc",
+						Headers: []commonapi.NameValuePair{
+							{
+								Name: "api-key",
+								SecretKeyRef: commonapi.SecretKeyRef{
+									Name: "otel-secret",
+									Key:  "token",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		s := runtime.NewScheme()
+		err := scheme.AddToScheme(s)
+		require.NoError(t, err)
+
+		err = corev1.AddToScheme(s)
+		require.NoError(t, err)
+
+		client := fake.NewClientBuilder().
+			WithScheme(s).
+			WithObjects(&corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "otel-secret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"token": []byte("my-secret-api-key"),
+				},
+			}).
+			Build()
+
+		err = processConfigurationSecrets(t.Context(), &config, "default", client)
+		require.NoError(t, err)
+		require.Len(t, config.Spec.TracingSpec.Otel.Headers, 1)
+		assert.Equal(t, "api-key", config.Spec.TracingSpec.Otel.Headers[0].Name)
+		assert.Equal(t, "my-secret-api-key", config.Spec.TracingSpec.Otel.Headers[0].Value.String())
+	})
+
+	t.Run("secret ref with missing secret returns error", func(t *testing.T) {
+		config := configurationapi.Configuration{
+			Spec: configurationapi.ConfigurationSpec{
+				TracingSpec: &configurationapi.TracingSpec{
+					Otel: &configurationapi.OtelSpec{
+						EndpointAddress: "otel.example.com:4317",
+						Protocol:        "grpc",
+						Headers: []commonapi.NameValuePair{
+							{
+								Name: "api-key",
+								SecretKeyRef: commonapi.SecretKeyRef{
+									Name: "nonexistent-secret",
+									Key:  "token",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		s := runtime.NewScheme()
+		err := scheme.AddToScheme(s)
+		require.NoError(t, err)
+
+		err = corev1.AddToScheme(s)
+		require.NoError(t, err)
+
+		client := fake.NewClientBuilder().
+			WithScheme(s).
+			Build()
+
+		err = processConfigurationSecrets(t.Context(), &config, "default", client)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "failed to get secret")
+	})
+
+	t.Run("secret ref with missing key returns error", func(t *testing.T) {
+		config := configurationapi.Configuration{
+			Spec: configurationapi.ConfigurationSpec{
+				TracingSpec: &configurationapi.TracingSpec{
+					Otel: &configurationapi.OtelSpec{
+						EndpointAddress: "otel.example.com:4317",
+						Protocol:        "grpc",
+						Headers: []commonapi.NameValuePair{
+							{
+								Name: "api-key",
+								SecretKeyRef: commonapi.SecretKeyRef{
+									Name: "otel-secret",
+									Key:  "nonexistent-key",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		s := runtime.NewScheme()
+		err := scheme.AddToScheme(s)
+		require.NoError(t, err)
+
+		err = corev1.AddToScheme(s)
+		require.NoError(t, err)
+
+		client := fake.NewClientBuilder().
+			WithScheme(s).
+			WithObjects(&corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "otel-secret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"token": []byte("my-secret-value"),
+				},
+			}).
+			Build()
+
+		err = processConfigurationSecrets(t.Context(), &config, "default", client)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "key nonexistent-key not found in secret")
+	})
+
+	t.Run("multiple headers with mix of plaintext and secrets", func(t *testing.T) {
+		config := configurationapi.Configuration{
+			Spec: configurationapi.ConfigurationSpec{
+				TracingSpec: &configurationapi.TracingSpec{
+					Otel: &configurationapi.OtelSpec{
+						EndpointAddress: "otel.example.com:4317",
+						Protocol:        "grpc",
+						Headers: []commonapi.NameValuePair{
+							{
+								Name: "plain-header",
+								Value: commonapi.DynamicValue{
+									JSON: apiextensionsV1.JSON{Raw: []byte(`"plain-value"`)},
+								},
+							},
+							{
+								Name: "secret-header",
+								SecretKeyRef: commonapi.SecretKeyRef{
+									Name: "otel-secret",
+									Key:  "token",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		s := runtime.NewScheme()
+		err := scheme.AddToScheme(s)
+		require.NoError(t, err)
+
+		err = corev1.AddToScheme(s)
+		require.NoError(t, err)
+
+		client := fake.NewClientBuilder().
+			WithScheme(s).
+			WithObjects(&corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "otel-secret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"token": []byte("resolved-secret"),
+				},
+			}).
+			Build()
+
+		err = processConfigurationSecrets(t.Context(), &config, "default", client)
+		require.NoError(t, err)
+		require.Len(t, config.Spec.TracingSpec.Otel.Headers, 2)
+		assert.Equal(t, "plain-header", config.Spec.TracingSpec.Otel.Headers[0].Name)
+		assert.Equal(t, "plain-value", config.Spec.TracingSpec.Otel.Headers[0].Value.String())
+		assert.Equal(t, "secret-header", config.Spec.TracingSpec.Otel.Headers[1].Name)
+		assert.Equal(t, "resolved-secret", config.Spec.TracingSpec.Otel.Headers[1].Value.String())
+	})
+
+	t.Run("empty key returns error", func(t *testing.T) {
+		config := configurationapi.Configuration{
+			Spec: configurationapi.ConfigurationSpec{
+				TracingSpec: &configurationapi.TracingSpec{
+					Otel: &configurationapi.OtelSpec{
+						EndpointAddress: "otel.example.com:4317",
+						Protocol:        "grpc",
+						Headers: []commonapi.NameValuePair{
+							{
+								Name: "api-key",
+								SecretKeyRef: commonapi.SecretKeyRef{
+									Name: "otel-secret",
+									Key:  "",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		s := runtime.NewScheme()
+		err := scheme.AddToScheme(s)
+		require.NoError(t, err)
+
+		err = corev1.AddToScheme(s)
+		require.NoError(t, err)
+
+		client := fake.NewClientBuilder().
+			WithScheme(s).
+			WithObjects(&corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "otel-secret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"token": []byte("my-secret-value"),
+				},
+			}).
+			Build()
+
+		err = processConfigurationSecrets(t.Context(), &config, "default", client)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "secret key is required")
 	})
 }
 

commit 1da4a32037a2272400e9f671c8e5b12343d622d8

Fix: Operator config defaulting
Fix regression introduced in https://github.com/dapr/dapr/pull/9523
where daprd was no longer defaulting config, resulting in cases like
service invocation access control being restricted by default.

Signed-off-by: joshvanl <me@joshvanl.dev>

pkg/config/configuration.go
@@ -600,8 +600,8 @@ func LoadKubernetesConfiguration(config string, namespace string, podName string
 	if len(b) == 0 {
 		return nil, fmt.Errorf("configuration %s not found", config)
 	}
-	var conf Configuration
-	if err = json.Unmarshal(b, &conf); err != nil {
+	conf := LoadDefaultConfiguration()
+	if err = json.Unmarshal(b, conf); err != nil {
 		return nil, err
 	}
 
@@ -612,7 +612,7 @@ func LoadKubernetesConfiguration(config string, namespace string, podName string
 
 	conf.sortMetricsSpec()
 	conf.SetDefaultFeatures()
-	return &conf, nil
+	return conf, nil
 }
 
 // Update configuration from Otlp Environment Variables, if they exist.

tests/integration/suite/daprd/serviceinvocation/accesscontrol/trustdomain.go
@@ -1,0 +1,155 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package accesscontrol
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	configapi "github.com/dapr/dapr/pkg/apis/configuration/v1alpha1"
+	"github.com/dapr/dapr/pkg/proto/common/v1"
+	rtv1 "github.com/dapr/dapr/pkg/proto/runtime/v1"
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/daprd"
+	"github.com/dapr/dapr/tests/integration/framework/process/exec"
+	"github.com/dapr/dapr/tests/integration/framework/process/grpc/app"
+	"github.com/dapr/dapr/tests/integration/framework/process/kubernetes"
+	"github.com/dapr/dapr/tests/integration/framework/process/operator"
+	"github.com/dapr/dapr/tests/integration/framework/process/placement"
+	"github.com/dapr/dapr/tests/integration/framework/process/scheduler"
+	"github.com/dapr/dapr/tests/integration/framework/process/sentry"
+	"github.com/dapr/dapr/tests/integration/suite"
+)
+
+func init() {
+	suite.Register(new(trustdomain))
+}
+
+type trustdomain struct {
+	daprd1       *daprd.Daprd
+	daprd2       *daprd.Daprd
+	sentry       *sentry.Sentry
+	trustAnchors []byte
+}
+
+func (e *trustdomain) Setup(t *testing.T) []framework.Option {
+	e.sentry = sentry.New(t)
+
+	bundle := e.sentry.CABundle()
+	e.trustAnchors = bundle.X509.TrustAnchors
+	taFile := filepath.Join(t.TempDir(), "ca.pem")
+	require.NoError(t, os.WriteFile(taFile, bundle.X509.TrustAnchors, 0o600))
+
+	placement := placement.New(t,
+		placement.WithEnableTLS(true),
+		placement.WithTrustAnchorsFile(taFile),
+		placement.WithSentryAddress(e.sentry.Address()),
+	)
+
+	scheduler := scheduler.New(t,
+		scheduler.WithSentry(e.sentry),
+		scheduler.WithID("dapr-scheduler-server-0"),
+	)
+
+	cnf := configapi.Configuration{
+		TypeMeta:   metav1.TypeMeta{APIVersion: "dapr.io/v1alpha1", Kind: "Configuration"},
+		ObjectMeta: metav1.ObjectMeta{Name: "my-config", Namespace: "default"},
+		Spec: configapi.ConfigurationSpec{
+			NameResolutionSpec: &configapi.NameResolutionSpec{
+				Component: "mdns",
+			},
+			MTLSSpec: &configapi.MTLSSpec{
+				ControlPlaneTrustDomain: "localhost",
+				SentryAddress:           e.sentry.Address(),
+			},
+			AccessControlSpec: &configapi.AccessControlSpec{
+				TrustDomain: "helloworld",
+			},
+		},
+	}
+
+	kubeapi := kubernetes.New(t,
+		kubernetes.WithBaseOperatorAPI(t,
+			spiffeid.RequireTrustDomainFromString("localhost"),
+			"default",
+			e.sentry.Port(),
+		),
+		kubernetes.WithClusterDaprConfigurationList(t, &configapi.ConfigurationList{
+			TypeMeta: metav1.TypeMeta{APIVersion: "dapr.io/v1alpha1", Kind: "ConfigurationList"},
+			Items:    []configapi.Configuration{cnf},
+		}),
+	)
+
+	operator := operator.New(t,
+		operator.WithNamespace("default"),
+		operator.WithKubeconfigPath(kubeapi.KubeconfigPath(t)),
+		operator.WithTrustAnchorsFile(e.sentry.TrustAnchorsFile(t)),
+	)
+
+	app := app.New(t)
+
+	e.daprd1 = daprd.New(t,
+		daprd.WithAppProtocol("grpc"),
+		daprd.WithConfigs("my-config"),
+		daprd.WithAppPort(app.Port(t)),
+		daprd.WithExecOptions(exec.WithEnvVars(t, "DAPR_TRUST_ANCHORS", string(bundle.X509.TrustAnchors))),
+		daprd.WithSentryAddress(e.sentry.Address()),
+		daprd.WithEnableMTLS(true),
+		daprd.WithMode("kubernetes"),
+		daprd.WithPlacementAddresses(placement.Address()),
+		daprd.WithSchedulerAddresses(scheduler.Address()),
+		daprd.WithControlPlaneAddress(operator.Address()),
+		daprd.WithDisableK8sSecretStore(true),
+		daprd.WithNamespace("default"),
+	)
+
+	e.daprd2 = daprd.New(t,
+		daprd.WithAppProtocol("grpc"),
+		daprd.WithConfigs("my-config"),
+		daprd.WithAppPort(app.Port(t)),
+		daprd.WithExecOptions(exec.WithEnvVars(t, "DAPR_TRUST_ANCHORS", string(bundle.X509.TrustAnchors))),
+		daprd.WithSentryAddress(e.sentry.Address()),
+		daprd.WithEnableMTLS(true),
+		daprd.WithControlPlaneAddress(operator.Address()),
+		daprd.WithPlacementAddresses(placement.Address()),
+		daprd.WithSchedulerAddresses(scheduler.Address()),
+		daprd.WithMode("kubernetes"),
+		daprd.WithDisableK8sSecretStore(true),
+		daprd.WithNamespace("default"),
+	)
+
+	return []framework.Option{
+		framework.WithProcesses(app, e.sentry, kubeapi, placement, scheduler, operator, e.daprd1, e.daprd2),
+	}
+}
+
+func (e *trustdomain) Run(t *testing.T, ctx context.Context) {
+	e.sentry.WaitUntilRunning(t, ctx)
+	e.daprd1.WaitUntilRunning(t, ctx)
+	e.daprd2.WaitUntilRunning(t, ctx)
+
+	_, err := e.daprd2.GRPCClient(t, ctx).InvokeService(ctx, &rtv1.InvokeServiceRequest{
+		Id: e.daprd1.AppID(),
+		Message: &common.InvokeRequest{
+			Method: "hello",
+		},
+	})
+	require.NoError(t, err)
+}

tests/integration/suite/daprd/serviceinvocation/serviceinvocation.go
@@ -14,6 +14,7 @@ limitations under the License.
 package serviceinvocation
 
 import (
+	_ "github.com/dapr/dapr/tests/integration/suite/daprd/serviceinvocation/accesscontrol"
 	_ "github.com/dapr/dapr/tests/integration/suite/daprd/serviceinvocation/grpc"
 	_ "github.com/dapr/dapr/tests/integration/suite/daprd/serviceinvocation/http"
 )