427d9b64297aab78e3d3bd80eca8c8538a7aba41
joshvanl <me@joshvanl.dev>
2026-03-03 09:41:01 +0000

    Lint
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/proto/sentry/v1/sentry.pb.go
+++ pkg/proto/sentry/v1/sentry.pb.go
@@ -19,13 +19,12 @@
 package sentry
 
 import (
-	reflect "reflect"
-	sync "sync"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 	wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
