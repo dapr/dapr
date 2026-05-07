QaColony/dapr_IaC

Workflow: dedup duplicate completion events at the actor inbox (#9875)
* Workflow: dedup duplicate completion events at the actor inbox

A workflow actor's inbox could accumulate duplicate
TaskCompleted/TaskFailed/TimerFired/ChildWorkflowInstance{Completed,
Failed} events when the upstream emitter (typically an activity actor
whose run-activity reminder fires twice during pod migration) re-sent a
completion that the parent had already committed to history. The
language SDKs do not deduplicate these on the read side: they walk
pending_tasks, find no matching entry, log "Ignoring unexpected <event>
with ID = N" and silently return without producing actions. The next
reminder fire reads the same inbox, runs the orchestrator again, and the
cycle repeats.

This change adds two complementary checks in the workflow actor, both
backed by a shared dedup helper that mirrors the engine-side dedup in
durabletask-go's runtimestate/dedup subpackage:

- addWorkflowEvent (the inbox-write boundary) drops a completion whose
  resolution is already present in either state.History or state.Inbox.
  EventRaised and ExecutionTerminated are intentionally excluded:
  EventRaised is a user-raised signal that may legitimately repeat and
  ExecutionTerminated is idempotent. A dropped event short-circuits
  before the inbox AddToInbox, the signAndSaveState round-trip, and
  the new-event reminder, so a misbehaving emitter no longer drives
  redundant workflow runs.

- callActivities (the dispatch boundary) skips invoking the activity
  actor for any TaskScheduled whose resolution
  (TaskCompleted/TaskFailed) is already in state.History or state.Inbox.

Uses branch: https://github.com/dapr/durabletask-go/pull/96

Signed-off-by: joshvanl <me@joshvanl.dev>

* Adds v1.17.7 release notes for dedup events

Signed-off-by: joshvanl <me@joshvanl.dev>

* Fix callActivity dedup by using runtime state

Signed-off-by: joshvanl <me@joshvanl.dev>

* Update durabletask-go to origin HEAD

Signed-off-by: joshva

tests/integration/suite/daprd/workflow/dedup/childfailed.go
@@ -1,0 +1,105 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	fworkflow "github.com/dapr/dapr/tests/integration/framework/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/api/protos"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(childfailed))
+}
+
+type childfailed struct {
+	workflow *workflow.Workflow
+}
+
+func (cf *childfailed) Setup(t *testing.T) []framework.Option {
+	cf.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(cf.workflow),
+	}
+}
+
+func (cf *childfailed) Run(t *testing.T, ctx context.Context) {
+	cf.workflow.WaitUntilRunning(t, ctx)
+
+	var childCalls atomic.Int32
+
+	cf.workflow.Registry().AddWorkflowN("dedup-failparent", func(ctx *task.WorkflowContext) (any, error) {
+		err := ctx.CallChildWorkflow("dedup-failchild").Await(nil)
+		if err == nil {
+			return nil, errors.New("child unexpectedly succeeded")
+		}
+		return nil, ctx.WaitForSingleEvent("done", time.Minute).Await(nil)
+	})
+	cf.workflow.Registry().AddWorkflowN("dedup-failchild", func(ctx *task.WorkflowContext) (any, error) {
+		childCalls.Add(1)
+		return nil, errors.New("planned failure")
+	})
+
+	cl := cf.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewWorkflow(ctx, "dedup-failparent")
+	require.NoError(t, err)
+	_, err = cl.WaitForWorkflowStart(ctx, id)
+	require.NoError(t, err)
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.Equal(c, int32(1), childCalls.Load())
+		assert.Equal(c, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsChildFailedFor(0)))
+	}, 15*time.Second, 10*time.Millisecond)
+
+	fworkflow.InjectInboxEvent(t, ctx, cf.workflow.DB(), cf.workflow.Dapr(), string(id), &protos.HistoryEvent{
+		EventId:   -1,
+		Timestamp: timestamppb.Now(),
+		EventType: &protos.HistoryEvent_ChildWorkflowInstanceFailed{
+			ChildWorkflowInstanceFailed: &protos.ChildWorkflowInstanceFailedEvent{
+				TaskScheduledId: 0,
+				FailureDetails: &protos.TaskFailureDetails{
+					ErrorType:    "injected",
+					ErrorMessage: "duplicate child failure",
+				},
+			},
+		},
+	})
+
+	cf.workflow.Dapr().Restart(t, ctx)
+	cf.workflow.Dapr().WaitUntilRunning(t, ctx)
+	cl = cf.workflow.BackendClient(t, ctx)
+
+	require.NoError(t, cl.RaiseEvent(ctx, id, "done"))
+
+	meta, err := cl.WaitForWorkflowCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	assert.Equal(t, int32(1), childCalls.Load(), "child workflow must not re-run")
+	assert.Equal(t, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsChildFailedFor(0)),
+		"history must contain exactly one ChildWorkflowInstanceFailed for taskScheduledId=0")
+}

tests/integration/suite/daprd/workflow/dedup/continueasnew.go
@@ -1,0 +1,82 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(continueasnew))
+}
+
+type continueasnew struct {
+	workflow *workflow.Workflow
+}
+
+func (c *continueasnew) Setup(t *testing.T) []framework.Option {
+	c.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(c.workflow),
+	}
+}
+
+func (c *continueasnew) Run(t *testing.T, ctx context.Context) {
+	c.workflow.WaitUntilRunning(t, ctx)
+
+	const iterations = 5
+	var activityCalls atomic.Int32
+
+	c.workflow.Registry().AddWorkflowN("dedup-continueasnew", func(ctx *task.WorkflowContext) (any, error) {
+		var iter int
+		require.NoError(t, ctx.GetInput(&iter))
+
+		if err := ctx.CallActivity("ping").Await(nil); err != nil {
+			return nil, err
+		}
+
+		if iter+1 >= iterations {
+			return iter + 1, nil
+		}
+		ctx.ContinueAsNew(iter + 1)
+		return nil, nil
+	})
+	require.NoError(t, c.workflow.Registry().AddActivityN("ping", func(ctx task.ActivityContext) (any, error) {
+		activityCalls.Add(1)
+		return nil, nil
+	}))
+
+	cl := c.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewWorkflow(ctx, "dedup-continueasnew")
+	require.NoError(t, err)
+
+	meta, err := cl.WaitForWorkflowCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	time.Sleep(time.Second * 2)
+	assert.Equal(t, int32(iterations), activityCalls.Load(),
+		"activity must run once per ContinueAsNew iteration; same EventId across generations must not be skipped")
+}

tests/integration/suite/daprd/workflow/dedup/taskcompleted.go
@@ -1,0 +1,101 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	fworkflow "github.com/dapr/dapr/tests/integration/framework/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/api/protos"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(taskcompleted))
+}
+
+type taskcompleted struct {
+	workflow *workflow.Workflow
+}
+
+func (tc *taskcompleted) Setup(t *testing.T) []framework.Option {
+	tc.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(tc.workflow),
+	}
+}
+
+func (tc *taskcompleted) Run(t *testing.T, ctx context.Context) {
+	tc.workflow.WaitUntilRunning(t, ctx)
+
+	var activityCalls atomic.Int32
+
+	tc.workflow.Registry().AddWorkflowN("dedup-taskcompleted", func(ctx *task.WorkflowContext) (any, error) {
+		if err := ctx.CallActivity("inc").Await(nil); err != nil {
+			return nil, err
+		}
+		return nil, ctx.WaitForSingleEvent("done", time.Minute).Await(nil)
+	})
+	require.NoError(t, tc.workflow.Registry().AddActivityN("inc", func(ctx task.ActivityContext) (any, error) {
+		activityCalls.Add(1)
+		return nil, nil
+	}))
+
+	cl := tc.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewWorkflow(ctx, "dedup-taskcompleted")
+	require.NoError(t, err)
+	_, err = cl.WaitForWorkflowStart(ctx, id)
+	require.NoError(t, err)
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.Equal(c, int32(1), activityCalls.Load())
+		assert.Equal(c, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskCompletedFor(0)))
+	}, 10*time.Second, 10*time.Millisecond)
+
+	fworkflow.InjectInboxEvent(t, ctx, tc.workflow.DB(), tc.workflow.Dapr(), string(id), &protos.HistoryEvent{
+		EventId:   -1,
+		Timestamp: timestamppb.Now(),
+		EventType: &protos.HistoryEvent_TaskCompleted{
+			TaskCompleted: &protos.TaskCompletedEvent{
+				TaskScheduledId: 0,
+				Result:          wrapperspb.String(`null`),
+			},
+		},
+	})
+
+	tc.workflow.Dapr().Restart(t, ctx)
+	tc.workflow.Dapr().WaitUntilRunning(t, ctx)
+	cl = tc.workflow.BackendClient(t, ctx)
+
+	require.NoError(t, cl.RaiseEvent(ctx, id, "done"))
+
+	meta, err := cl.WaitForWorkflowCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	assert.Equal(t, int32(1), activityCalls.Load(), "activity must not re-run")
+	assert.Equal(t, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskCompletedFor(0)),
+		"history must contain exactly one TaskCompleted for taskScheduledId=0")
+}

tests/integration/suite/daprd/workflow/dedup/taskfailed.go
@@ -1,0 +1,105 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	fworkflow "github.com/dapr/dapr/tests/integration/framework/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/api/protos"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(taskfailed))
+}
+
+type taskfailed struct {
+	workflow *workflow.Workflow
+}
+
+func (tf *taskfailed) Setup(t *testing.T) []framework.Option {
+	tf.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(tf.workflow),
+	}
+}
+
+func (tf *taskfailed) Run(t *testing.T, ctx context.Context) {
+	tf.workflow.WaitUntilRunning(t, ctx)
+
+	var activityCalls atomic.Int32
+
+	tf.workflow.Registry().AddWorkflowN("dedup-taskfailed", func(ctx *task.WorkflowContext) (any, error) {
+		err := ctx.CallActivity("boom").Await(nil)
+		if err == nil {
+			return nil, errors.New("activity unexpectedly succeeded")
+		}
+		return nil, ctx.WaitForSingleEvent("done", time.Minute).Await(nil)
+	})
+	require.NoError(t, tf.workflow.Registry().AddActivityN("boom", func(ctx task.ActivityContext) (any, error) {
+		activityCalls.Add(1)
+		return nil, errors.New("planned failure")
+	}))
+
+	cl := tf.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewWorkflow(ctx, "dedup-taskfailed")
+	require.NoError(t, err)
+	_, err = cl.WaitForWorkflowStart(ctx, id)
+	require.NoError(t, err)
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.Equal(c, int32(1), activityCalls.Load())
+		assert.Equal(c, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskFailedFor(0)))
+	}, 10*time.Second, 10*time.Millisecond)
+
+	fworkflow.InjectInboxEvent(t, ctx, tf.workflow.DB(), tf.workflow.Dapr(), string(id), &protos.HistoryEvent{
+		EventId:   -1,
+		Timestamp: timestamppb.Now(),
+		EventType: &protos.HistoryEvent_TaskFailed{
+			TaskFailed: &protos.TaskFailedEvent{
+				TaskScheduledId: 0,
+				FailureDetails: &protos.TaskFailureDetails{
+					ErrorType:    "injected",
+					ErrorMessage: "duplicate failure",
+				},
+			},
+		},
+	})
+
+	tf.workflow.Dapr().Restart(t, ctx)
+	tf.workflow.Dapr().WaitUntilRunning(t, ctx)
+	cl = tf.workflow.BackendClient(t, ctx)
+
+	require.NoError(t, cl.RaiseEvent(ctx, id, "done"))
+
+	meta, err := cl.WaitForWorkflowCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	assert.Equal(t, int32(1), activityCalls.Load(), "activity must not re-run")
+	assert.Equal(t, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskFailedFor(0)),
+		"history must contain exactly one TaskFailed for taskScheduledId=0")
+}

dapr/dapr

(cherry picked from commit e06abfc6ddea208927229136458c976d322249a9)

tests/integration/suite/daprd/workflow/dedup/childfailed.go
@@ -1,0 +1,105 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	fworkflow "github.com/dapr/dapr/tests/integration/framework/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/api/protos"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(childfailed))
+}
+
+type childfailed struct {
+	workflow *workflow.Workflow
+}
+
+func (cf *childfailed) Setup(t *testing.T) []framework.Option {
+	cf.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(cf.workflow),
+	}
+}
+
+func (cf *childfailed) Run(t *testing.T, ctx context.Context) {
+	cf.workflow.WaitUntilRunning(t, ctx)
+
+	var childCalls atomic.Int32
+
+	cf.workflow.Registry().AddOrchestratorN("dedup-failparent", func(ctx *task.OrchestrationContext) (any, error) {
+		err := ctx.CallSubOrchestrator("dedup-failchild").Await(nil)
+		if err == nil {
+			return nil, errors.New("child unexpectedly succeeded")
+		}
+		return nil, ctx.WaitForSingleEvent("done", time.Minute).Await(nil)
+	})
+	cf.workflow.Registry().AddOrchestratorN("dedup-failchild", func(ctx *task.OrchestrationContext) (any, error) {
+		childCalls.Add(1)
+		return nil, errors.New("planned failure")
+	})
+
+	cl := cf.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewOrchestration(ctx, "dedup-failparent")
+	require.NoError(t, err)
+	_, err = cl.WaitForOrchestrationStart(ctx, id)
+	require.NoError(t, err)
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.Equal(c, int32(1), childCalls.Load())
+		assert.Equal(c, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsChildFailedFor(0)))
+	}, 15*time.Second, 10*time.Millisecond)
+
+	fworkflow.InjectInboxEvent(t, ctx, cf.workflow.DB(), cf.workflow.Dapr(), string(id), &protos.HistoryEvent{
+		EventId:   -1,
+		Timestamp: timestamppb.Now(),
+		EventType: &protos.HistoryEvent_SubOrchestrationInstanceFailed{
+			SubOrchestrationInstanceFailed: &protos.SubOrchestrationInstanceFailedEvent{
+				TaskScheduledId: 0,
+				FailureDetails: &protos.TaskFailureDetails{
+					ErrorType:    "injected",
+					ErrorMessage: "duplicate child failure",
+				},
+			},
+		},
+	})
+
+	cf.workflow.Dapr().Restart(t, ctx)
+	cf.workflow.Dapr().WaitUntilRunning(t, ctx)
+	cl = cf.workflow.BackendClient(t, ctx)
+
+	require.NoError(t, cl.RaiseEvent(ctx, id, "done"))
+
+	meta, err := cl.WaitForOrchestrationCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	assert.Equal(t, int32(1), childCalls.Load(), "child workflow must not re-run")
+	assert.Equal(t, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsChildFailedFor(0)),
+		"history must contain exactly one ChildWorkflowInstanceFailed for taskScheduledId=0")
+}

tests/integration/suite/daprd/workflow/dedup/continueasnew.go
@@ -1,0 +1,82 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(continueasnew))
+}
+
+type continueasnew struct {
+	workflow *workflow.Workflow
+}
+
+func (c *continueasnew) Setup(t *testing.T) []framework.Option {
+	c.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(c.workflow),
+	}
+}
+
+func (c *continueasnew) Run(t *testing.T, ctx context.Context) {
+	c.workflow.WaitUntilRunning(t, ctx)
+
+	const iterations = 5
+	var activityCalls atomic.Int32
+
+	c.workflow.Registry().AddOrchestratorN("dedup-continueasnew", func(ctx *task.OrchestrationContext) (any, error) {
+		var iter int
+		require.NoError(t, ctx.GetInput(&iter))
+
+		if err := ctx.CallActivity("ping").Await(nil); err != nil {
+			return nil, err
+		}
+
+		if iter+1 >= iterations {
+			return iter + 1, nil
+		}
+		ctx.ContinueAsNew(iter + 1)
+		return nil, nil
+	})
+	require.NoError(t, c.workflow.Registry().AddActivityN("ping", func(ctx task.ActivityContext) (any, error) {
+		activityCalls.Add(1)
+		return nil, nil
+	}))
+
+	cl := c.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewOrchestration(ctx, "dedup-continueasnew")
+	require.NoError(t, err)
+
+	meta, err := cl.WaitForOrchestrationCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	time.Sleep(time.Second * 2)
+	assert.Equal(t, int32(iterations), activityCalls.Load(),
+		"activity must run once per ContinueAsNew iteration; same EventId across generations must not be skipped")
+}

tests/integration/suite/daprd/workflow/dedup/taskcompleted.go
@@ -1,0 +1,101 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	fworkflow "github.com/dapr/dapr/tests/integration/framework/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/api/protos"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(taskcompleted))
+}
+
+type taskcompleted struct {
+	workflow *workflow.Workflow
+}
+
+func (tc *taskcompleted) Setup(t *testing.T) []framework.Option {
+	tc.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(tc.workflow),
+	}
+}
+
+func (tc *taskcompleted) Run(t *testing.T, ctx context.Context) {
+	tc.workflow.WaitUntilRunning(t, ctx)
+
+	var activityCalls atomic.Int32
+
+	tc.workflow.Registry().AddOrchestratorN("dedup-taskcompleted", func(ctx *task.OrchestrationContext) (any, error) {
+		if err := ctx.CallActivity("inc").Await(nil); err != nil {
+			return nil, err
+		}
+		return nil, ctx.WaitForSingleEvent("done", time.Minute).Await(nil)
+	})
+	require.NoError(t, tc.workflow.Registry().AddActivityN("inc", func(ctx task.ActivityContext) (any, error) {
+		activityCalls.Add(1)
+		return nil, nil
+	}))
+
+	cl := tc.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewOrchestration(ctx, "dedup-taskcompleted")
+	require.NoError(t, err)
+	_, err = cl.WaitForOrchestrationStart(ctx, id)
+	require.NoError(t, err)
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.Equal(c, int32(1), activityCalls.Load())
+		assert.Equal(c, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskCompletedFor(0)))
+	}, 10*time.Second, 10*time.Millisecond)
+
+	fworkflow.InjectInboxEvent(t, ctx, tc.workflow.DB(), tc.workflow.Dapr(), string(id), &protos.HistoryEvent{
+		EventId:   -1,
+		Timestamp: timestamppb.Now(),
+		EventType: &protos.HistoryEvent_TaskCompleted{
+			TaskCompleted: &protos.TaskCompletedEvent{
+				TaskScheduledId: 0,
+				Result:          wrapperspb.String(`null`),
+			},
+		},
+	})
+
+	tc.workflow.Dapr().Restart(t, ctx)
+	tc.workflow.Dapr().WaitUntilRunning(t, ctx)
+	cl = tc.workflow.BackendClient(t, ctx)
+
+	require.NoError(t, cl.RaiseEvent(ctx, id, "done"))
+
+	meta, err := cl.WaitForOrchestrationCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	assert.Equal(t, int32(1), activityCalls.Load(), "activity must not re-run")
+	assert.Equal(t, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskCompletedFor(0)),
+		"history must contain exactly one TaskCompleted for taskScheduledId=0")
+}

tests/integration/suite/daprd/workflow/dedup/taskfailed.go
@@ -1,0 +1,105 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dedup
+
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/dapr/dapr/tests/integration/framework"
+	"github.com/dapr/dapr/tests/integration/framework/process/workflow"
+	fworkflow "github.com/dapr/dapr/tests/integration/framework/workflow"
+	"github.com/dapr/dapr/tests/integration/suite"
+	"github.com/dapr/durabletask-go/api/protos"
+	"github.com/dapr/durabletask-go/task"
+)
+
+func init() {
+	suite.Register(new(taskfailed))
+}
+
+type taskfailed struct {
+	workflow *workflow.Workflow
+}
+
+func (tf *taskfailed) Setup(t *testing.T) []framework.Option {
+	tf.workflow = workflow.New(t)
+	return []framework.Option{
+		framework.WithProcesses(tf.workflow),
+	}
+}
+
+func (tf *taskfailed) Run(t *testing.T, ctx context.Context) {
+	tf.workflow.WaitUntilRunning(t, ctx)
+
+	var activityCalls atomic.Int32
+
+	tf.workflow.Registry().AddOrchestratorN("dedup-taskfailed", func(ctx *task.OrchestrationContext) (any, error) {
+		err := ctx.CallActivity("boom").Await(nil)
+		if err == nil {
+			return nil, errors.New("activity unexpectedly succeeded")
+		}
+		return nil, ctx.WaitForSingleEvent("done", time.Minute).Await(nil)
+	})
+	require.NoError(t, tf.workflow.Registry().AddActivityN("boom", func(ctx task.ActivityContext) (any, error) {
+		activityCalls.Add(1)
+		return nil, errors.New("planned failure")
+	}))
+
+	cl := tf.workflow.BackendClient(t, ctx)
+	id, err := cl.ScheduleNewOrchestration(ctx, "dedup-taskfailed")
+	require.NoError(t, err)
+	_, err = cl.WaitForOrchestrationStart(ctx, id)
+	require.NoError(t, err)
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.Equal(c, int32(1), activityCalls.Load())
+		assert.Equal(c, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskFailedFor(0)))
+	}, 10*time.Second, 10*time.Millisecond)
+
+	fworkflow.InjectInboxEvent(t, ctx, tf.workflow.DB(), tf.workflow.Dapr(), string(id), &protos.HistoryEvent{
+		EventId:   -1,
+		Timestamp: timestamppb.Now(),
+		EventType: &protos.HistoryEvent_TaskFailed{
+			TaskFailed: &protos.TaskFailedEvent{
+				TaskScheduledId: 0,
+				FailureDetails: &protos.TaskFailureDetails{
+					ErrorType:    "injected",
+					ErrorMessage: "duplicate failure",
+				},
+			},
+		},
+	})
+
+	tf.workflow.Dapr().Restart(t, ctx)
+	tf.workflow.Dapr().WaitUntilRunning(t, ctx)
+	cl = tf.workflow.BackendClient(t, ctx)
+
+	require.NoError(t, cl.RaiseEvent(ctx, id, "done"))
+
+	meta, err := cl.WaitForOrchestrationCompletion(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, "ORCHESTRATION_STATUS_COMPLETED", meta.GetRuntimeStatus().String())
+
+	assert.Equal(t, int32(1), activityCalls.Load(), "activity must not re-run")
+	assert.Equal(t, 1, fworkflow.CountHistoryEventsMatching(t, ctx, cl, id, fworkflow.IsTaskFailedFor(0)),
+		"history must contain exactly one TaskFailed for taskScheduledId=0")
+}