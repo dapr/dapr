96859373f0b9e605a06a5965bee035f17295d591
Manuel Menegazzo <65919883+m3nax@users.noreply.github.com>
2026-09-09 22:01:55 +0000

    Added reminder list method to rest api (#10487)
    Signed-off-by: Manuel Menegazzo <manuel.menegazzo@outlook.com>

--- pkg/api/http/actors_tracing_test.go
+++ pkg/api/http/actors_tracing_test.go
@@ -44,6 +44,7 @@ func TestAppendActorReminderTimerSpanAttributesFn(t *testing.T) {
 		{"create reminder", http.MethodPost, "/v1.0/actors/OrderActor/order-123/reminders/CancelOrder", "RegisterActorReminder", "RegisterActorReminder/OrderActor"},
 		{"delete reminder", http.MethodDelete, "/v1.0/actors/OrderActor/order-123/reminders/CancelOrder", "UnregisterActorReminder", "UnregisterActorReminder/OrderActor"},
 		{"get reminder", http.MethodGet, "/v1.0/actors/OrderActor/order-123/reminders/CancelOrder", "GetActorReminder", "GetActorReminder/OrderActor"},
+		{"list reminders", http.MethodGet, "/v1.0/actors/OrderActor/order-123/reminders", "ListActorReminders", "ListActorReminders/OrderActor"},
 		{"create timer", http.MethodPost, "/v1.0/actors/OrderActor/order-123/timers/Refresh", "RegisterActorTimer", "RegisterActorTimer/OrderActor"},
 		{"delete timer", http.MethodDelete, "/v1.0/actors/OrderActor/order-123/timers/Refresh", "UnregisterActorTimer", "UnregisterActorTimer/OrderActor"},
 		{"list timers", http.MethodGet, "/v1.0/actors/OrderActor/order-123/timers", "ListActorTimers", "ListActorTimers/OrderActor"},
