a33edb9fd774626bf13e468329d231306d8db487
joshvanl <me@joshvanl.dev>
2026-09-10 12:03:58 -0300

    Fix activity name colon parsing
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/actors/targets/workflow/activity/activity.go
+++ pkg/actors/targets/workflow/activity/activity.go
@@ -91,10 +91,11 @@ func (a *activity) ID() string {
 	return a.actorID
 }
 
-// workflowID returns the parent workflow instance ID encoded in this
-// activity's actor ID.
+// workflowID returns the parent instance ID encoded in the actor ID. Instance
+// IDs may themselves contain the separator; the task ID after the last one
+// never does.
 func (a *activity) workflowID() (string, error) {
-	if i := strings.Index(a.actorID, common.ActivityIDSeparator); i >= 0 {
+	if i := strings.LastIndex(a.actorID, common.ActivityIDSeparator); i >= 0 {
 		return a.actorID[:i], nil
 	}
 	return "", fmt.Errorf("invalid activity actor ID: '%s'", a.actorID)
