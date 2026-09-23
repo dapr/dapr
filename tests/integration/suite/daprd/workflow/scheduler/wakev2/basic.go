ae6522a16f9538820d1510b9b04ecba47d8fdf9a
Cassandra Coyle <cassie@diagrid.io>
2026-09-23 15:03:27 -0500

    assert eventually for timing to not flake
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/integration/suite/daprd/workflow/scheduler/wakev2/basic.go
+++ tests/integration/suite/daprd/workflow/scheduler/wakev2/basic.go
@@ -121,5 +121,7 @@ func (w *basic) Run(t *testing.T, ctx context.Context) {
 		assert.Zero(c, newEvents)
 	}, time.Second*60, time.Millisecond*50)
 
-	assert.GreaterOrEqual(t, w.daprd.Metrics(t, ctx).SumWithLabels("dapr_runtime_workflow_local_wake_count", "status:success"), float64(3))
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.GreaterOrEqual(c, w.daprd.Metrics(t, ctx).SumWithLabels("dapr_runtime_workflow_local_wake_count", "status:success"), float64(3))
+	}, time.Second*5, time.Millisecond*50)
 }
