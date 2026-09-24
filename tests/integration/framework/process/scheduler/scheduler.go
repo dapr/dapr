181e769ae78d95eb39d637a0fb970c697140f245
Cassandra Coyle <cassie@diagrid.io>
2026-09-24 15:20:53 -0500

    fix ci
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/integration/framework/process/scheduler/scheduler.go
+++ tests/integration/framework/process/scheduler/scheduler.go
@@ -356,7 +356,12 @@ func (s *Scheduler) Client(t *testing.T, ctx context.Context) schedulerv1pb.Sche
 		grpc.WithBlock(), grpc.WithReturnConnectionError(),
 	)
 	require.NoError(t, err)
-	t.Cleanup(func() { require.NoError(t, conn.Close()) })
+	t.Cleanup(func() {
+		s.clientLock.Lock()
+		s.client = nil
+		s.clientLock.Unlock()
+		require.NoError(t, conn.Close())
+	})
 
 	s.client = schedulerv1pb.NewSchedulerClient(conn)
 	return s.client
